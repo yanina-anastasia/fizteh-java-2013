07f7fbe8c104c553e92efd7b37e0a4e1a66ec678
Dmitriy Komanov <dkomanov@gmail.com>
2013-09-14 01:28:23 +0400

    Added code examples

---
+++ src/ru/fizteh/fivt/students/test/calculator/CalculatorMain.java
@@ -0,0 +1,12 @@
+package ru.fizteh.fivt.students.test.calculator;
+
+/**
+ * @author test
+ */
+public class CalculatorMain {
+
+    public static void main(String[] args) {
+        System.out.println("not working");
+        System.exit(1);
+    }
+}
