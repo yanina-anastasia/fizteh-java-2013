00db39dcb7fd887b049def44287d1368e0d3d2fb
haskile <haskile@gmail.com>
2013-09-26 13:42:06 +0400

    Bugs fixed

--- src/ru/fizteh/fivt/students/lizaignatyeva/calculator/Main.java
+++ src/ru/fizteh/fivt/students/lizaignatyeva/calculator/Main.java
@@ -13,10 +13,10 @@ class Calculator {
     private int currentIndexInExpression = 0;
     private int base;
 
-    private void getNextChar()
-    {
-        if (currentIndexInExpression < expression.length())
+    private void getNextChar() {
+        if (currentIndexInExpression < expression.length()) {
             nextChar = expression.charAt(currentIndexInExpression);
+        }
         currentIndexInExpression++;
     }
 
@@ -28,79 +28,74 @@ class Calculator {
     }
 
     private void getNextToken() {
-        if (isDigit(nextChar))
-        {
+        if (isDigit(nextChar)) {
             StringBuilder number = new StringBuilder();
-            while (isDigit(nextChar))
-            {
+            while (isDigit(nextChar)) {
                 number.append(nextChar);
                 getNextChar();
             }
             currentToken = number.toString();
-        }
-        else
-        {
+        } else {
             currentToken = Character.toString(nextChar);
             getNextChar();
         }
-        //System.out.println(currentToken);
     }
 
     private int readExpr()
     {
         int res = readAdd();
-        while (currentToken.equals("+") || currentToken.equals("-"))
-        {
+        while (currentToken.equals("+") || currentToken.equals("-")) {
             char buf = currentToken.charAt(0);
             getNextToken();
             if ((!isDigit(currentToken.charAt(0))) && (!currentToken.equals("(") && (!currentToken.equals("-"))))
                 throw new IllegalArgumentException("a digit expected; symbol " + Character.toString(currentToken.charAt(0)) + " found");
             int add = readAdd();
-            if (buf == '+') res += add;
-            if (buf == '-') res -= add;
+            if (buf == '+') {
+                res += add;
+            }
+            if (buf == '-') {
+                res -= add;
+            }
         }
         return res;
     }
 
-    private int readAdd()
-    {
+    private int readAdd() {
         int res = readMul();
-        while (currentToken.equals("*")||currentToken.equals("/"))
-        {
+        while (currentToken.equals("*")||currentToken.equals("/")) {
             char buf = currentToken.charAt(0);
             getNextToken();
             int mul = readMul();
             if (buf == '*') {
                 res *= mul;
             } else {
-                if (mul == 0)
+                if (mul == 0) {
                     throw new ArithmeticException("division by zero;");
+                }
                 res /= mul;
             }
         }
         return res;
     }
 
-    private int readMul()
-    {
+    private int readMul() {
         int res;
-        if (currentToken.equals("("))
-        {
+        if (currentToken.equals("(")) {
             getNextToken();
             res = readExpr();
-            if (!currentToken.equals(")"))
+            if (!currentToken.equals(")")) {
                 throw new IllegalArgumentException("a closing bracket expected");
+            }
             getNextToken();
-        }
-        else
-        {
+        } else {
             int sign = 1;
             if (currentToken.charAt(0) == '-') {
                 sign = -1;
                 getNextToken();
             }
-            if (!isDigit(currentToken.charAt(0)))
+            if (!isDigit(currentToken.charAt(0))) {
                 throw new IllegalArgumentException("a valid digit expected");
+            }
             res = sign*Integer.parseInt(currentToken, base);
             getNextToken();
         }
@@ -109,43 +104,46 @@ class Calculator {
 
 
     private String cleanFromSpaces(String s) {
-        s = s.replaceAll("\\s","");
+        s = s.replaceAll("\\s\\s", "\\s");
+        //System.out.println(s); //debug output
+        for (int i = 1; i < s.length() - 1; ++i) {
+            if (isDigit(s.charAt(i - 1)) && isDigit(s.charAt(i + 1)) && (s.charAt(i) == ' ')) {
+                throw new IllegalArgumentException("there are two consequent numbers");
+            }
+        }
+        s = s.replaceAll("\\s", "");
         return s;
     }
 
     private String concatenateStrings(String[] args) {
         StringBuilder s = new StringBuilder();
         for (int i = 0; i < args.length; i++) {
-            s.append(cleanFromSpaces(args[i]));
+            s.append(args[i]);
+            s.append(" ");
         }
-        return s.toString();
+        return cleanFromSpaces(s.toString());
     }
 
 
-    public void run(String[] args, int ourBase)
-    {
+    public void run(String[] args, int ourBase) {
         base = ourBase;
         expression = concatenateStrings(args);
-
-       // System.out.println(expression);//for debugging
-
         expression += ".";
+        //System.out.println(expression); //debug output
+
         getNextChar();
         getNextToken();
 
         try {
             int ans = readExpr();
             System.out.println(Integer.toString(ans, base));
-        }
-        catch(ArithmeticException e) {
+        } catch(ArithmeticException e) {
             System.err.println("Invalid operation: " + e.getMessage());
             System.exit(1);
-        }
-        catch(IllegalArgumentException e) {
+        } catch(IllegalArgumentException e) {
             System.err.println("Invalid input: " + e.getMessage());
-
+            System.exit(1);
         }
-
     }
 }
 
