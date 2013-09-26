4bba782798a46b2fea740971fd049ec952e7a75d
AnnaSavinova <anna.savinova@phystech.edu>
2013-09-26 20:03:19 +0400

    Исправлена проверка на переполнение, исправлен баг с 1 1 + 1

--- src/ru/fizteh/fivt/students/annasavinova/calculator/Calculator.java
+++ src/ru/fizteh/fivt/students/annasavinova/calculator/Calculator.java
@@ -1,11 +1,13 @@
 package ru.fizteh.fivt.students.annasavinova.calculator;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Calculator {
+    static int BASE = 10;
     static Stack<String> signStack = new Stack<>();
-    static Stack<Integer> digitStack = new Stack<>();
+    static Stack<Long> digitStack = new Stack<>();
     
     public static int priority(String c) {
         if (c.equals("+") || c.equals("-")) {
@@ -17,39 +19,23 @@ public class Calculator {
             }
     }
     
-    public static int getVal() {
+    public static long getVal() {
         String sign = signStack.pop();
-        int secondVal = digitStack.pop();
-        int firstVal = digitStack.pop();
-        int res = 0;
-        long tmp = 0;
+        long secondVal = digitStack.pop();
+        long firstVal = digitStack.pop();
+        long res = 0;
+        BigInteger tmpFirstVal = BigInteger.valueOf(firstVal);
+        BigInteger tmpSecondVal = BigInteger.valueOf(secondVal);
+        BigInteger tmpRes = BigInteger.ZERO;
         switch (sign) {
         case "+":
-            tmp = firstVal + secondVal;
-            if (tmp * Long.signum(tmp) > Integer.MAX_VALUE) {
-                System.err.println("IntOverflow");
-                System.exit(1);
-            } else {
-                res = (int) tmp;
-            }
+            tmpRes = tmpFirstVal.add(tmpSecondVal);
             break;
         case "-":
-            tmp = firstVal - secondVal;
-            if (tmp * Long.signum(tmp) > Integer.MAX_VALUE) {
-                System.err.println("IntOverflow");
-                System.exit(1);
-            } else {
-                res = (int) tmp;
-            }
+            tmpRes = tmpFirstVal.subtract(tmpSecondVal);
             break;
         case "*":
-            tmp = firstVal * secondVal;
-            if (tmp * Long.signum(tmp) > Integer.MAX_VALUE) {
-                System.err.println("IntOverflow");
-                System.exit(1);
-            } else {
-                res = (int) tmp;
-            }
+            tmpRes = tmpFirstVal.multiply(tmpSecondVal);
             break;
         case "/":
             if (secondVal != 0) {
@@ -58,8 +44,17 @@ public class Calculator {
                 System.err.println("Dividing by zero");
                 System.exit(2);
             }
-            break;
+            return res;
         }
+        if (tmpRes.compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > 0) {
+            System.err.println("Int Overflow");
+            System.exit(1);
+        }
+        if (tmpRes.compareTo(BigInteger.valueOf(Long.MIN_VALUE)) < 0) {
+            System.err.println("Int Overflow");
+            System.exit(1);
+        }
+        res = tmpRes.longValue();
         return res;
     }
     
@@ -81,33 +76,57 @@ public class Calculator {
         return true;
     }
     
+    public static boolean checkSpaces(String str) {
+        Scanner scanner = new Scanner(str);
+        scanner.useRadix(BASE);
+        scanner.useDelimiter("[\\s/+/-/*//]+");
+        while (scanner.hasNext()) {
+            if (scanner.hasNextLong()) {
+                scanner.nextInt();
+                if (scanner.hasNextLong()) {
+                    scanner.close();
+                    return false;
+                }
+            } else {
+                scanner.next();
+            }
+        }
+        scanner.close();
+        return true;
+    }
+    
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Please, input expression, that you want to know");
             System.out.println("Acceptable symbols: +, -, *, /");
-            System.out.println("Radix: 18");
+            System.out.println("Radix: " + BASE);
             return;
         }
         StringBuffer s = new StringBuffer();
         s.append("(");
         for (int i = 0; i < args.length; ++i) {
             s.append(args[i]);
+            s.append(" ");
         }
         s.append(")");
-        String str = s.toString();
-        str = str.replace(" ", "");
-        if (!checkBrackets(str)) {
-            System.err.println("Incorrect bracket sequance");
+        String workingStr = s.toString();
+        if (!checkSpaces(workingStr)) {
+            System.out.println("Incorrect expression: two operands without operator");
             System.exit(3);
         }
-        str = str.replace("(", "( ");
-        str = str.replace(")", " )");
-        str = str.replace("+", " + ");
-        str = str.replace("-", " - ");
-        str = str.replace("*", " * ");
-        str = str.replace("/", " / ");
-        Scanner sc = new Scanner(str);
-        sc.useRadix(18);
+        workingStr = workingStr.replace(" ", "");
+        if (!checkBrackets(workingStr)) {
+            System.err.println("Incorrect bracket sequance");
+            System.exit(4);
+        }
+        workingStr = workingStr.replace("(", "( ");
+        workingStr = workingStr.replace(")", " )");
+        workingStr = workingStr.replace("+", " + ");
+        workingStr = workingStr.replace("-", " - ");
+        workingStr = workingStr.replace("*", " * ");
+        workingStr = workingStr.replace("/", " / ");
+        Scanner sc = new Scanner(workingStr);
+        sc.useRadix(BASE);
         sc.useDelimiter(" ");
         while (sc.hasNext()) {
             if (sc.hasNext("[/(/)/+-/*//]")) {
@@ -134,23 +153,23 @@ public class Calculator {
                     }
                 }
             } else {
-                if (sc.hasNextInt()) {
-                    int currentDigit = sc.nextInt();
+                if (sc.hasNextLong()) {
+                    long currentDigit = sc.nextInt();
                     digitStack.push(currentDigit);
                 } else {
-                    System.err.println("Incorrect expression: incorrect symbol or num of operators and operands");
+                    System.err.println("Incorrect expression");
                     sc.close();
-                    System.exit(4);
+                    System.exit(5);
                 }
             }
         }
-        int result = digitStack.pop();
+        long result = digitStack.pop();
         if (digitStack.empty()) {
-            System.out.println("Result: " + Integer.toString(result, 18));
+            System.out.println("Result: " + Long.toString(result, BASE));
         } else {
             System.err.println("Incorrect num of operators and operands" + digitStack.peek());
             sc.close();
-            System.exit(5);
+            System.exit(6);
         }
         sc.close();
     }
