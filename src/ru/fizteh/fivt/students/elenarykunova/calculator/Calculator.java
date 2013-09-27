200378ff7b67fe779294a2a53bb11a9a496e5026
Arlenka <alenarykunova@gmail.com>
2013-09-28 01:53:13 +0400

    fixed again...

--- src/ru/fizteh/fivt/students/elenarykunova/calculator/Calculator.java
+++ src/ru/fizteh/fivt/students/elenarykunova/calculator/Calculator.java
@@ -19,7 +19,6 @@ public class Calculator {
     }
     
     private static boolean makeCalculation() {
-        // помним об обратном порядке!
         if (!isCorrectExpression()) {
             return false;
         }
@@ -28,7 +27,6 @@ public class Calculator {
         String operator = operators.pop();
         
         long result = 0;
-        // проверяем на переполнение
         BigInteger firstBig = BigInteger.valueOf(firstOperand);
         BigInteger secondBig = BigInteger.valueOf(secondOperand);
         BigInteger realRes = null, myRes = null;
@@ -83,15 +81,15 @@ public class Calculator {
     }
     
     private static boolean isCorrectSpaceSeq(String str) {
-        // если между двумя цифрами стоит хотя бы один пробел, то выражение неверно. 
-        if (Pattern.matches("[0-9a-gA-G\\s/(/)/+-/*//]*([0-9a-zA-Z]([\\s]+)[0-9a-zA-Z])+[0-9a-gA-G\\s/(/)/+-/*//]*", str)) {
+        if (Pattern.matches("[0-9a-gA-G\\s/(/)/+-/*//]*" +
+                "([0-9a-zA-Z]([\\s]+)[0-9a-zA-Z])+" +
+                "[0-9a-gA-G\\s/(/)/+-/*//]*", str)) {
             return false;
         }
         return true;
     }
     
     private static int getPriority(String str) {
-        // приоритет операций
         if (str.equals("(") || str.equals(")")) {
             return 1;
         }
@@ -106,7 +104,7 @@ public class Calculator {
     
     public static void main(String [] args) {
         if (args.length < 1) {
-            System.err.println("No expression is found. Use 0-9, A-G, a-g, +, -, *, /, (, ) to write an expression that you want to calc.");
+            System.err.println("No expression is found.");
             System.exit(1);
         }
     
@@ -119,48 +117,39 @@ public class Calculator {
         }
         String str = s.toString();
         
-        // проверка на корректность входа
-        // 1) проверка на соответствие символов
         if (!Pattern.matches("[0-9a-gA-G\\s/(/)/+-/*//]*", str)) {
             System.err.println("Unexpected symbol. Use 0-9, A-G, a-g, +, -, *, /, (, ).");
             System.exit(1);
         }
-        // 2) проверка на правильную скобочную последовательность
         if (!isCorrectBracketSeq(str)) {
             System.err.println("Wrong expression: brackets mismatch.");
             System.exit(1);
         }
-        // 3) проверка на лишние пробелы (не должно быть пробелов между двумя цифрами)
         if (!isCorrectSpaceSeq(str)) {
             System.err.println("Wrong expression: too many spaces.");
             System.exit(1);
         }
         
-        // убираем все пробелы
         str = str.replace(" ", "");
         
         Scanner myScan = new Scanner(str);
         myScan.useRadix(base);
 
-        // разделяем на операнды и операции. Разделение будет происходить на стыке цифры и операции, операции и цифры, операции и операции
-        myScan.useDelimiter("((?<=[0-9a-gA-G])(?=[/(/)/+-/*//]))|((?<=[/(/)/+-/*//])(?=[0-9a-gA-G]))|((?<=[/(/)/+-/*//])(?=[/(/)/+-/*//]))");         
+        myScan.useDelimiter("((?<=[0-9a-gA-G])(?=[/(/)/+-/*//]))" +
+                "|((?<=[/(/)/+-/*//])(?=[0-9a-gA-G]))" +
+                "|((?<=[/(/)/+-/*//])(?=[/(/)/+-/*//]))");         
 
-        // собственно начинаем вычисления
         int currPriority = 0;
         String currOperator;
         while (myScan.hasNext()) {
             if (myScan.hasNextLong()) {
-                // получили операнд - добавили в стек операндов
                 long newInt = myScan.nextLong();
                 operands.push(newInt);
             } else {
                 currOperator = myScan.next();
                 if (currOperator.equals("(")) {
-                    // получили ( - добавили в стек операций
                     operators.push(currOperator);
                 } else if (currOperator.equals(")")) {
-                    // получили ) -
-                    // пока не наткнемся на открывающую скобку, достаем из стека операции и тут же вычисляем
                     while (!operators.isEmpty() && !operators.peek().equals("(")) {
                         if (!makeCalculation()) {
                             myScan.close();
@@ -168,13 +157,16 @@ public class Calculator {
                         }
                     }
                     if (!operators.isEmpty()) {
-                        operators.pop();  // достали последнюю открывающую скобку
+                        operators.pop();
                     } else {
-                        System.err.println("Wrong expression: '(' not found"); // хотя такого не должно быть, 
-                        myScan.close();                            // т.к. мы проверили скобочную последовательность на правильность
+                        System.err.println("Wrong expression: '(' not found");  
+                        myScan.close();                            
                         System.exit(1);
                     }
-                } else if (currOperator.equals("+") || currOperator.equals("-") || currOperator.equals("*") || currOperator.equals("/")) {
+                } else if (currOperator.equals("+") 
+                            || currOperator.equals("-") 
+                            || currOperator.equals("*") 
+                            || currOperator.equals("/")) {
                     currPriority = getPriority(currOperator);
                     while (!operators.isEmpty() && getPriority(operators.peek()) >= currPriority) {
                         if (!makeCalculation()) {
@@ -187,10 +179,7 @@ public class Calculator {
                     System.err.println("Too large number.");
                     myScan.close();
                     System.exit(1);
-                }
-                    // для других операций достаем из стека все с приоритетом не меньше нашего 
-                    // и производим вычисления, потом добавляем операцию в стек
-                     
+                }                     
             }
         }
         
