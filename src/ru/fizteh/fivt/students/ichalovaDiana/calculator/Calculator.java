153486a90e3ce0c3c1a62ba5702ccdface6e1ac5
Meerstein <dermeerstein@gmail.com>
2013-09-28 01:27:36 +0400

    калькулятор №3

--- src/ru/fizteh/fivt/students/ichalovaDiana/calculator/Calculator.java
+++ src/ru/fizteh/fivt/students/ichalovaDiana/calculator/Calculator.java
@@ -5,9 +5,9 @@ import java.math.BigInteger;
 
 
 public class Calculator {
-    
+
     static final int RADIX = 19;
-    
+
     public static void main(String[] args) {
         //args = new String[]{"3 * (4E + 5) * (3 + 7) + 1 * 2"};
         StringBuilder concatArgs = new StringBuilder();
@@ -15,24 +15,24 @@ public class Calculator {
             concatArgs.append(item).append(" ");
         }
         String expression = concatArgs.toString();
-        
+
         try {
             StringParser parser = new StringParser(expression, RADIX);
             BigInteger result = calculateExpression(parser);
             if (parser.getCurrentLexemeType() != LexemeType.END) {
                 throw new Exception("Unexpected lexeme");
             }
-            System.out.println(result.toString(RADIX));            
+            System.out.println(result.toString(RADIX));
         } catch (Exception e) {
             System.err.println(e.getMessage());
             System.exit(1);
         }
     }
-    
+
     private static BigInteger calculateExpression(StringParser parser) throws Exception {
         BigInteger result = calculateItem(parser);
         LexemeType currentLexeme = parser.getCurrentLexemeType();
-        while (currentLexeme == LexemeType.PLUS 
+        while (currentLexeme == LexemeType.PLUS
                 || currentLexeme == LexemeType.MINUS) {
             parser.getNextLexemeType();
             if (currentLexeme == LexemeType.PLUS) {
@@ -44,11 +44,11 @@ public class Calculator {
         }
         return result;
     }
-      
+
     private static BigInteger calculateItem(StringParser parser) throws Exception {
         BigInteger result = calculateMultiplier(parser);
         LexemeType currentLexeme = parser.getCurrentLexemeType();
-        while (currentLexeme == LexemeType.MULTIPLICATION 
+        while (currentLexeme == LexemeType.MULTIPLICATION
                 || currentLexeme == LexemeType.INTEGER_DIVISION) {
             parser.getNextLexemeType();
             if (currentLexeme == LexemeType.MULTIPLICATION) {
@@ -60,11 +60,11 @@ public class Calculator {
         }
         return result;
     }
-    
+
     private static BigInteger calculateMultiplier(StringParser parser) throws Exception {
         BigInteger result;
         LexemeType currentLexeme = parser.getCurrentLexemeType();
-        
+
         if (currentLexeme == LexemeType.NUMBER) {
             result = parser.getCurrentNumber();
         } else if (currentLexeme == LexemeType.OPENING_BRACKET) {
@@ -78,45 +78,51 @@ public class Calculator {
             throw new Exception("Unexpected lexeme");
         }
         parser.getNextLexemeType();
-        
+
         return result;
     }
 }
 
 enum LexemeType {
-    NUMBER, OPENING_BRACKET, CLOSING_BRACKET, PLUS, MINUS, 
-    MULTIPLICATION, INTEGER_DIVISION, END;
+    NUMBER,
+    OPENING_BRACKET,
+    CLOSING_BRACKET,
+    PLUS,
+    MINUS,
+    MULTIPLICATION,
+    INTEGER_DIVISION,
+    END;
 }
 
 class StringParser {
-    
+
     private String expression;
     private HashSet<Character> possibleDigitsSet = new HashSet<Character>();
     private int radix;
     private int currentPosition = 0;
     private LexemeType currentState;
-    
+
     private BigInteger currentNumber;
-    
+
     StringParser(String stringToParse, int base) throws Exception {
         expression = stringToParse;
         calculatePossibleDigitsSet(base);
         radix = base;
         getNextLexemeType();
     }
-    
+
 
     LexemeType getNextLexemeType() throws Exception {
-       while (currentPosition < expression.length() 
+       while (currentPosition < expression.length()
                && Character.isWhitespace(expression.charAt(currentPosition))) {
            currentPosition += 1;
        }
-       
+
        if (currentPosition >= expression.length()) {
            currentState = LexemeType.END;
            return currentState;
        }
-       
+
        char currentChar = expression.charAt(currentPosition);
        
        if (currentChar == '(') {
@@ -140,8 +146,8 @@ class StringParser {
        } else if (possibleDigitsSet.contains(currentChar)) {
            int startPosition = currentPosition;
            int endPosition;
-           
-           while (currentPosition + 1 < expression.length() 
+
+           while (currentPosition + 1 < expression.length()
                    && possibleDigitsSet.contains(expression.charAt(currentPosition + 1))) {
                currentPosition += 1;
            }
@@ -159,11 +165,11 @@ class StringParser {
         //assert (currentState == LexemeType.NUMBER);
         return currentNumber;
     }
-    
+
     LexemeType getCurrentLexemeType() {
         return currentState;
     }
-    
+
     private void calculatePossibleDigitsSet(int base) {
         for (int i = 0; i < base; ++i) {
             if (i < 10) {
