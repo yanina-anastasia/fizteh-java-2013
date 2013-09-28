2c280a74eaf85262506d66f9413f1c9b320faccd
Dmitry Zvonarev <dmitry.zvonorev@phystech.edu>
2013-09-28 16:27:27 +0400

    latest version of calc

--- src/ru/fizteh/fivt/students/dzvonarev/calc/Calc.java
+++ src/ru/fizteh/fivt/students/dzvonarev/calc/Calc.java
@@ -6,108 +6,110 @@ class Calc {
 
     public static int priority(String str) {
         if (str.equals("+") || str.equals("-")) {
-	        return 1;
-	    }
-	    if (str.equals("*") || str.equals("/")) {
-	        return 2;
-	    }
-	    if (str.equals("(")) {
-	        return 0;
-	    }
-	    if (!str.equals("*") && !str.equals("-") && !str.equals("/")
-	        && !str.equals("+") && !str.equals("(")) {
-	            System.err.println("Wrong operation");
-		        System.exit(1);
-	    }
+            return 1;
+        }
+        if (str.equals("*") || str.equals("/")) {
+            return 2;
+        }
+        if (str.equals("(")) {
+            return 0;
+        }
+        if (!str.equals("*") && !str.equals("-") && !str.equals("/")
+                && !str.equals("+") && !str.equals("(")) {
+            System.err.println("Wrong operation");
+            System.exit(1);
+        }
         return 0;
     }
 
     public static boolean noOverFlowAdd(long num1, long num2) {
         if (num2 <= 0) {
-	        if (Long.MIN_VALUE - num2 <= num1) {
-	            return true;
-	        } else {
-		        return false;
-		    }
-		}
-	    if (num2 > 0) {
-	        if (Long.MAX_VALUE - num2 >= num1) {
-		        return true;
-	        } else {
-		        return false;
+            if (Long.MIN_VALUE - num2 <= num1) {
+                return true;
+            } else {
+                return false;
             }
         }
-	    return true;
+        if (num2 > 0) {
+            if (Long.MAX_VALUE - num2 >= num1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
-	
+
     public static boolean noOverFlowMinus(long num1, long num2) {
         if (num2 <= 0) {
-	        if (Long.MAX_VALUE + num2 >= num1) {
-	            return true;
-	        } else { 
-	            return false;
+            if (Long.MAX_VALUE + num2 >= num1) {
+                return true;
+            } else {
+                return false;
             }
         }
-	    if (num2 > 0) {
-	        if (Long.MIN_VALUE + num2 <= num1) {
-		        return true;
-	        } else {
-		        return false;
+        if (num2 > 0) {
+            if (Long.MIN_VALUE + num2 <= num1) {
+                return true;
+            } else {
+                return false;
             }
         }
-	    return true;
+        return true;
     }
-	
+
     public static boolean noOverFlowMul(long num1, long num2) {
         if (num1 == 0 || num2 == 0) {
             return true;
         }
         if (num1 > 0 && num2 > 0 || num1 < 0 && num2 < 0 || num1 < 0) {
-            if ((Long.MIN_VALUE / num2 <= num1) && (num1 <= Long.MAX_VALUE / num2)) {
-	            return true;
-	        } else {
-	            return false;
+            if ((Long.MIN_VALUE / num2 <= num1)
+                    && (num1 <= Long.MAX_VALUE / num2)) {
+                return true;
+            } else {
+                return false;
             }
         }
         if (num2 < 0) {
-            if ((Long.MIN_VALUE / num2 >= num1) && (num1 >= Long.MAX_VALUE / num2)) {
-	            return true;
-	        } else {
-	            return false;
+            if ((Long.MIN_VALUE / num2 >= num1)
+                    && (num1 >= Long.MAX_VALUE / num2)) {
+                return true;
+            } else {
+                return false;
             }
         }
         return true;
     }
-	
+
     public static long doOp(long num1, String op, long num2) {
         if (op.equals("+")) {
-	        if (noOverFlowAdd(num1, num2)) {
-	            return num1 + num2;
-	        } else {
-	            System.err.println("Too big numbers plus");
-		        System.exit(1);
-	        }
+            if (noOverFlowAdd(num1, num2)) {
+                return num1 + num2;
+            } else {
+                System.err.println("Too big numbers plus");
+                System.exit(1);
+            }
         }
-	    if (op.equals("*")) {
-	        if (noOverFlowMul(num1, num2)) {
-		        return num1 * num2;
-	        } else {
-	            System.err.println("Too big numbers mul");
-		        System.exit(1);
-	        }
+        if (op.equals("*")) {
+            if (noOverFlowMul(num1, num2)) {
+                return num1 * num2;
+            } else {
+                System.err.println("Too big numbers mul");
+                System.exit(1);
+            }
         }
-	    if (op.equals("-")) {
-	        if (noOverFlowMinus(num1, num2)) {
-	            return num1 - num2;
-	        } else {
-		        System.err.println("Too big numbers min");
-		        System.exit(1);
-	        }
+        if (op.equals("-")) {
+            if (noOverFlowMinus(num1, num2)) {
+                return num1 - num2;
+            } else {
+                System.err.println("Too big numbers min");
+                System.exit(1);
+            }
         }
         if (op.equals("/") && num2 != 0) {
-	        return num1 / num2;
+            return num1 / num2;
         } else {
-	        System.err.println("Division by zero");
+            System.err.println("Dividing by zero");
             System.exit(1);
         }
         return 0;
@@ -127,77 +129,106 @@ class Calc {
 
     public static void main(String[] args) {
         for (int i = 0; i < args.length; ++i) {
-            if (args[i].matches("[0-9A-Ia-i]+")) {
-                number.push(args[i]);
-	    } else {
-            if (!args[i].equals("+") && !args[i].equals("-")
-                && !args[i].equals("*") && !args[i].equals("/")
-                    && !args[i].equals("(") && !args[i].equals(")")) {
-                        System.err.println("Wrong expression");
-                        System.exit(1);
-		            }
-        }
-
-	    if (args[i].equals("(")) {
-	        operation.push(args[i]);
-        }
-
-	    if (args[i].equals(")")) {
-	        if (!operation.isEmpty()) {
-		        while (!operation.peek().equals("(")) {
-                    currentOperation();
-			        if (operation.isEmpty()) {
-			            System.err.println("Wrong bracket sequence");
-			            System.exit(1);
-			        }
-		        }
-		        operation.pop(); /* we pop open clause */
-		    } else {
-		        System.err.println("Wrong bracket sequence");
-		        System.exit(1);
-            }
-        }
-		
-        if (args[i].equals("+") || args[i].equals("-") 
-            || args[i].equals("*") || args[i].equals("/")) {
-                if (operation.isEmpty()) {
-			        operation.push(args[i]);
-			        continue;
-			    }
-			    if (priority(operation.peek()) < priority(args[i])) {
-			        operation.push(args[i]);
-			        continue;
-			    }
-			    /* now we do if op's bigger priority then now */
-			    while (priority(operation.peek()) >= priority(args[i])) {
-			        if (number.size() < 2) {
-			            System.err.print("I can't do ");
-			            System.err.print(args[i]);
-			            System.err.println(" operation");
-			            System.exit(1);
-			        }
-			        currentOperation();
-			        if (operation.isEmpty()) {
-			            break;
-			        }
+            int j = 0;
+            while (j < args[i].length()) {
+                String str = "";
+                boolean num = true;
+                while (num && j < args[i].length()) {
+                    String strNew = str + args[i].charAt(j);
+                    if (strNew.matches("[0-9A-Ia-i]+")) {
+                        str = strNew;
+                        num = true;
+                        ++j;
+                    } else {
+                        num = false;
+                    }
                 }
-		        operation.push(args[i]); /* we push current op */
+                str = str.substring(0, str.length()); /*
+                                                       * deleting last - not
+                                                       * number symbol
+                                                       */
+                if (!str.isEmpty()) { /* we've found number */
+                    number.push(str);
+                    continue;
+                }
+                str = "" + args[i].charAt(j);
+                if (str.equals(" ")) {
+                    ++j;
+                    continue;
+                }
+
+                if (!str.equals("+") && !str.equals("-") && !str.equals("*")
+                        && !str.equals("/") && !str.equals("(") && !str.equals(")")) {
+                    System.err.println("Unknown symbol =  " + str);
+                    System.exit(1);
+                }
+
+                if (str.equals("+") || str.equals("-") || str.equals("*")
+                        || str.equals("/")) { /* we've found operation */
+                    if (operation.isEmpty()) {
+                        operation.push(str);
+                    } else {
+                        if (priority(operation.peek()) < priority(str)) {
+                            operation.push(str);
+                        } else { /* now we do if op's bigger priority then now */                            
+                            while (priority(operation.peek()) >= priority(str)) {
+                                if (number.size() < 2) {
+                                    System.err.print("I can't do " + str + " operation");
+                                    System.exit(1);
+                                }
+                                currentOperation();
+                                if (operation.isEmpty()) {
+                                    break;
+                                }
+                            }
+                            operation.push(str);
+                        }
+                    }
+                    ++j;
+                    continue;
+                }
+
+                if (str.equals("(")) {
+                    operation.push(str);
+                    ++j;
+                    continue;
+                }
+
+                if (str.equals(")")) {
+                    if (!operation.isEmpty()) {
+                        while (!operation.peek().equals("(")) {
+                            currentOperation();
+                            if (operation.isEmpty()) {
+                                System.err.println("Wrong bracket sequence");
+                                System.exit(1);
+                            }
+                        }
+                        operation.pop(); // we pop open clause
+                    } else {
+                        System.err.println("Wrong bracket sequence");
+                        System.exit(1);
+                    }
+                    ++j;
+                    continue;
+                }
             }
         }
 
         while (!operation.isEmpty()) {
-	        if (number.size() < 2) {
-	            System.err.print("I can't do ");
-		        System.err.print(operation.peek());
-		        System.err.println(" operation");
-		        System.exit(1);
-	        }
-	        currentOperation();
-	    }
-	    if (number.size() >= 2) {
-	        System.err.println("Wrong expression");
-	        System.exit(1);
-	    }
+            if (number.size() < 2) {
+                System.err.print("I can't do " + operation.peek() + " operation");
+                System.exit(1);
+            }
+            if (operation.peek().equals("(")) {
+                System.err.print("Wrong bracket sequence");
+                System.exit(1);
+            }
+            currentOperation();
+        }
+        if (number.size() != 1) {
+            System.err.println("Wrong expression");
+            System.exit(1);
+        }
         System.out.println(number.pop());
     }
 }
