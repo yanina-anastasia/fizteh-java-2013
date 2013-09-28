1a885fb8d76e7fa2e20d367fe357739243d7703d
Anna Vyatkina <ava.katushka@gmail.com>
2013-09-28 11:10:50 +0400

    A little more...

--- src/ru/fizteh/fivt/students/vyatkina/calc/StackCalculator.java
+++ src/ru/fizteh/fivt/students/vyatkina/calc/StackCalculator.java
@@ -6,105 +6,105 @@ import java.util.Stack;
 
 public class StackCalculator {
 
-    private Stack<Integer> numberStack = new Stack<Integer> ();
-    private Stack<Character> operantionStack = new Stack<Character> ();
+    private Stack<Integer> numberStack = new Stack<Integer>();
+    private Stack<Character> operantionStack = new Stack<Character>();
     static final int RADIX = 19;
 
     boolean nextIsNumber = true;
 
-    int calculateExpression (String expression) throws IllegalArgumentException {
-        Scanner scanner = new Scanner ("(" + expression.replace (" ", "") + ")");
+    int calculateExpression(String expression) throws IllegalArgumentException {
+        Scanner scanner = new Scanner("(" + expression.replace(" ", "") + ")");
 
         String bracket = "\\(|\\)";
         String operand = "\\+|\\-|\\*|\\/";
         String minus = "\\-";
 
-        scanner.useDelimiter ( "((?<=\\w)(?=\\p{Punct})|(?<=\\p{Punct})(?=\\w)|(?<=\\p{Punct})(?=\\p{Punct}))" );
-        scanner.useRadix (RADIX);
+        scanner.useDelimiter("((?<=\\w)(?=\\p{Punct})|(?<=\\p{Punct})(?=\\w)|(?<=\\p{Punct})(?=\\p{Punct}))");
+        scanner.useRadix(RADIX);
 
-        while (scanner.hasNext ()) {
-            if (scanner.hasNext (bracket)) {
-                handleBrackets (scanner.next (bracket).charAt (0));
-            } else if (expectedNumber ()) {
+        while (scanner.hasNext()) {
+            if (scanner.hasNext(bracket)) {
+                handleBrackets(scanner.next(bracket).charAt(0));
+            } else if (expectedNumber()) {
                 int sign = 1;
-                if (scanner.hasNext (minus)) {
-                    scanner.next (minus);
+                if (scanner.hasNext(minus)) {
+                    scanner.next(minus);
                     sign = -1;
                 }
-                if (scanner.hasNextInt ()) {
-                    handleNumber (scanner.nextInt () * sign);
-                    changeExpectation ();
+                if (scanner.hasNextInt()) {
+                    handleNumber(scanner.nextInt() * sign);
+                    changeExpectation();
                 }
-            } else if (expectedOperand () && scanner.hasNext (operand)) {
-                handleOperand (scanner.next (operand).charAt (0));
-                changeExpectation ();
+            } else if (expectedOperand() && scanner.hasNext(operand)) {
+                handleOperand(scanner.next(operand).charAt(0));
+                changeExpectation();
             } else {
-                throw new IllegalArgumentException ( "Unexpected token: [" + scanner.next ()
+                throw new IllegalArgumentException("Unexpected token: [" + scanner.next()
                         + "]");
             }
         }
-        scanner.close ();
+        scanner.close();
 
-        return getResult ();
+        return getResult();
     }
 
-    private void handleOperand (char operand) {
-        while (!operantionStack.empty ()
-                && (priority (operantionStack.peek () ) >= priority (operand))) {
-            moveTheLastOperation ();
+    private void handleOperand(char operand) {
+        while (!operantionStack.empty()
+                && (priority(operantionStack.peek()) >= priority(operand))) {
+            moveTheLastOperation();
         }
-        operantionStack.push (operand);
+        operantionStack.push(operand);
     }
 
-    private int priority (char operand) {
+    private int priority(char operand) {
         int result = -1;
 
         if ((operand == '+') || (operand == '-')) {
             result = 0;
-        } else if (( operand == '*') || (operand == '/')) {
+        } else if ((operand == '*') || (operand == '/')) {
             result = 1;
         }
         return result;
     }
 
-    private void handleNumber (int number) {
-        numberStack.push (number);
+    private void handleNumber(int number) {
+        numberStack.push(number);
     }
 
-    private void handleBrackets (char operand)  {
+    private void handleBrackets(char operand) {
         if (operand == '(')
-            operantionStack.push (operand);
+            operantionStack.push(operand);
         else if (operand == ')') {
             // pop everything out of stack until find the '('
-            while (operantionStack.peek () != '(') {
-                moveTheLastOperation ();
+            while (operantionStack.peek() != '(') {
+                moveTheLastOperation();
             }
-            operantionStack.pop ();
+            operantionStack.pop();
         }
     }
 
-    private void moveTheLastOperation () throws IllegalArgumentException {
-        int a = numberStack.pop ();
-        int b = numberStack.pop ();
+    private void moveTheLastOperation() throws IllegalArgumentException {
+        int a = numberStack.pop();
+        int b = numberStack.pop();
 
         int result = 0;
-        char operand = operantionStack.pop ();
-        ExpressionCheck.operationOverflowCheck (b,a,operand);
+        char operand = operantionStack.pop();
+        ExpressionCheck.operationOverflowCheck(b, a, operand);
 
         switch (operand) {
-            case '+' : {
+            case '+': {
                 result = b + a;
                 break;
             }
-            case '-' : {
+            case '-': {
                 result = b - a;
                 break;
             }
-            case '*' : {
+            case '*': {
                 result = b * a;
                 break;
             }
-            case '/' : {
+            case '/': {
                 result = b / a;
                 break;
             }
@@ -113,45 +113,52 @@ public class StackCalculator {
             }
         }
 
-        numberStack.push (result);
+        numberStack.push(result);
 
     }
 
-    boolean expectedNumber () {
+    boolean expectedNumber() {
         return nextIsNumber;
     }
 
-    boolean expectedOperand () {
+    boolean expectedOperand() {
         return !nextIsNumber;
     }
 
-    void changeExpectation () {
+    void changeExpectation() {
         nextIsNumber = !nextIsNumber;
     }
 
-    int getResult () {
-        return numberStack.pop ();
+    int getResult() {
+        return numberStack.pop();
     }
 
-    public static void main (String [] args) {
-        StringBuilder sb = new StringBuilder ();
-        for (String s : args) {
-            sb.append (s);
-            sb.append (" ");
-        }
-        String expression = sb.toString ();
-        ExpressionCheck.bracketBalanceCheck (expression);
-        ExpressionCheck.invalidExpressionCheck (expression);
+    public static void main(String[] args) {
 
-        StackCalculator sc = new StackCalculator ();
-        try {
-            int result = sc.calculateExpression (expression);
-            System.out.println (Integer.toString (result,sc.RADIX));
-        } catch (IllegalArgumentException iae) {
-            System.err.println (iae.getMessage());
+        if (args.length > 0) {
+            StringBuilder sb = new StringBuilder();
+            for (String s : args) {
+                sb.append(s);
+                sb.append(" ");
+            }
+            String expression = sb.toString();
+            ExpressionCheck.bracketBalanceCheck(expression);
+            ExpressionCheck.invalidExpressionCheck(expression);
+
+            StackCalculator sc = new StackCalculator();
+            try {
+                int result = sc.calculateExpression(expression);
+                System.out.println(Integer.toString(result, sc.RADIX));
+            } catch (IllegalArgumentException iae) {
+                System.err.println(iae.getMessage());
+                System.exit(1);
+            }
+            System.exit(0);
+        } else {
+            System.out.println ("This is Calculator. Give it arguments like '16 - 3 * (5 + 5)'");
             System.exit (1);
         }
-        System.exit (0);
+
     }
 
 }
