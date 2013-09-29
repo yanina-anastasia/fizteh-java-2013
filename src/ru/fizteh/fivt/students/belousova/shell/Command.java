84a1744406631e5259f24e988543083a2ca9ccfe
abelousova <sashbel@inbox.ru>
2013-09-29 23:18:03 +0400

    Delete Command.java

--- src/ru/fizteh/fivt/students/belousova/shell/Command.java
+++
@@ -1,8 +0,0 @@
-package ru.fizteh.fivt.students.belousova.shell;
-
-import java.io.IOException;
-
-public interface Command {
-    public String getName();
-    public void execute(String args) throws IOException;
-}
