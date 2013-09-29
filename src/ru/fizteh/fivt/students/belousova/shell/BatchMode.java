beed0371cee24f8cb9071b2cad7de832428e7624
abelousova <sashbel@inbox.ru>
2013-09-29 23:17:52 +0400

    Delete BatchMode.java

--- src/ru/fizteh/fivt/students/belousova/shell/BatchMode.java
+++
@@ -1,9 +0,0 @@
-package ru.fizteh.fivt.students.belousova.shell;
-
-public class BatchMode {
-    public static void work(String[] args) {
-        for (String s : args) {
-            StringHandler.handle(s);
-        }
-    }
-}
