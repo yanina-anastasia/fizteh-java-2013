aaaa52ae8207edf8b54008b5f679f3173de5ca86
Elena Vorobyeva <good-mark@mail.ru>
2013-10-02 01:06:51 +0400

    fixed

--- src/ru/fizteh/fivt/students/elenav/calc/Calculator.java
+++ src/ru/fizteh/fivt/students/elenav/calc/Calculator.java
@@ -24,7 +24,6 @@ public class Calculator {
         int i = 0;
         while(i < inputString.length()) {
             char token = inputString.charAt(i);
-            //System.out.println(token);
             if((token >= '0' && token <='9') || (token >= 'A' && token <= 'G')) {
                 sb.append(token);
                 ++i;
@@ -72,62 +71,65 @@ public class Calculator {
     }
 	
 	public static int getNumber(String convertString, int i) {
-	        int num = 0;
-	        int length = convertString.length()-1;
-	        while(i < length && convertString.charAt(i) != ' ') {
-	            if(convertString.charAt(i) >= '0' && convertString.charAt(i) <= '9') {
-	                num = num * 17 + convertString.charAt(i) - '0';
-	            }
-	            else {
-	                num = num * 17 + convertString.charAt(i) - 'A' + 10;
-	            }
-	            ++i;
-	        }
-	        return num;
+		int length = convertString.length()-1;
+        StringBuilder sb = new StringBuilder();
+	    while (i < length && convertString.charAt(i) != ' ') {
+	        sb.append(convertString.charAt(i));
+	        ++i;
 	    }
+	    return Integer.parseInt(sb.toString(), 17);
+	}
 	
     public static int calculate(String convertString) throws IOException {
         LinkedList<Integer> numbers = new LinkedList<>();
         for (int i = 0; i < convertString.length(); ++i) {
             char token = convertString.charAt(i);
             if((token >= '0' && token <='9') || (token >= 'A' && token <= 'G')) {
-                int temp = getNumber(convertString, i);
+                try {
+                	numbers.push(getNumber(convertString, i));
+                } catch (NumberFormatException e) {
+                	throw new IOException("Int overflow");
+                }
                 while(convertString.charAt(i) != ' ')
                 	++i;
-                numbers.push(temp);
                 continue;
             }
             if(numbers.size() < 2 && priority(token) != -1)
-            	throw new IOException("invalid input");
+            	throw new IOException("Invalid input");
             if(token == '+') {
             	int x1 = numbers.pop();
             	int x2 = numbers.pop();
             	if(Integer.MAX_VALUE - x1 < x2) {
-            		throw new IOException("more than max int");
+            		throw new IOException("Int overflow");
             	}
                 numbers.push(x1 + x2);
                 continue;
             }
             if(token == '/') {
                 int temp = numbers.pop();
+                if (temp == 0) {
+                	throw new IOException("Division by zero");
+                }
                 temp =  numbers.pop() / temp;
                 numbers.push(temp);
                 continue;
             }
-            if(token == '-') {
+            if (token == '-') {
             	int x1 = numbers.pop();
             	int x2 = numbers.pop();
             	if( Integer.MIN_VALUE + x1 > x2) {
-            		throw new IOException("more than max int");
+            		throw new IOException("Int overflow");
             	}
                numbers.push(x2 - x1);
                 continue;
             }
-            if(token == '*') {
+            if (token == '*') {
             	int x1 = numbers.pop();
             	int x2 = numbers.pop();
-            	if( Integer.MAX_VALUE / x1 < x2) {
-            		throw new IOException("more than max int");
+            	if (x1 != 0) {
+            		if (Integer.MAX_VALUE / x1 < x2) {
+            		    throw new IOException("Int overflow");
+            	    }
             	}
                 numbers.push(x1 * x2);
                 continue;
@@ -138,15 +140,17 @@ public class Calculator {
 
 	public static void main(String[] args) throws IOException {
 		StringBuilder sb = new StringBuilder();
+		if (args.length == 0) {
+			System.out.println("Usage");
+			System.exit(1);
+		}
 		for (String s : args) {
 			sb.append(s);
 		}
-		String expression = sb.toString(); 
-		System.out.println(expression);
+		String expression = sb.toString();
 		
-		try{
+		try {
 			String convertString = toPostfix(expression);
-			//System.out.println(convertString);
 			int result = calculate(convertString);
 			String outStr = Integer.toString(result, 17);
 			System.out.println(outStr);
