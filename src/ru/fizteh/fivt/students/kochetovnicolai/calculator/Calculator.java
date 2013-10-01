bd952b3e82b71711fb424a6fc6d2cde8f7b4c420
Николай Кочетов <kochetovnicolai@gmail.com>
2013-10-01 14:58:01 +0400

    Updated Calculator.java

--- src/ru/fizteh/fivt/students/kochetovnicolai/calculator/Calculator.java
+++ src/ru/fizteh/fivt/students/kochetovnicolai/calculator/Calculator.java
@@ -3,11 +3,11 @@
 package ru.fizteh.fivt.students.kochetovnicolai.calculator;
 
 import java.util.Stack;
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
-    
+
     private static int nextNumber;
     private static int position;
 
@@ -22,12 +22,12 @@ public class Calculator {
         }
         System.err.println();
     }
-    
+
     private static void terminate(String error) {
         System.err.println(error);
         System.exit(1);
     }
-    
+
     private static int calculate(String operations, String[] numbers) {
         Stack<Integer> values = new Stack<>();
         values.push(0);
@@ -49,7 +49,7 @@ public class Calculator {
             } else {
                 value = calculate(operations, numbers);
             }
-            
+
             if (operation == '+') {
                 values.push(value);
             } else if (operation == '-') {
@@ -65,10 +65,10 @@ public class Calculator {
                     printSubExpression(operations, numbers);
                     terminate("Division by zero");
                 }
-                Integer previos = (Integer) values.pop();
-                values.push(previos / value);
+                Integer previous = values.pop();
+                values.push(previous / value);
             }
-            
+
             position++;
             if (position == operations.length()) {
                 terminate("Unexpected end of expression. Expected +, -, *, / or )");
@@ -76,52 +76,53 @@ public class Calculator {
             if (operations.charAt(position) == ')') {
                 break;
             }
-            
+
             if (!"-+*/".contains(operations.subSequence(position, position + 1))) {
                 printSubExpression(operations, numbers);
-                terminate("Expected operation +, -, * or / after constant, but \'" + 
+                terminate("Expected operation +, -, * or / after constant, but \'" +
                         operations.charAt(position) + "\' found");
             }
             operation = operations.charAt(position);
-            
+
             position++;
             if (position == operations.length()) {
                 terminate("Unexpected end of expression. Expected constant or ( ");
             }
         }
-        Long result = new Long(0);
+        long result = (long) 0;
         while (!values.empty()) {
             result += values.pop();
             if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
                 terminate("result of an expression out of range");
             }
         }
-        return result.intValue();
+        return (int) result;
     }
-        
+
     public static void main(String[] args) {
-        
-        if(args.length == 0)
+
+        if (args.length == 0) {
             terminate("Usage: java Calculator [arithmetic expression]");
-        
+        }
+
         StringBuilder builder = new StringBuilder("(");
-        for (int i = 0; i < args.length; i++) {
-            builder.append(args[i]);
+        for (String arg : args) {
+            builder.append(arg);
             builder.append(" ");
         }
         builder.append(")");
         String expression = builder.toString();
-        
+
         Matcher matcher = Pattern.compile(".*[0-9A-Ha-h] +[0-9A-Ha-h].*").matcher(expression);
         if (matcher.find()) {
             System.err.println(expression.substring(1, matcher.start() + 4));
             terminate("Expected operation +, -, * or / after constant, but \' \' found");
         }
-                
+
         expression = expression.replace(" ", "");
         String[] numbers = expression.split("[^0-9A-Ha-h]+");
         String operations = expression.replaceAll("[0-9a-hA-H]+", " ");
-        
+
         position = 0;
         nextNumber = 1;
         System.out.println(Integer.toString(calculate(operations, numbers), 18));
