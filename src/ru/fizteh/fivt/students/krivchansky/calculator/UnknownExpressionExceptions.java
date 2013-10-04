2d08f5926891fb2a136224cf6d625bc1c87d9862
isItJavaOrSomething <nikrivchanskii@yandex.ru>
2013-10-04 22:15:05 +0400

    Переназвал переменные, класс исключений. Настроил Eclipse и, нашёл случайно затесавшийся ! который приводил к ошибке.

---
+++ src/ru/fizteh/fivt/students/krivchansky/calculator/UnknownExpressionExceptions.java
@@ -0,0 +1,14 @@
+package ru.fizteh.fivt.students.krivchansky.calculator
+
+class UnknownExpressionExceptions extends Exception {
+    public UnknownExpressionExceptions() {
+        super();
+    }
+    public UnknownExpressionExceptions(String errorDescription){
+        super(errorDescription);
+    }
+    public UnknownExpressionExceptions(Throwable except) {
+        super(except);
+    }
+
+}
