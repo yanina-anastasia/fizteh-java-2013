aa8bc729abb64388f9f67305e9224c4e46904e46
Quadrocube <gliderok@gmail.com>
2013-10-05 17:51:25 +0400

    Calculator rev0.1. Fixed spacing, div by zero and imports

--- src/ru/fizteh/fivt/students/sterzhanovVladislav/calculator/ExpressionNotationUtility.java
+++ src/ru/fizteh/fivt/students/sterzhanovVladislav/calculator/ExpressionNotationUtility.java
@@ -8,67 +8,67 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ExpressionNotationUtility {
-	
-	public static Queue<String> convertToReversePolishNotation(String infixExpression, String validNumMatcher, String validOperatorMatcher) throws IllegalArgumentException {
-		/* Initialize buffers */
-		Queue<String> converted = new LinkedList<String>();
-		Stack<String> operatorStack = new Stack<String>();
+    
+    public static Queue<String> convertToReversePolishNotation(String infixExpression, String validNumMatcher, String validOperatorMatcher) throws IllegalArgumentException {
+        /* Initialize buffers */
+        Queue<String> converted = new LinkedList<String>();
+        Stack<String> operatorStack = new Stack<String>();
 
-		/* Handle unary minuses */
-		infixExpression = infixExpression.replace(" ", "").replace("(-", "(0-");
-		if (infixExpression.charAt(0) == '-') {
-			infixExpression = "0" + infixExpression;
-		}
-		
-		/* Initialize regex */
-		String capturingRegex = "(" + validNumMatcher + "|" + validOperatorMatcher + ")";
-		Matcher nextToken = Pattern.compile(capturingRegex).matcher(infixExpression);
-		
-		/* Shunting-yard implementation */
-		while (nextToken.find())
-		{
-			String token = nextToken.group(1);
-			if (token.equals("(")) {
-				operatorStack.push("(");
-			} else if (token.equals(")")) {
-				while (!operatorStack.empty() && !operatorStack.peek().equals("(")) {
-					converted.add(operatorStack.pop());
-				}
-				if (!operatorStack.empty()) {
-					operatorStack.pop();
-				} else {
-					throw new IllegalArgumentException("Unmatched parenthesis");
-				}
-			} else if (token.matches(validOperatorMatcher)) {
-				while (!operatorStack.empty() && (precedence(token) <= precedence(operatorStack.peek()))) {
-					converted.add(operatorStack.pop());
-				}
-				operatorStack.push(token);
-			} else {
-				converted.add(token);
-			}
-		}
-		infixExpression = infixExpression.replaceAll(capturingRegex, "");
-		if (!infixExpression.isEmpty()) {
-			throw new IllegalArgumentException("Illegal symbols found");
-		}
-		
-		while (!operatorStack.isEmpty()) {
-			if (operatorStack.peek().equals("(")) {
-				throw new IllegalArgumentException("Unmatched parenthesis");
-			}
-			converted.add(operatorStack.pop());
-		}
-		return converted;
-	}
-	
-	private static int precedence(String operator) {
-		if (operator.equals("-") || operator.equals("+")) {
-			return 1;
-		} else if (operator.equals("*") || operator.equals("/")) {
-			return 2;
-		} else {
-			return 0;
-		}
-	}
+        /* Handle unary minuses */
+        infixExpression = infixExpression.replace(" ", "").replace("(-", "(0-");
+        if (infixExpression.charAt(0) == '-') {
+            infixExpression = "0" + infixExpression;
+        }
+        
+        /* Initialize regex */
+        String capturingRegex = "(" + validNumMatcher + "|" + validOperatorMatcher + ")";
+        Matcher nextToken = Pattern.compile(capturingRegex).matcher(infixExpression);
+        
+        /* Shunting-yard implementation */
+        while (nextToken.find())
+        {
+            String token = nextToken.group(1);
+            if (token.equals("(")) {
+                operatorStack.push("(");
+            } else if (token.equals(")")) {
+                while (!operatorStack.empty() && !operatorStack.peek().equals("(")) {
+                    converted.add(operatorStack.pop());
+                }
+                if (!operatorStack.empty()) {
+                    operatorStack.pop();
+                } else {
+                    throw new IllegalArgumentException("Unmatched parenthesis");
+                }
+            } else if (token.matches(validOperatorMatcher)) {
+                while (!operatorStack.empty() && (precedence(token) <= precedence(operatorStack.peek()))) {
+                    converted.add(operatorStack.pop());
+                }
+                operatorStack.push(token);
+            } else {
+                converted.add(token);
+            }
+        }
+        infixExpression = infixExpression.replaceAll(capturingRegex, "");
+        if (!infixExpression.isEmpty()) {
+            throw new IllegalArgumentException("Illegal symbols found");
+        }
+        
+        while (!operatorStack.isEmpty()) {
+            if (operatorStack.peek().equals("(")) {
+                throw new IllegalArgumentException("Unmatched parenthesis");
+            }
+            converted.add(operatorStack.pop());
+        }
+        return converted;
+    }
+    
+    private static int precedence(String operator) {
+        if (operator.equals("-") || operator.equals("+")) {
+            return 1;
+        } else if (operator.equals("*") || operator.equals("/")) {
+            return 2;
+        } else {
+            return 0;
+        }
+    }
 }
