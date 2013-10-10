a902c741deb6cad59e4fa3c693a79f80f3e54e77
Elena Vorobyeva <good-mark@mail.ru>
2013-10-11 00:51:28 +0400

    Убрала спецификатор public из определения методов внутри интерфейса

--- src/ru/fizteh/fivt/students/elenav/shell/Command.java
+++ src/ru/fizteh/fivt/students/elenav/shell/Command.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 public interface Command {
-	public String getName();
-	public int getArgNumber();
-	public void setName(String name);
-	public void setArgNumber(int number);
-	public void setShell(ShellState s);
-	public File getWorkingDirectory();
-	public void setWorkingDirectory(File f);
-	public void execute(String args[], PrintStream s) throws IOException;
+	String getName();
+	int getArgNumber();
+	void setName(String name);
+	void setArgNumber(int number);
+	void setShell(ShellState s);
+	File getWorkingDirectory();
+	void setWorkingDirectory(File f);
+	void execute(String args[], PrintStream s) throws IOException;
 }
