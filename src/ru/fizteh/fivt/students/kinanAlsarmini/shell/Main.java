096c2586aba62cf865c3d8fe164374996451c5bc
Sarkin <kinan.sarmini@gmail.com>
2013-10-11 22:21:12 +0400

    Fix

---
+++ src/ru/fizteh/fivt/students/kinanAlsarmini/shell/Main.java
@@ -0,0 +1,21 @@
+package ru.fizteh.fivt.students.kinanAlsarmini.shell;
+
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Shell shell = new Shell();
+
+        if (args.length == 0) {
+            shell.startInteractive();
+        } else {
+            StringBuilder commands = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                commands.append(args[i] + " ");
+            }
+
+            shell.runCommands(commands.toString());
+        }
+    }
+}
+
---
+++ src/ru/fizteh/fivt/students/kinanAlsarmini/shell/RemoveCommand.java
@@ -0,0 +1,35 @@
+package ru.fizteh.fivt.students.kinanAlsarmini.shell;
+
+import java.io.File;
+
+class RemoveCommand extends ExternalCommand {
+    public RemoveCommand() {
+        super("rm", 1);
+    }
+
+    private void recursiveRemove(File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+
+            for (File f: files) {
+                if (f.isDirectory())  {
+                    recursiveRemove(f);
+                } else {
+                    f.delete();
+                }
+            }
+        }
+
+        file.delete();
+    }
+
+    public void execute(String[] args, Shell shell) {
+        File target = Utilities.getAbsoluteFile(args[0], shell.getCurrentPath());
+
+        if (!target.exists()) {
+            throw new IllegalArgumentException("rm: target file / directory doesn't exist.");
+        }
+
+        recursiveRemove(target);
+    }
+}
