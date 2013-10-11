df4f09ab2ad73252325c864aac01a54476b9d4e4
Dmitry Zvonarev <dmitry.zvonorev@phystech.edu>
2013-10-11 20:14:07 +0400

    Shell v3, Zvonarev Dmitry 294

--- src/ru/fizteh/fivt/students/dzvonarev/shell/Main.java
+++ src/ru/fizteh/fivt/students/dzvonarev/shell/Main.java
@@ -19,7 +19,8 @@ class Main {
     public static String mergeAll(String[] arr) {
         StringBuilder s = new StringBuilder();
         for (int i = 0; i < arr.length; i++) {
-            s.append(arr[i].trim());
+            s.append(arr[i]);
+            s.append(" ");
         }
         return s.toString();
     }
