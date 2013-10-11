f0db2c229684f6e3e618db220927691d0380caea
Nadezhda94 <amdnady@yandex.ru>
2013-10-11 14:44:06 +0400

    New commit

--- src/ru/fizteh/fivt/students/nadezhdakaratsapova/shell/CurrentDirectory.java
+++ src/ru/fizteh/fivt/students/nadezhdakaratsapova/shell/CurrentDirectory.java
@@ -3,13 +3,13 @@ package ru.fizteh.fivt.students.nadezhdakaratsapova.shell;
 import java.io.File;
 
 public class CurrentDirectory {
-    private File curDir = new File("");
+    private File curDir = new File("").getAbsoluteFile();
 
     File getCurDir() {
         return curDir;
     }
 
-    File changeCurDir (File newDir) {
+    File changeCurDir(File newDir) {
         curDir = newDir;
         return curDir;
     }
--- src/ru/fizteh/fivt/students/nadezhdakaratsapova/shell/DirCommand.java
+++ src/ru/fizteh/fivt/students/nadezhdakaratsapova/shell/DirCommand.java
@@ -2,6 +2,7 @@ package ru.fizteh.fivt.students.nadezhdakaratsapova.shell;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class DirCommand implements Command {
     public String getName() {
@@ -10,22 +11,29 @@ public class DirCommand implements Command {
 
     public void execute(CurrentDirectory currentDirectory, String[] args) {
         if (currentDirectory.getCurDir().isDirectory()) {
-            String[] fileList= currentDirectory.getCurDir().list();
-            Arrays.sort(fileList);
+            System.out.println(currentDirectory.getCurDir());
+            File[] fileList = currentDirectory.getCurDir().getAbsoluteFile().listFiles();
+            Arrays.sort(fileList, new Comparator<File>() {
+                @Override
+                public int compare(File o1, File o2) {
+                    return o1.getName().compareTo(o2.getName());
+                }
+            });
             if (fileList.length > 0) {
                 int i = 0;
-                while (fileList[i].charAt(0) == '.') {
+                while (fileList[i].getName().charAt(0) == ('.')) {
                     ++i;
                 }
                 int length = fileList.length;
 
                 for (int j = i; j < length; ++j) {
-                    System.out.println(fileList[j]);
+                    System.out.println(fileList[j].getName());
                 }
             }
 
         }
     }
+
     public int getArgsCount() {
         return 0;
     }
