d9b551a1029054d0098ddca95a994d957fec9068
Quadrocube <gliderok@gmail.com>
2013-10-12 15:50:13 +0400

    Fixed spacing

--- src/ru/fizteh/fivt/students/sterzhanovVladislav/shell/Command.java
+++ src/ru/fizteh/fivt/students/sterzhanovVladislav/shell/Command.java
@@ -14,114 +14,114 @@ import java.io.IOException;
 import java.util.EnumSet;
 
 public abstract class Command {
-	protected Shell parentShell;
-	abstract public Command newCommand();
-	abstract public void innerExecute() throws Exception, IOException;
-	
-	private int argc;
-	public String[] args;
-	
-	void execute() throws IllegalArgumentException, Exception {
-		if (argc != args.length) {
-			throw new IllegalArgumentException("Unable to handle " + args.length + " arguments");
-		}
-		if (parentShell == null) {
-			throw new Exception("Cannot execute a command without a shell attached");
-		}
-		innerExecute();
-	}
-	
-	Command() {
-	}
-	
-	Command(String[] newArgs) {
-		args = newArgs.clone();
-	}
-	
-	Command setArgc(int argCount) {
-		argc = argCount;
-		return this;
-	}
-	
-	Command setShell(Shell newShell) {
-		parentShell = newShell;
-		return this;
-	}
-	
-	public static class Cd extends Command {
-		@Override
-		public void innerExecute() throws Exception {
-			Path newPath = Paths.get(args[1]);
+    protected Shell parentShell;
+    abstract public Command newCommand();
+    abstract public void innerExecute() throws Exception, IOException;
+    
+    private int argc;
+    public String[] args;
+    
+    void execute() throws IllegalArgumentException, Exception {
+        if (argc != args.length) {
+            throw new IllegalArgumentException("Unable to handle " + args.length + " arguments");
+        }
+        if (parentShell == null) {
+            throw new Exception("Cannot execute a command without a shell attached");
+        }
+        innerExecute();
+    }
+    
+    Command() {
+    }
+    
+    Command(String[] newArgs) {
+        args = newArgs.clone();
+    }
+    
+    Command setArgc(int argCount) {
+        argc = argCount;
+        return this;
+    }
+    
+    Command setShell(Shell newShell) {
+        parentShell = newShell;
+        return this;
+    }
+    
+    public static class Cd extends Command {
+        @Override
+        public void innerExecute() throws Exception {
+            Path newPath = Paths.get(args[1]);
             newPath = parentShell.getAbsolutePath(newPath);
             if (newPath.toFile().isDirectory()) {
                 parentShell.setWorkingDir(newPath);
             } else {
                 throw new Exception("No such file or directory");
             }
-		}
-		@Override
-		public Command newCommand() {
-			return new Cd();
-		}
-		Cd() {
-			setArgc(2);
-		}
-		Cd (String... args) {
-			super(args);
-			setArgc(2);
-		}
-	}
-	
-	public static class Mkdir extends Command {
-		@Override
-		public void innerExecute() throws Exception {
-			Path newPath = Paths.get(args[1]);
+        }
+        @Override
+        public Command newCommand() {
+            return new Cd();
+        }
+        Cd() {
+            setArgc(2);
+        }
+        Cd (String... args) {
+            super(args);
+            setArgc(2);
+        }
+    }
+    
+    public static class Mkdir extends Command {
+        @Override
+        public void innerExecute() throws Exception {
+            Path newPath = Paths.get(args[1]);
             newPath = parentShell.getAbsolutePath(newPath);
             if (!newPath.toFile().mkdir()) {
                 throw new Exception("Unable to create a directory");
             }
-		}
-		@Override
-		public Command newCommand() {
-			return new Mkdir();
-		}
-		Mkdir() {
-			setArgc(2);
-		}
-		Mkdir (String... args) {
-			super(args);
-			setArgc(2);
-		}
-	}
-	
-	public static class Pwd extends Command {
-		@Override
-		public void innerExecute() {
-			parentShell.out.println(parentShell.getWorkingDir());
-		}
-		@Override
-		public Command newCommand() {
-			return new Pwd();
-		}
-		Pwd() {
-			setArgc(1);
-		}
-		Pwd (String... args) {
-			super(args);
-			setArgc(1);
-		}
-	}
-	
-	public static class Rm extends Command {
-		@Override
-		public void innerExecute() throws IOException, Exception {
-			Path path = Paths.get(args[1]);
+        }
+        @Override
+        public Command newCommand() {
+            return new Mkdir();
+        }
+        Mkdir() {
+            setArgc(2);
+        }
+        Mkdir (String... args) {
+            super(args);
+            setArgc(2);
+        }
+    }
+    
+    public static class Pwd extends Command {
+        @Override
+        public void innerExecute() {
+            parentShell.out.println(parentShell.getWorkingDir());
+        }
+        @Override
+        public Command newCommand() {
+            return new Pwd();
+        }
+        Pwd() {
+            setArgc(1);
+        }
+        Pwd (String... args) {
+            super(args);
+            setArgc(1);
+        }
+    }
+    
+    public static class Rm extends Command {
+        @Override
+        public void innerExecute() throws IOException, Exception {
+            Path path = Paths.get(args[1]);
             path = parentShell.getAbsolutePath(path);
             if (path.toFile().isFile()) {
                 Files.delete(path);
             } else {
                 if (Shell.isSubdirectory(parentShell.getWorkingDir(), path) 
-                		|| parentShell.getWorkingDir().equals(path)) {
+                        || parentShell.getWorkingDir().equals(path)) {
                     throw new Exception("Unable to remove: directory in use");
                 }
                 Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
@@ -142,23 +142,23 @@ public abstract class Command {
                     }
                 });
             }
-		}
-		@Override
-		public Command newCommand() {
-			return new Rm();
-		}
-		Rm() {
-			setArgc(2);
-		}
-		Rm (String... args) {
-			super(args);
-			setArgc(2);
-		}
-	}
-	
-	public static class Cp extends Command {
-		@Override
-		public void innerExecute() throws Exception, IOException{
+        }
+        @Override
+        public Command newCommand() {
+            return new Rm();
+        }
+        Rm() {
+            setArgc(2);
+        }
+        Rm (String... args) {
+            super(args);
+            setArgc(2);
+        }
+    }
+    
+    public static class Cp extends Command {
+        @Override
+        public void innerExecute() throws Exception, IOException{
             Path source = Paths.get(args[1]);
             Path destination = Paths.get(args[2]);
             source = parentShell.getAbsolutePath(source);
@@ -173,10 +173,10 @@ public abstract class Command {
                 }
                 Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
             } else if (source.toFile().isDirectory() && 
-            		(destination.toFile().isDirectory() || !destination.toFile().exists())) {
-            	if (!destination.toFile().exists()) {
-            		destination.toFile().mkdir();
-            	}
+                    (destination.toFile().isDirectory() || !destination.toFile().exists())) {
+                if (!destination.toFile().exists()) {
+                    destination.toFile().mkdir();
+                }
                 if (Shell.isSubdirectory(destination, source) || destination.equals(source)) {
                     throw new FileSystemLoopException("Unable to modify subdirectory of source dir");
                 }
@@ -201,81 +201,81 @@ public abstract class Command {
 
                         });
             } else if (!source.toFile().exists()) {
-            	throw new Exception("Source file does not exist");
+                throw new Exception("Source file does not exist");
             } else {
                 throw new Exception("Will not overwrite a file with a directory");
             }
-		}
-		@Override
-		public Command newCommand() {
-			return new Cp();
-		}
-		Cp () {
-			setArgc(3);
-		}
-		Cp (String... args) {
-			super(args);
-			setArgc(3);
-		}
-	}
-	
-	public static class Mv extends Command {
-		@Override
-		public void innerExecute() throws Exception {
-			Command cp = new Cp("cp", args[1], args[2]).setShell(parentShell);
-			Command rm = new Rm("rm", args[1]).setShell(parentShell);
-			cp.execute();
-			rm.execute();
-		}
-		@Override
-		public Command newCommand() {
-			return new Mv();
-		}
-		Mv() {
-			setArgc(3);
-		}
-		Mv(String... args) {
-			super(args);
-			setArgc(3);
-		}
-	}
-	
-	public static class Dir extends Command {
-		@Override
-		public void innerExecute() {
-			Path cwd = parentShell.getWorkingDir();
+        }
+        @Override
+        public Command newCommand() {
+            return new Cp();
+        }
+        Cp () {
+            setArgc(3);
+        }
+        Cp (String... args) {
+            super(args);
+            setArgc(3);
+        }
+    }
+    
+    public static class Mv extends Command {
+        @Override
+        public void innerExecute() throws Exception {
+            Command cp = new Cp("cp", args[1], args[2]).setShell(parentShell);
+            Command rm = new Rm("rm", args[1]).setShell(parentShell);
+            cp.execute();
+            rm.execute();
+        }
+        @Override
+        public Command newCommand() {
+            return new Mv();
+        }
+        Mv() {
+            setArgc(3);
+        }
+        Mv(String... args) {
+            super(args);
+            setArgc(3);
+        }
+    }
+    
+    public static class Dir extends Command {
+        @Override
+        public void innerExecute() {
+            Path cwd = parentShell.getWorkingDir();
             for (String fileName : cwd.toFile().list()) {
                 parentShell.out.println(fileName);
             }
-		}
-		@Override
-		public Command newCommand() {
-			return new Dir();
-		}
-		Dir() {
-			setArgc(1);
-		}
-		Dir (String... args) {
-			super(args);
-			setArgc(1);
-		}
-	}
-	
-	public static class Exit extends Command {
-		@Override
-		public void innerExecute() {
-			parentShell.exit(0);
-		}
-		@Override
-		public Command newCommand() {
-			return new Exit();
-		}
-		Exit() {
-			setArgc(1);
-		}
-		Exit (String... args) {
-			super(args);
-			setArgc(1);
-		}
-	}
+        }
+        @Override
+        public Command newCommand() {
+            return new Dir();
+        }
+        Dir() {
+            setArgc(1);
+        }
+        Dir (String... args) {
+            super(args);
+            setArgc(1);
+        }
+    }
+    
+    public static class Exit extends Command {
+        @Override
+        public void innerExecute() {
+            parentShell.exit(0);
+        }
+        @Override
+        public Command newCommand() {
+            return new Exit();
+        }
+        Exit() {
+            setArgc(1);
+        }
+        Exit (String... args) {
+            super(args);
+            setArgc(1);
+        }
+    }
 }
--- src/ru/fizteh/fivt/students/sterzhanovVladislav/shell/Shell.java
+++ src/ru/fizteh/fivt/students/sterzhanovVladislav/shell/Shell.java
@@ -8,51 +8,51 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Shell {
-	public PrintStream out = System.out;
-	public PrintStream err = System.err;
-	private Path workingDir;
-	
-	static HashMap<String, Command> cmdMap = new HashMap<String, Command>();
-	
-	static {
-		cmdMap.put("cd", new Command.Cd());
-		cmdMap.put("mkdir", new Command.Mkdir());
-		cmdMap.put("pwd", new Command.Pwd());
-		cmdMap.put("rm", new Command.Rm());
-		cmdMap.put("cp", new Command.Cp());
-		cmdMap.put("mv", new Command.Mv());
-		cmdMap.put("dir", new Command.Dir());
-		cmdMap.put("exit", new Command.Exit());
-	}
-	
-	public void execCommandStream(InputStream cmdStream, boolean doPrintPrompt) throws Exception {
-		Scanner cmdReader = new Scanner(cmdStream);
-		try {
-			maybePrintPrompt(doPrintPrompt);
-			while (cmdReader.hasNextLine()) {
-				try {
-					String[] cmdList = cmdReader.nextLine().split(";");
-					for (String cmdString : cmdList) {
-						Command cmd = parseNextCommand(cmdString);
-						if (cmd != null) {
-							cmd.execute();
-						}
-					}
-				} catch (Exception e) {
-					out.println(e.getMessage());
-				}
-				maybePrintPrompt(doPrintPrompt);
-			}
-		} finally {
-			cmdReader.close();
-		}
-	}
-	
-	public void exit(int exitCode) {
-		System.exit(exitCode);
-	}
-	
-	public Path getWorkingDir() {
+    public PrintStream out = System.out;
+    public PrintStream err = System.err;
+    private Path workingDir;
+    
+    static HashMap<String, Command> cmdMap = new HashMap<String, Command>();
+    
+    static {
+        cmdMap.put("cd", new Command.Cd());
+        cmdMap.put("mkdir", new Command.Mkdir());
+        cmdMap.put("pwd", new Command.Pwd());
+        cmdMap.put("rm", new Command.Rm());
+        cmdMap.put("cp", new Command.Cp());
+        cmdMap.put("mv", new Command.Mv());
+        cmdMap.put("dir", new Command.Dir());
+        cmdMap.put("exit", new Command.Exit());
+    }
+    
+    public void execCommandStream(InputStream cmdStream, boolean doPrintPrompt) throws Exception {
+        Scanner cmdReader = new Scanner(cmdStream);
+        try {
+            maybePrintPrompt(doPrintPrompt);
+            while (cmdReader.hasNextLine()) {
+                try {
+                    String[] cmdList = cmdReader.nextLine().split(";");
+                    for (String cmdString : cmdList) {
+                        Command cmd = parseNextCommand(cmdString);
+                        if (cmd != null) {
+                            cmd.execute();
+                        }
+                    }
+                } catch (Exception e) {
+                    out.println(e.getMessage());
+                }
+                maybePrintPrompt(doPrintPrompt);
+            }
+        } finally {
+            cmdReader.close();
+        }
+    }
+    
+    public void exit(int exitCode) {
+        System.exit(exitCode);
+    }
+    
+    public Path getWorkingDir() {
         return workingDir;
     }
 
@@ -75,28 +75,28 @@ public class Shell {
         }
         return false;
     }
-	
-	private Command parseNextCommand(String cmdLine) {
-		if (cmdLine.trim().isEmpty()) {
-			return null;
-		}
-		String[] tokens = cmdLine.trim().split("[\t ]+");
-		String cmdName = tokens[0];
-		if (!cmdMap.containsKey(cmdName)) {
-			throw new IllegalArgumentException("Illegal command");
-		}
-		Command cmd = cmdMap.get(cmdName).newCommand().setShell(this);
-		cmd.args = tokens;
-		return cmd;
-	}
-	
-	private void maybePrintPrompt(boolean doPrintPrompt) {
-		if (doPrintPrompt) {
-			out.print("$ ");
-		}
-	}
     
-	Shell() {
-		workingDir = Paths.get(System.getProperty("user.dir"));
-	}
+    private Command parseNextCommand(String cmdLine) {
+        if (cmdLine.trim().isEmpty()) {
+            return null;
+        }
+        String[] tokens = cmdLine.trim().split("[\t ]+");
+        String cmdName = tokens[0];
+        if (!cmdMap.containsKey(cmdName)) {
+            throw new IllegalArgumentException("Illegal command");
+        }
+        Command cmd = cmdMap.get(cmdName).newCommand().setShell(this);
+        cmd.args = tokens;
+        return cmd;
+    }
+    
+    private void maybePrintPrompt(boolean doPrintPrompt) {
+        if (doPrintPrompt) {
+            out.print("$ ");
+        }
+    }
+    
+    Shell() {
+        workingDir = Paths.get(System.getProperty("user.dir"));
+    }
 }
