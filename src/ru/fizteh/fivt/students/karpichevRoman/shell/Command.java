c8d4b1f75da1e0a0c349d7c7714f4859ea5f26ed
Sarkin <kinan.sarmini@gmail.com>
2013-10-12 16:03:41 +0400

    Commit of Kinan only

--- src/ru/fizteh/fivt/students/karpichevRoman/shell/Command.java
+++
@@ -1,6 +0,0 @@
-package ru.fizteh.fivt.students.karpichevRoman.shell;
-
-interface Command {
-    boolean isThatCommand(String command);
-    void run(Shell shell, String command) throws IllegalArgumentException;
-}
