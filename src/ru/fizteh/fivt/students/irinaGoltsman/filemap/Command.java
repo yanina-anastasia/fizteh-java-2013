dee6afc91ef017a59b2cf99a39e38e8b749ac339
irina-goltsman <irina-goltsman@mail.ru>
2013-10-16 10:42:08 +0400

    Убрала public у полей интерфейса.

--- src/ru/fizteh/fivt/students/irinaGoltsman/filemap/Command.java
+++ src/ru/fizteh/fivt/students/irinaGoltsman/filemap/Command.java
@@ -2,15 +2,14 @@ package ru.fizteh.fivt.students.irinaGoltsman.filemap;
 
 public interface Command {
     //Получить название команды
-    public String getName();
+    String getName();
 
     //Получить количество аргументов команды.
-    public int getCountOfArguments();
+    int getCountOfArguments();
 
     //Проверить аргументы.
-    public boolean check(String[] parts);
+    boolean check(String[] parts);
 
     //Выполнить команду.
-    public Code perform(String[] parts);
-
+    Code perform(String[] parts);
 }
