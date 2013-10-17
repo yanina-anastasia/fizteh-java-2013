93e64bbf03eb12f0b122ebca209e1e4b3b1d320f
dmitry_ivanovsky <dima-iv@mail.ru>
2013-10-17 22:52:14 +0400

    мучу fileMap

--- src/ru/fizteh/fivt/students/dmitryIvanovsky/fileMap/DbMain.java
+++ src/ru/fizteh/fivt/students/dmitryIvanovsky/fileMap/DbMain.java
@@ -45,5 +45,6 @@ public class DbMain {
             System.err.println("Ошибка выполнения");
             System.exit(1);
         }
+
     }
 }
--- src/ru/fizteh/fivt/students/dmitryIvanovsky/fileMap/MyFileMap.java
+++ src/ru/fizteh/fivt/students/dmitryIvanovsky/fileMap/MyFileMap.java
@@ -6,12 +6,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 
 import ru.fizteh.fivt.students.dmitryIvanovsky.shell.CommandAbstract;
 import ru.fizteh.fivt.students.dmitryIvanovsky.shell.CommandLauncher.Code;
-import ru.fizteh.fivt.students.dmitryIvanovsky.shell.CommandShell;
 
-public class MyFileMap implements CommandAbstract{
+public class MyFileMap implements CommandAbstract {
 
     private final RandomAccessFile dbFile;
     private final Path pathDbFile;
@@ -29,7 +29,7 @@ public class MyFileMap implements CommandAbstract{
         dbMap = new HashMap<String, String>();
     }
 
-    public MyFileMap(String db, Map dbMap) throws IOException {
+    public MyFileMap(String db, Map<String, String> dbMap) throws IOException {
         this.pathDbFile = Paths.get(db).resolve(nameTable);
         dbFile = new RandomAccessFile(pathDbFile.toFile(), "rw");
         this.dbMap = dbMap;
@@ -56,56 +56,80 @@ public class MyFileMap implements CommandAbstract{
         if (dbFile.length() == 0) {
             return;
         }
-
         dbFile.seek(0);
-        String key = dbFile.readUTF();
-        dbFile.readChar();
-        int separate = dbFile.readInt();
 
-        long point = dbFile.getFilePointer();
-        dbFile.seek(separate);
-        String value = dbFile.readUTF();
-        long point2 = dbFile.getFilePointer();
+        byte[] arrayByte;
+        Vector<Byte> vectorByte = new Vector<Byte>();
+        long separator = -1;
 
-        dbMap.put(key, value);
-        while (point < separate) {
-            dbFile.seek(point);
-            key = dbFile.readUTF();
-            dbFile.readChar();
-            dbFile.readInt();
-            point = dbFile.getFilePointer();
-            dbFile.seek(point2);
-            value = dbFile.readUTF();
-            point2 = dbFile.getFilePointer();
-            dbMap.put(key, value);
+        while (dbFile.getFilePointer() != dbFile.length()) {
+            byte currentByte = dbFile.readByte();
+            if (currentByte == '\0') {
+                int point1 = dbFile.readInt();
+                if (separator == -1) {
+                    separator = point1;
+                }
+                long currentPoint = dbFile.getFilePointer();
+
+                while (dbFile.getFilePointer() != separator) {
+                    if (dbFile.readByte() == '\0') {
+                        break;
+                    }
+                }
+
+                int point2;
+                if (dbFile.getFilePointer() == separator) {
+                    point2 = (int) dbFile.length();
+                } else {
+                    point2 = dbFile.readInt();
+                }
+
+                dbFile.seek(point1);
+
+                arrayByte = new byte[point2 - point1];
+                dbFile.readFully(arrayByte);
+                String value = new String(arrayByte, "UTF8");
+
+                arrayByte = new byte[vectorByte.size()];
+                for (int i = 0; i < vectorByte.size(); ++i) {
+                    arrayByte[i] = vectorByte.elementAt(i).byteValue();
+                }
+                String key = new String(arrayByte, "UTF8");
+
+                dbMap.put(key, value);
+
+                vectorByte.clear();
+                dbFile.seek(currentPoint);
+            } else {
+                vectorByte.add(currentByte);
+            }
         }
     }
 
     public void closeDbFile() throws IOException {
         dbFile.setLength(0);
         dbFile.seek(0);
-
         int len = 0;
-        for (Object key : dbMap.keySet()) {
-            len += key.toString().getBytes().length + 4 + 4;
+
+        for (String key : dbMap.keySet()) {
+            len += key.getBytes("UTF8").length + 1 + 4;
         }
-        for (Object key : dbMap.keySet()) {
-            dbFile.writeUTF(key.toString());
-            dbFile.writeChar('\0');
+
+        for (String key : dbMap.keySet()) {
+            dbFile.write(key.getBytes("UTF8"));
+            dbFile.writeByte(0);
             dbFile.writeInt(len);
-            len += dbMap.get(key).toString().getBytes().length + 2;
+
+            long point = dbFile.getFilePointer();
+
+            dbFile.seek(len);
+            String value = dbMap.get(key);
+            dbFile.write(value.getBytes("UTF8"));
+            len += value.getBytes("UTF8").length;
+
+            dbFile.seek(point);
         }
 
-        for (Object value : dbMap.values()) {
-            dbFile.writeUTF(value.toString());
-        }
-
-        long lengthFile = dbFile.length();
-        dbFile.close();
-        if (lengthFile == 0) {
-            CommandShell sys = new CommandShell(pathDbFile.toString());
-            sys.rm(new String[]{nameTable});
-        }
     }
 
     public String startShellString() {
