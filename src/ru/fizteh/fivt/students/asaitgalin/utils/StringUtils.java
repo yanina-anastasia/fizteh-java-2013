8cc863156c00a3d8f96140770ae30f48b55a3594
Andrey Saitgalin <andrey.saitgalin@gmail.com>
2013-10-17 20:07:39 +0400

    Убрал лишнее

--- src/ru/fizteh/fivt/students/asaitgalin/utils/StringUtils.java
+++ src/ru/fizteh/fivt/students/asaitgalin/utils/StringUtils.java
@@ -1,14 +1,11 @@
 package ru.fizteh.fivt.students.asaitgalin.utils;
 
-import java.nio.charset.Charset;
-import java.util.Collection;
-
 public class StringUtils {
 
     public static String join(Iterable<?> objects, String separator) {
         StringBuilder sb = new StringBuilder();
         boolean isFirst = true;
-        for (Object o: objects) {
+        for (Object o : objects) {
             if (!isFirst) {
                 sb.append(separator);
             } else {
@@ -17,15 +14,6 @@ public class StringUtils {
             sb.append(o.toString());
         }
         return sb.toString();
-    }
-
-    public static String getStringFromArray(Collection<Byte> arrayList, String charsetName) {
-        byte[] stringData = new byte[arrayList.size()];
-        int counter = 0;
-        for (Byte b : arrayList) {
-            stringData[counter++] = b.byteValue();
-        }
-        return new String(stringData, Charset.forName(charsetName));
     }
 
 }
