c46eaf849ec6a2e9138707756dd8f009b9b2a8f3
vlad-vorotilov <vlad.vorotilov@gmail.com>
2013-10-18 03:36:47 +0400

    add db

---
+++ src/ru/fizteh/fivt/students/vorotilov/db/DataBaseFile.java
@@ -0,0 +1,96 @@
+package ru.fizteh.fivt.students.vorotilov.db;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class DataBaseFile {
+
+    private RandomAccessFile dbFile;
+    private HashMap<String, byte[]> dbMap;
+
+    public DataBaseFile(File dbFilePath) throws IOException {
+        try {
+            if (!dbFilePath.exists()) {
+                if (!dbFilePath.createNewFile()) {
+                    throw new CantCreateDbFile(dbFilePath.getCanonicalPath());
+                }
+            }
+            dbFile = new RandomAccessFile(dbFilePath, "rw");
+            dbMap = new HashMap<>();
+            while (dbFile.getFilePointer() < dbFile.length() - 1) {
+                int keyLength = dbFile.readInt();
+                int valueLength = dbFile.readInt();
+                byte[] key = new byte[keyLength];
+                byte[] value = new byte[valueLength];
+                dbFile.read(key);
+                dbFile.read(value);
+                dbMap.put(new String(key), value);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("can't open data base file: '" + dbFilePath.getCanonicalPath() + "' file not found");
+        } catch (CantCreateDbFile e) {
+            System.out.println("can't create new db file: '" + e.getProblematicFile() + "'");
+        }
+    }
+
+    public void put(String newKey, byte[] newValue) throws IOException {
+        byte[] currentValue = dbMap.get(newKey);
+        if (currentValue == null) {
+            System.out.println("new");
+        } else {
+            System.out.println("overwrite");
+            System.out.println(new String(currentValue));
+        }
+        dbMap.put(newKey, newValue);
+    }
+
+    public byte[] get(String newKey) throws IOException {
+        byte[] currentValue = dbMap.get(newKey);
+        if (currentValue == null) {
+            System.out.println("not found");
+        } else {
+            System.out.println("found");
+            System.out.println(new String(currentValue));
+        }
+        return currentValue;
+    }
+
+    public byte[] remove(String newKey) throws IOException {
+        byte[] currentValue = dbMap.remove(newKey);
+        if (currentValue == null) {
+            System.out.println("not found");
+        } else {
+            System.out.println("removed");
+        }
+        return currentValue;
+    }
+
+    public void save() throws IOException {
+        Set<Map.Entry<String, byte[]>> dbSet = dbMap.entrySet();
+        Iterator<Map.Entry<String, byte[]>> i = dbSet.iterator();
+        dbFile.seek(0);
+        while (i.hasNext()) {
+            Map.Entry<String, byte[]> tempEntry = i.next();
+            dbFile.writeInt(tempEntry.getKey().length());
+            dbFile.writeInt(tempEntry.getValue().length);
+            dbFile.write(tempEntry.getKey().getBytes());
+            dbFile.write(tempEntry.getValue());
+        }
+        dbFile.setLength(dbFile.getFilePointer() + 1);
+    }
+
+    public void close() {
+        try {
+            save();
+            dbFile.close();
+        } catch (IOException e) {
+            System.out.println("can't close data base file");
+        }
+    }
+}
---
+++ src/ru/fizteh/fivt/students/vorotilov/db/FileMapMain.java
@@ -0,0 +1,83 @@
+package ru.fizteh.fivt.students.vorotilov.db;
+
+import ru.fizteh.fivt.students.vorotilov.shell.*;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileMapMain {
+    private static boolean interactiveMode;
+    private static DataBaseFile currentDataBase;
+    private static final String DEFAULT_FILE_NAME = "db.dat";
+
+    private static void processCommand(String[] parsedCommand) throws ExitCommand, IOException, WrongCommand {
+        try {
+            switch (parsedCommand[0]) {
+                case "exit":
+                    if (parsedCommand.length > 1) {
+                        System.out.println("exit: must not get parameter");
+                        throw new WrongCommand();
+                    } else {
+                        throw new ExitCommand();
+                    }
+                case "put":
+                    if (parsedCommand.length != 3) {
+                        System.out.println("put: must get 2 parameters");
+                        throw new WrongCommand();
+                    } else {
+                        currentDataBase.put(parsedCommand[1], parsedCommand[2].getBytes());
+                    }
+                    break;
+                case "get":
+                    if (parsedCommand.length != 2) {
+                        System.out.println("get: must get 1 parameter");
+                        throw new WrongCommand();
+                    } else {
+                        currentDataBase.get(parsedCommand[1]);
+                    }
+                    break;
+                case "remove":
+                    if (parsedCommand.length != 2) {
+                        System.out.println("remove: must get 1 parameter");
+                        throw new WrongCommand();
+                    } else {
+                        currentDataBase.remove(parsedCommand[1]);
+                    }
+                    break;
+                case "":
+                    throw new WrongCommand();
+                default:
+                    System.out.println("unknown command: '" + parsedCommand[0] + "'");
+                    throw new WrongCommand();
+            }
+        } catch (WrongCommand e) {
+            if (!interactiveMode) {
+                throw e;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        interactiveMode = (args.length == 0);
+        try {
+            File currentFile = new File(System.getProperty("fizteh.db.dir"), DEFAULT_FILE_NAME).getCanonicalFile();
+            currentDataBase = new DataBaseFile(currentFile);
+            ConsoleInput shellInputCommands;
+            if (interactiveMode) {
+                shellInputCommands = new InteractiveInput();
+            } else {
+                shellInputCommands = new PackageInput(args);
+            }
+            while (shellInputCommands.hasNext()) {
+                processCommand(shellInputCommands.getNext());
+            }
+        } catch (ExitCommand | NoNextCommand e) {
+            currentDataBase.close();
+            System.exit(0);
+        } catch (IOException | WrongCommand e) {
+            currentDataBase.close();
+            System.exit(1);
+        }
+    }
+
+}
