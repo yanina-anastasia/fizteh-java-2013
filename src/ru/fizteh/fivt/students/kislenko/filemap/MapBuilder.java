0e9ec1bd57cd00e31182ef0d78530d86e3d35a0d
ignorer <ignorer@bk.ru>
2013-10-17 19:55:14 +0400

    Fixed validation again.

--- src/ru/fizteh/fivt/students/kislenko/filemap/MapBuilder.java
+++ src/ru/fizteh/fivt/students/kislenko/filemap/MapBuilder.java
@@ -8,12 +8,9 @@ public class MapBuilder {
     private final long MAX_FILE_SIZE = 100000000;
 
     void buildMap(State state) throws IOException {
-        File checker = state.getPath().toFile();
-        if (!checker.exists()) {
-            throw new FileNotFoundException("File not found.");
-        }
         RandomAccessFile database = new RandomAccessFile(state.getPath().toFile(), "r");
         if (database.length() > MAX_FILE_SIZE) {
+            database.close();
             throw new IOException("Too big database file.");
         }
         int keyLength;
@@ -23,10 +20,12 @@ public class MapBuilder {
         while (database.getFilePointer() != database.length()) {
             keyLength = database.readInt();
             if (keyLength > database.length() - database.getFilePointer() + 4) {
+                database.close();
                 throw new IOException("Incorrect key length in input.");
             }
             valueLength = database.readInt();
             if (valueLength > database.length() - database.getFilePointer() + 4) {
+                database.close();
                 throw new IOException("Incorrect value length in input.");
             }
             byte[] keySymbols = new byte[keyLength];
@@ -46,6 +45,7 @@ public class MapBuilder {
         Set<String> keySet = state.getMap().keySet();
         for (String key : keySet) {
             if (database.getFilePointer() > MAX_FILE_SIZE) {
+                database.close();
                 throw new IOException("Too big database file.");
             }
             database.writeInt(key.getBytes(StandardCharsets.UTF_8).length);
