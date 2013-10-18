e53fdfcc6ce38189ab1c42123c48cb927c92b668
RyabovaMaria <liastramv@gmail.com>
2013-10-19 01:01:37 +0400

    Рябова Мария, 295, FileMap Update

--- src/ru/fizteh/fivt/students/ryabovaMaria/fileMap/FileMap.java
+++ src/ru/fizteh/fivt/students/ryabovaMaria/fileMap/FileMap.java
@@ -17,57 +17,62 @@ public class FileMap {
         File dbFile = currentDir.toPath().resolve("db.dat").toFile();
         RandomAccessFile db;
         db = new RandomAccessFile(dbFile, "r");
-        long curPointer = 0;
-        long lastPointer = 0;
-        long length = db.length();
-        if (length == 0) {
-            return;
-        }
-        db.seek(0);
-        String lastKey = "";
-        int lastOffset = 0;
-        while (curPointer < length) {
-            byte curByte = db.readByte();
-            if (curByte == '\0') {
-                byte[] byteKey = new byte[(int) curPointer - (int) lastPointer];
-                curPointer = db.getFilePointer();
-                db.seek(lastPointer);
-                db.readFully(byteKey);
-                db.seek(curPointer);
-                String currentKey = new String(byteKey, "UTF-8");
-                int offset = db.readInt();
-                if (!lastKey.isEmpty()) {
-                    byte[] byteValue = new byte[offset - lastOffset];
-                    curPointer = db.getFilePointer();
-                    db.seek(lastOffset);
-                    db.readFully(byteValue);
-                    String lastValue = new String(byteValue, "UTF-8");
-                    db.seek(curPointer);
-                    if (loadList.containsKey(lastKey)) {
-                        System.err.println(lastKey + " meets twice in db.dat");
-                        System.exit(1);
-                    }
-                    loadList.put(lastKey, lastValue);
-                }
-                lastOffset = offset;
-                lastKey = currentKey;
-                lastPointer = db.getFilePointer();
+        try {
+            long curPointer = 0;
+            long lastPointer = 0;
+            long length = db.length();
+            if (length == 0) {
+                return;
             }
-            curPointer = db.getFilePointer();
+            db.seek(0);
+            String lastKey = "";
+            int lastOffset = 0;
+            while (curPointer < length) {
+                byte curByte = db.readByte();
+                if (curByte == '\0') {
+                    byte[] byteKey = new byte[(int) curPointer - (int) lastPointer];
+                    curPointer = db.getFilePointer();
+                    db.seek(lastPointer);
+                    db.readFully(byteKey);
+                    db.seek(curPointer);
+                    String currentKey = new String(byteKey, "UTF-8");
+                    int offset = db.readInt();
+                    if (!lastKey.isEmpty()) {
+                        byte[] byteValue = new byte[offset - lastOffset];
+                        curPointer = db.getFilePointer();
+                        db.seek(lastOffset);
+                        db.readFully(byteValue);
+                        String lastValue = new String(byteValue, "UTF-8");
+                        db.seek(curPointer);
+                        if (loadList.containsKey(lastKey)) {
+                            System.err.println(lastKey + " meets twice in db.dat");
+                            System.exit(1);
+                        }
+                        loadList.put(lastKey, lastValue);
+                    }
+                    lastOffset = offset;
+                    lastKey = currentKey;
+                    lastPointer = db.getFilePointer();
+                }
+                curPointer = db.getFilePointer();
+            }
+            if (lastOffset == 0 || lastKey.isEmpty()) {
+                System.err.println("Incorrect db");
+                System.exit(1);
+            }
+            byte[] byteValue = new byte[(int) length - lastOffset];
+            db.seek(lastOffset);
+            db.readFully(byteValue);
+            String lastValue = new String(byteValue, "UTF-8");
+            if (loadList.containsKey(lastKey)) {
+                System.err.println(lastKey + " meets twice in db.dat");
+                System.exit(1);
+            }
+            loadList.put(lastKey, lastValue);
+        } catch (Exception e) {
+            db.close();
+            throw new Exception(e);
         }
-        if (lastOffset == 0 || lastKey.isEmpty()) {
-            System.err.println("Incorrect db");
-            System.exit(1);
-        }
-        byte[] byteValue = new byte[(int) length - lastOffset];
-        db.seek(lastOffset);
-        db.readFully(byteValue);
-        String lastValue = new String(byteValue, "UTF-8");
-        if (loadList.containsKey(lastKey)) {
-            System.err.println(lastKey + " meets twice in db.dat");
-            System.exit(1);
-        }
-        loadList.put(lastKey, lastValue);
         db.close();
     }
     
@@ -75,37 +80,42 @@ public class FileMap {
         File dbFile = commands.currentDir.toPath().resolve("db.dat").toFile();
         RandomAccessFile db;
         db = new RandomAccessFile(dbFile, "rw");
-        db.setLength(0);
-        Iterator<Map.Entry<String, String>> it;
-        it = commands.list.entrySet().iterator();
-        long[] pointers = new long[commands.list.size()];
-        int counter = 0;
-        while (it.hasNext()) {
-            Map.Entry<String, String> m = (Map.Entry<String, String>) it.next();
-            String key = m.getKey();
-            db.write(key.getBytes("UTF-8"));
-            db.write("\0".getBytes("UTF-8"));
-            pointers[counter] = db.getFilePointer();
-            db.seek(pointers[counter] + 4);
-            ++counter;
-        }
-        it = commands.list.entrySet().iterator();
-        counter = 0;
-        while (it.hasNext()) {
-            Map.Entry<String, String> m = (Map.Entry<String, String>) it.next();
-            String value = m.getValue();
-            int curPointer = (int) db.getFilePointer();
-            db.seek(pointers[counter]);
-            db.writeInt(curPointer);
-            db.seek(curPointer);
-            db.write(value.getBytes("UTF-8"));
-            ++counter;
+        try {    
+            db.setLength(0);
+            Iterator<Map.Entry<String, String>> it;
+            it = commands.list.entrySet().iterator();
+            long[] pointers = new long[commands.list.size()];
+            int counter = 0;
+            while (it.hasNext()) {
+                Map.Entry<String, String> m = (Map.Entry<String, String>) it.next();
+                String key = m.getKey();
+                db.write(key.getBytes("UTF-8"));
+                db.write("\0".getBytes("UTF-8"));
+                pointers[counter] = db.getFilePointer();
+                db.seek(pointers[counter] + 4);
+                ++counter;
+            }
+            it = commands.list.entrySet().iterator();
+            counter = 0;
+            while (it.hasNext()) {
+                Map.Entry<String, String> m = (Map.Entry<String, String>) it.next();
+                String value = m.getValue();
+                int curPointer = (int) db.getFilePointer();
+                db.seek(pointers[counter]);
+                db.writeInt(curPointer);
+                db.seek(curPointer);
+                db.write(value.getBytes("UTF-8"));
+                ++counter;
+            }
+        } catch (Exception e) {
+            db.close();
+            throw new Exception(e);
         }
         db.close();
     }
     
     public static void main(String[] args) {
-        currentDir = new File(System.getProperty("user.dir"));
+        currentDir = new File(System.getProperty("fizteh.db.dir"));
         try {
             loadFile();
         } catch (Exception e) {
@@ -113,7 +123,7 @@ public class FileMap {
             System.exit(1);
         }
         commands = new FileMapCommands(loadList);
-        shell = new Shell(commands, "user.dir");
+        shell = new Shell(commands, "fizteh.db.dir");
         int argc = args.length;
         if (argc == 0) {
             Shell.interactive();
