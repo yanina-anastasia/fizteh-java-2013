556c4ddbed73d4717e116e3a38b1e9fd3c944e9f
AnnaSavinova <anna.savinova@phystech.edu>
2013-10-18 11:03:17 +0400

    change Shell and FileMap

--- src/ru/fizteh/fivt/students/annasavinova/filemap/FileMap.java
+++ src/ru/fizteh/fivt/students/annasavinova/filemap/FileMap.java
@@ -6,12 +6,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class FileMap extends UserShell {
     private RandomAccessFile dataFile;
+    private HashMap<String, String> dataMap;
 
+    @Override
+    public void printError(String errStr) {
+        if (isPacket) {
+            System.err.println(errStr);
+            unloadFile();
+            System.exit(1);
+        } else {
+            System.out.println(errStr);
+        }
+    }
+    
     protected void printErrorAndExit(String errMessage) {
         System.err.println(errMessage);
+        unloadFile();
         System.exit(1);
     }
 
@@ -32,173 +48,118 @@ public class FileMap extends UserShell {
         return str.toString();
     }
 
-    private String getKey(long pointer) {
+    private void loadKeyAndValue() {
         try {
-            dataFile.seek(pointer);
             int keyLong = dataFile.readInt();
             int valueLong = dataFile.readInt();
-            byte[] byteArray = new byte[keyLong];
-            dataFile.read(byteArray, 0, keyLong);
-            String key = new String(byteArray, "UTF-16");
-            dataFile.skipBytes(valueLong);
-            return key;
+            byte[] keyBytes = new byte[keyLong];
+            byte[] valueBytes = new byte[valueLong];
+            dataFile.read(keyBytes);
+            dataFile.read(valueBytes);
+            String key = new String(keyBytes);
+            String value = new String(valueBytes);
+            dataMap.put(key, value);
         } catch (IOException e) {
             printErrorAndExit("Can't read file");
         }
-        return "PANIC_KEY";
     }
 
-    private String getValue(long pointer) {
-        try {
-            dataFile.seek(pointer);
-            int keyLong = dataFile.readInt();
-            int valueLong = dataFile.readInt();
-            byte[] byteArray = new byte[valueLong];
-            dataFile.skipBytes(keyLong);
-            dataFile.read(byteArray, 0, valueLong);
-            String value = new String(byteArray, "UTF-16");
-            return value;
-        } catch (IOException e) {
-            printErrorAndExit("Can't read file");
-        }
-        return "PANIC_VALUE";
-    }
-
-    private long findKey(String key) {
+    protected void loadFile(File data) {
+        dataMap = new HashMap<>();
+        setDataFile(data);
         try {
             dataFile.seek(0);
-            long currPointer = dataFile.getFilePointer();
-            while (currPointer != dataFile.length()) {
-                if (key.equals(getKey(currPointer))) {
-                    return currPointer;
-                }
-                currPointer = dataFile.getFilePointer();
+            while (dataFile.getFilePointer() != dataFile.length()) {
+                loadKeyAndValue();
             }
         } catch (IOException e) {
-            printErrorAndExit("Can't read file");
+            printErrorAndExit("Cannot load file");
         }
-        return -1;
+    }
+
+    protected void unloadFile() {
+        try {
+            dataFile.setLength(0);
+            Set<Map.Entry<String, String>> entries = dataMap.entrySet();
+            for (Map.Entry<String, String> entry : entries) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                dataFile.writeInt(key.getBytes("UTF-8").length);
+                dataFile.writeInt(value.getBytes("UTF-8").length);
+                dataFile.write(key.getBytes("UTF-8"));
+                dataFile.write(value.getBytes("UTF-8"));
+            }
+            dataFile.close();
+            dataMap.clear();
+        } catch (IOException e) {
+            printErrorAndExit("Cannot unload file");
+        }
     }
 
     public void doPut(String key, String value) {
-        try {
-            long pointToKey = findKey(key);
-            if (dataFile.length() == 0 || pointToKey == -1) {
-                System.out.println("new");
-            } else {
-                System.out.println("overwrite");
-                System.out.println(getValue(pointToKey));
-                delete(key);
-            }
-            dataFile.seek(dataFile.length());
-            dataFile.writeInt(2 * key.length());
-            dataFile.writeInt(2 * value.length());
-            dataFile.writeChars(key);
-            dataFile.writeChars(value);
-        } catch (IOException e) {
-            printErrorAndExit("Can't put");
+        if (dataMap.get(key) == null) {
+            System.out.println("new");
+        } else {
+            System.out.println("owerwrite");
+            System.out.println(dataMap.get(key));
         }
+        dataMap.put(key, value);
     }
 
     public void doGet(String key) {
-        long findRes = findKey(key);
-        try {
-            if (findRes == -1 || dataFile.length() == 0) {
-                System.out.println("not found");
-            } else {
-                String value = getValue(findRes);
-                System.out.println("found");
-                System.out.println(value);
-            }
-        } catch (IOException e) {
-            printErrorAndExit("Can't get");
-        }
-    }
-
-    private void copy(RandomAccessFile source, RandomAccessFile dest, long offset, long length) {
-        int tmp = (int) length;
-        byte[] arr = new byte[tmp];
-        try {
-            source.seek(offset);
-            source.read(arr, 0, tmp);
-            dest.seek(dest.length());
-            dest.write(arr);
-        } catch (IOException e) {
-            printErrorAndExit("Can't rewrite file");
-        }
-
-    }
-
-    private void delete(String key) {
-        try {
-            long keyPointer = findKey(key);
-            File tmp = File.createTempFile("DataBase", key);
-            RandomAccessFile tmpFile = new RandomAccessFile(tmp, "rw");
-            copy(dataFile, tmpFile, 0, keyPointer);
-            dataFile.seek(keyPointer);
-            int keyLong = dataFile.readInt();
-            int valueLong = dataFile.readInt();
-            long recordEnd = dataFile.getFilePointer() + keyLong + valueLong;
-            copy(dataFile, tmpFile, recordEnd, dataFile.length() - recordEnd);
-            dataFile.setLength(0);
-            copy(tmpFile, dataFile, 0, tmpFile.length());
-            dataFile.setLength(tmpFile.length());
-            tmpFile.close();
-        } catch (IOException e) {
-            printErrorAndExit("Can't remove");
+        if (dataMap.get(key) == null) {
+            System.out.println("not found");
+        } else {
+            System.out.println("found");
+            System.out.println(dataMap.get(key));
         }
     }
 
     public void doRemove(String key) {
-        long keyPointer = findKey(key);
-        if (keyPointer == -1) {
+        if (dataMap.get(key) == null) {
             System.out.println("not found");
         } else {
-            delete(key);
             System.out.println("removed");
+            dataMap.remove(key);
         }
     }
 
     @Override
     protected void execProc(String[] args) {
-        try {
-            File data = new File(System.getProperty("fizteh.db.dir") + File.separatorChar + "db.dat");
-            setDataFile(data);
-            if (args != null && args.length != 0) {
-                switch (args[0]) {
-                case "put":
-                    if (args.length > 2) {
-                        doPut(args[1], appendArgs(2, args));
-                    } else {
-                        printError("Incorrect number of args");
-                    }
-                    break;
-                case "get":
-                    if (checkArgs(2, args)) {
-                        doGet(args[1]);
-                    }
-                    break;
-                case "remove":
-                    if (checkArgs(2, args)) {
-                        doRemove(args[1]);
-                    }
-                    break;
-                case "exit":
-                    dataFile.close();
-                    System.exit(0);
-                    break;
-                default:
-                    printError("Unknown command");
+        if (args != null && args.length != 0) {
+            switch (args[0]) {
+            case "put":
+                if (args.length > 2) {
+                    doPut(args[1], appendArgs(2, args));
+                } else {
+                    printError("Incorrect number of args");
                 }
-                dataFile.close();
+                break;
+            case "get":
+                if (checkArgs(2, args)) {
+                    doGet(args[1]);
+                }
+                break;
+            case "remove":
+                if (checkArgs(2, args)) {
+                    doRemove(args[1]);
+                }
+                break;
+            case "exit":
+                unloadFile();
+                System.exit(0);
+                break;
+            default:
+                printError("Unknown command");
             }
-        } catch (IOException e) {
-            printErrorAndExit("Cannot open or close file");
         }
     }
 
     public static void main(String[] args) {
         FileMap tmp = new FileMap();
+        File data = new File(System.getProperty("fizteh.db.dir") + File.separatorChar + "db.dat");
+        tmp.loadFile(data);
         tmp.exec(args);
+        tmp.unloadFile();
     }
 }
