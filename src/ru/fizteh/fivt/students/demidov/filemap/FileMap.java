f30180208106a59d3534e4d3b6c30864f8f7f4ee
Demidov <demidov-study@yandex.ru>
2013-10-19 03:27:51 +0400

    Filemap by Demidov

--- src/ru/fizteh/fivt/students/demidov/filemap/FileMap.java
+++ src/ru/fizteh/fivt/students/demidov/filemap/FileMap.java
@@ -13,7 +13,7 @@ public class FileMap {
 		return currentTable;
 	}
 	
-	public void readDataFromFile(Shell usedShell) throws IOException {
+	public void openFile(Shell usedShell) throws IOException {
 		String path = System.getProperty("fizteh.db.dir");
 		if (path == null) {
 			throw new IOException("problem with property");
@@ -29,45 +29,75 @@ public class FileMap {
 		}
 		
 		dataBaseFile = new RandomAccessFile(currentDirectory, "rwd");
+	}
+	
+    private String readString(long readPosition, long finishPosition) throws IOException {
+        byte[] bytes = new byte[(int)(finishPosition - readPosition)];
+        dataBaseFile.seek(readPosition);
+        dataBaseFile.read(bytes);
+        return new String(bytes, "UTF-8");
+}
+	
+	public void readDataFromFile(Shell usedShell) throws IOException {	
+		openFile(usedShell);
 		
 		if (dataBaseFile.length() == 0) {
 			return;
 		}
 		
+		String readKey = null;
+		String previousKey = null;
 		long positionOfValues = Long.MAX_VALUE;
-		long readedPosition = 0;
-		while (readedPosition < positionOfValues) {
-			dataBaseFile.seek(readedPosition);
-			String key = dataBaseFile.readUTF();
-			dataBaseFile.readChar();
-			long offset = dataBaseFile.readInt();
-			readedPosition = dataBaseFile.getFilePointer();
-			dataBaseFile.seek(offset);
-			currentTable.put(key, dataBaseFile.readUTF());
-			if (positionOfValues == Long.MAX_VALUE) {
-				positionOfValues = offset;
+		long readPosition = 0;
+		long previousOffset = Long.MAX_VALUE;
+		dataBaseFile.seek(readPosition);
+
+		while (readPosition < positionOfValues) {
+			while ((dataBaseFile.getFilePointer() < dataBaseFile.length()) && !(dataBaseFile.readByte() == '\0')) {}
+			
+			long nextOffset = dataBaseFile.readInt();
+			long keyPosition = dataBaseFile.getFilePointer();
+			previousKey = readKey;
+			readKey = readString(readPosition, dataBaseFile.getFilePointer() - 5);
+			
+			if (previousOffset == Long.MAX_VALUE) {
+				positionOfValues = nextOffset;
+			} else {
+				currentTable.put(previousKey, readString(previousOffset, nextOffset));
 			}
-		}
+			
+			previousOffset = nextOffset;		
+			dataBaseFile.seek(keyPosition);
+			readPosition = dataBaseFile.getFilePointer();
+			System.out.println(nextOffset);
+		} 
+	
+		currentTable.put(readKey, readString(previousOffset, (int)dataBaseFile.length()));
+		
+		dataBaseFile.close();
 	}
 
 
 	public void writeDataToFile(Shell usedShell) throws IOException {
+		openFile(usedShell);
 		int offset = 0;		
 		for (String key: getCurrentTable().keySet()) {
-			offset += 8 + key.getBytes("UTF-8").length;
+			offset += 5 + key.getBytes("UTF-8").length;
 		}
 		
 		long writenPosition = 0;
 		for (Map.Entry<String, String> currentPair : getCurrentTable().entrySet()) {
 			dataBaseFile.seek(writenPosition);
-			dataBaseFile.writeUTF(currentPair.getKey());
-			dataBaseFile.writeChar('\0');
+			dataBaseFile.write(currentPair.getKey().getBytes("UTF-8"));
+			dataBaseFile.writeByte('\0');
 			dataBaseFile.writeInt(offset);
 			writenPosition = dataBaseFile.getFilePointer();
 			dataBaseFile.seek(offset);
-			dataBaseFile.writeUTF(currentPair.getValue());
+			dataBaseFile.write(currentPair.getValue().getBytes("UTF-8"));
 			offset = (int)dataBaseFile.getFilePointer();
 		}
+		
+		dataBaseFile.close();
 	}
 	
 	private final Map<String, String> currentTable = new HashMap<String, String>();
