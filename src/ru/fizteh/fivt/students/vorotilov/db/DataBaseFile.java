213de79ef178347566d6d38f649368fb3de83c5f
vlad-vorotilov <vlad.vorotilov@gmail.com>
2013-10-19 00:47:50 +0400

    db fix 1

--- src/ru/fizteh/fivt/students/vorotilov/db/DataBaseFile.java
+++ src/ru/fizteh/fivt/students/vorotilov/db/DataBaseFile.java
@@ -26,6 +26,9 @@ public class DataBaseFile {
             while (dbFile.getFilePointer() < dbFile.length() - 1) {
                 int keyLength = dbFile.readInt();
                 int valueLength = dbFile.readInt();
+                if (keyLength <= 0 || valueLength <= 0) {
+                    throw new DataBaseFileDamaged();
+                }
                 byte[] key = new byte[keyLength];
                 byte[] value = new byte[valueLength];
                 dbFile.read(key);
@@ -36,6 +39,8 @@ public class DataBaseFile {
             System.out.println("can't open data base file: '" + dbFilePath.getCanonicalPath() + "' file not found");
         } catch (CantCreateDbFile e) {
             System.out.println("can't create new db file: '" + e.getProblematicFile() + "'");
+        } catch (IOException | DataBaseFileDamaged e) {
+            System.out.println("can't read db file, it's damaged");
         }
     }
 
@@ -71,23 +76,27 @@ public class DataBaseFile {
         return currentValue;
     }
 
-    public void save() throws IOException {
-        Set<Map.Entry<String, byte[]>> dbSet = dbMap.entrySet();
-        Iterator<Map.Entry<String, byte[]>> i = dbSet.iterator();
-        dbFile.seek(0);
-        while (i.hasNext()) {
-            Map.Entry<String, byte[]> tempEntry = i.next();
-            dbFile.writeInt(tempEntry.getKey().length());
-            dbFile.writeInt(tempEntry.getValue().length);
-            dbFile.write(tempEntry.getKey().getBytes());
-            dbFile.write(tempEntry.getValue());
+    public void save() {
+        try {
+            Set<Map.Entry<String, byte[]>> dbSet = dbMap.entrySet();
+            Iterator<Map.Entry<String, byte[]>> i = dbSet.iterator();
+            dbFile.seek(0);
+            while (i.hasNext()) {
+                Map.Entry<String, byte[]> tempEntry = i.next();
+                dbFile.writeInt(tempEntry.getKey().length());
+                dbFile.writeInt(tempEntry.getValue().length);
+                dbFile.write(tempEntry.getKey().getBytes());
+                dbFile.write(tempEntry.getValue());
+            }
+            dbFile.setLength(dbFile.getFilePointer());
+        } catch (IOException e) {
+            System.out.println("can't save data base file");
         }
-        dbFile.setLength(dbFile.getFilePointer() + 1);
     }
 
     public void close() {
+        save();
         try {
-            save();
             dbFile.close();
         } catch (IOException e) {
             System.out.println("can't close data base file");
