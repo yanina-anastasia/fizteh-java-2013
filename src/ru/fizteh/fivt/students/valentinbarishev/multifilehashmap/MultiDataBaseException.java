8cf6dc813837db80336f4b8846a3911e8b67611b
BaJIuK <bbajiuk@gmail.com>
2013-10-19 00:35:43 +0400

    (multifilehashmap deleted)

--- src/ru/fizteh/fivt/students/valentinbarishev/multifilehashmap/MultiDataBaseException.java
+++
@@ -1,8 +0,0 @@
-package ru.fizteh.fivt.students.valentinbarishev.multifilehashmap;
-
-public class MultiDataBaseException extends Error {
-    public MultiDataBaseException(final String message) {
-        super(message);
-    }
-}
-
