3192056156cf56e5a34c8959f57e43b4da6f1423
nlevashov <me@yandex.ru>
2013-10-19 11:59:38 +0400

    filemap 3rd try

--- src/ru/fizteh/fivt/students/nlevashov/filemap/Table.java
+++ src/ru/fizteh/fivt/students/nlevashov/filemap/Table.java
@@ -18,58 +18,63 @@ public class Table {
         map = new HashMap<String, String>();
         if (Files.exists(addr)) {
             BufferedInputStream i = new BufferedInputStream(Files.newInputStream(addr));
-            Vector<String> keys = new Vector<String>();
-            Vector<Integer> offsets = new Vector<Integer>();
+            
+            try {
+                Vector<String> keys = new Vector<String>();
+                Vector<Integer> offsets = new Vector<Integer>();
 
-            int c = i.read();
-            int pos = 1;
-            if (c != -1) {
-                byte[] key = new byte[1024];
-                int keyLength = 0;
-                do {
-                    key[keyLength] = (byte) c;
-                    keyLength++;
-                    c = i.read();
-                    pos++;
-                } while (c != '\0');
-                byte[] shortKey = new byte[keyLength];
-                System.arraycopy(key, 0, shortKey, 0, keyLength);
-                keys.add(new String(shortKey, "UTF8"));
-
-                offsets.add(i.read() * 256 * 256 * 256 + i.read() * 256 * 256 + i.read() * 256 + i.read());
-                pos += 4;
-
-                int k = 1;
-                while (!(offsets.get(0) + k * 4 == pos)) {
-                    keyLength = 0;
-                    c = i.read();
-                    pos++;
+                int c = i.read();
+                int pos = 1;
+                if (c != -1) {
+                    byte[] key = new byte[1024];
+                    int keyLength = 0;
                     do {
                         key[keyLength] = (byte) c;
                         keyLength++;
                         c = i.read();
                         pos++;
                     } while (c != '\0');
-                    shortKey = new byte[keyLength];
+                    byte[] shortKey = new byte[keyLength];
                     System.arraycopy(key, 0, shortKey, 0, keyLength);
                     keys.add(new String(shortKey, "UTF8"));
 
                     offsets.add(i.read() * 256 * 256 * 256 + i.read() * 256 * 256 + i.read() * 256 + i.read());
                     pos += 4;
 
-                    k++;
-                }
+                    int k = 1;
+                    while (!(offsets.get(0) == pos)) {
+                        keyLength = 0;
+                        c = i.read();
+                        pos++;
+                        do {
+                            key[keyLength] = (byte) c;
+                            keyLength++;
+                            c = i.read();
+                            pos++;
+                        } while (c != '\0');
+                        shortKey = new byte[keyLength];
+                        System.arraycopy(key, 0, shortKey, 0, keyLength);
+                        keys.add(new String(shortKey, "UTF8"));
 
-                offsets.add((int) (Files.size(addr) - k * 4));
-                for (int j = 0; j < keys.size(); j++) {
-                    int valueLength = offsets.get(j + 1).intValue() - offsets.get(j).intValue();
-                    byte[] buf = new byte[valueLength];
-                    i.read(buf, 0, valueLength);
-                    map.put(keys.get(j), new String(buf, "UTF8"));
+                        offsets.add(i.read() * 256 * 256 * 256 + i.read() * 256 * 256 + i.read() * 256 + i.read());
+                        pos += 4;
+
+                        k++;
+                    }
+
+                    offsets.add((int) Files.size(addr));
+                    for (int j = 0; j < keys.size(); j++) {
+                        int valueLength = offsets.get(j + 1).intValue() - offsets.get(j).intValue();
+                        byte[] buf = new byte[valueLength];
+                        i.read(buf, 0, valueLength);
+                        map.put(keys.get(j), new String(buf, "UTF8"));
+                    }
                 }
+            } catch (IOException e) {
+                throw e;
+            } finally {
+                i.close();
             }
-
-            i.close();
         }
     }
 
@@ -78,36 +83,47 @@ public class Table {
         Files.createFile(addr);
         if (!map.isEmpty()) {
             BufferedOutputStream o = new BufferedOutputStream(Files.newOutputStream(addr));
-            Vector<Integer> valuesLengthSum = new Vector<Integer>();
-            valuesLengthSum.add(0);
-            int i = 1;
-            int head = 0;
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                head += entry.getKey().getBytes("UTF8").length;
-                valuesLengthSum.add(entry.getValue().getBytes("UTF8").length + valuesLengthSum.get(i - 1));
-                i++;
+            try {
+                byte[][] keys = new byte[map.size()][];
+                byte[][] values = new byte[map.size()][];
+                Vector<Integer> valuesLengthSum = new Vector<Integer>();
+                valuesLengthSum.add(0);
+                int head = 0;
+
+                int i = 0;
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    keys[i] = entry.getKey().getBytes("UTF8");
+                    head += keys[i].length;
+                    values[i] = entry.getValue().getBytes("UTF8");
+                    valuesLengthSum.add(values[i].length + valuesLengthSum.get(i));
+                    i++;
+                }
+                head += map.size() * 5;
+
+                i = 0;
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    o.write(keys[i], 0, keys[i].length);
+                    o.write('\0');
+
+                    int offset = head + valuesLengthSum.get(i);
+                    o.write((byte) (offset / 256 / 256 / 256));
+                    o.write((byte) (offset / 256 / 256 % 256));
+                    o.write((byte) (offset / 256 % 256));
+                    o.write((byte) (offset % 256));
+
+                    i++;
+                }
+
+                i = 0;
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    o.write(values[i], 0, values[i].length);
+                    i++;
+                }
+            } catch (IOException e) {
+                throw e;
+            } finally {
+                o.close();
             }
-            head += map.size();
-
-            i = 0;
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                o.write(entry.getKey().getBytes("UTF8"), 0, entry.getKey().getBytes("UTF8").length);
-                o.write('\0');
-
-                int offset = head + valuesLengthSum.get(i);
-                o.write((byte) (offset / 256 / 256 / 256));
-                o.write((byte) (offset / 256 / 256 % 256));
-                o.write((byte) (offset / 256 % 256));
-                o.write((byte) (offset % 256));
-
-                i++;
-            }
-
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                o.write(entry.getValue().getBytes("UTF8"), 0, entry.getValue().getBytes("UTF8").length);
-            }
-
-            o.close();
         }
     }
 
