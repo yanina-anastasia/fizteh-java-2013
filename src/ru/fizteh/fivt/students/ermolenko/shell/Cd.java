7aa0aba098ed10bf9a8033cd7fb6f4bc65db1337
Pingvin786 <pingvin786@mail.ru>
2013-10-21 20:12:03 +0400

    Попытка добавить команды по Code Convention.

---
+++ src/ru/fizteh/fivt/students/ermolenko/shell/Cd.java
@@ -0,0 +1,25 @@
+package ru.fizteh.fivt.students.ermolenko.shell;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class Cd implements Command {
+
+    public String getName() {
+        return "cd";
+    }
+
+    public void executeCmd(Shell shell, String[] args) throws IOException {
+        if (1 == args.length) {
+            Path thePath = shell.getState().getPath().resolve(args[0]);
+            File theFile = new File(thePath.toString());
+            if (!theFile.isDirectory()) {
+                throw new IOException("Directory doesn't exist");
+            }
+            shell.setState(thePath.normalize());
+        } else {
+            throw new IOException("not allowed number of arguments");
+        }
+    }
+}
