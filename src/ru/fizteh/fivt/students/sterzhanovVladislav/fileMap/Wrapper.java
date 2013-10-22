3bfffacde942a79b95863d53622c525b012df7c4
Quadrocube <gliderok@gmail.com>
2013-10-23 01:58:19 +0400

    Initial MultiFileMap impl

--- src/ru/fizteh/fivt/students/sterzhanovVladislav/fileMap/Wrapper.java
+++ src/ru/fizteh/fivt/students/sterzhanovVladislav/fileMap/Wrapper.java
@@ -5,7 +5,6 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 
 import ru.fizteh.fivt.students.sterzhanovVladislav.shell.Command;
-import ru.fizteh.fivt.students.sterzhanovVladislav.shell.ShellCommands;
 import ru.fizteh.fivt.students.sterzhanovVladislav.shell.ShellUtility;
 
 public class Wrapper {
@@ -20,10 +19,6 @@ public class Wrapper {
             System.exit(-1);
         }
         Path dbPath = Paths.get(dbDir);
-        if (dbPath == null) {
-            System.out.println("fizteh.db.dir did not resolve to a valid directory");
-            System.exit(-1);
-        }
         try {
             context = new DatabaseContext(dbPath);
         } catch (Exception e) {
@@ -34,7 +29,10 @@ public class Wrapper {
         cmdMap.put("put", new FileMapCommands.Put().setContext(context));
         cmdMap.put("get", new FileMapCommands.Get().setContext(context));
         cmdMap.put("remove", new FileMapCommands.Remove().setContext(context));
-        cmdMap.put("exit", new ShellCommands.Exit());
+        cmdMap.put("create", new FileMapCommands.Create().setContext(context));
+        cmdMap.put("drop", new FileMapCommands.Drop().setContext(context));
+        cmdMap.put("use", new FileMapCommands.Use().setContext(context));
+        cmdMap.put("exit", new FileMapCommands.Exit().setContext(context));
         
         ShellUtility.execShell(args, cmdMap);
         System.exit(0);
--- src/ru/fizteh/fivt/students/sterzhanovVladislav/shell/ShellUtility.java
+++ src/ru/fizteh/fivt/students/sterzhanovVladislav/shell/ShellUtility.java
@@ -1,9 +1,15 @@
 package ru.fizteh.fivt.students.sterzhanovVladislav.shell;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
 import java.util.HashMap;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class ShellUtility {
     public static InputStream createStream(String[] args) {
@@ -28,5 +34,25 @@ public class ShellUtility {
             System.err.println(e.getMessage());
             System.exit(-1);
         }
+    }
+    
+    public static void removeDir(Path path) throws IOException {
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                            throws IOException {
+                        Files.delete(file);
+                        return FileVisitResult.CONTINUE;
+                    }
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException e) throws IOException {
+                        if (e == null) {
+                            Files.delete(dir);
+                            return FileVisitResult.CONTINUE;
+                        } else {
+                            throw e;
+                        }
+                    }
+                });
     }
 }
