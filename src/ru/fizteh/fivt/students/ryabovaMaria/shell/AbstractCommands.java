541c879bc4b2fedd1b245568dd4a3d1042576103
RyabovaMaria <liastramv@gmail.com>
2013-10-23 00:58:54 +0400

    Update AbstractCommands.java

--- src/ru/fizteh/fivt/students/ryabovaMaria/shell/AbstractCommands.java
+++ src/ru/fizteh/fivt/students/ryabovaMaria/shell/AbstractCommands.java
@@ -3,6 +3,6 @@ package ru.fizteh.fivt.students.ryabovaMaria.shell;
 import java.io.File;
 
 public abstract class AbstractCommands {
-    public static String[] lexems;
-    public static File currentDir;
+    public String[] lexems;
+    public File currentDir;
 }
