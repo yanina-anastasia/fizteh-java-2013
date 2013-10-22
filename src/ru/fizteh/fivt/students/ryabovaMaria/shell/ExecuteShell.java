24a525cdea2912aedc7aeec87b975dfe91e515ce
RyabovaMaria <liastramv@gmail.com>
2013-10-23 00:59:29 +0400

    Update ExecuteShell.java

--- src/ru/fizteh/fivt/students/ryabovaMaria/shell/ExecuteShell.java
+++ src/ru/fizteh/fivt/students/ryabovaMaria/shell/ExecuteShell.java
@@ -1,11 +1,21 @@
 package ru.fizteh.fivt.students.ryabovaMaria.shell;
 
+import java.io.File;
+
 public class ExecuteShell {
-    static ShellCommands commands = new ShellCommands();
-    static Shell shell = new Shell(commands, "user.dir");
+    private static ShellCommands commands;
+    private static Shell shell;
     
     public static void main(String[] args) {
         int argc = args.length;
+        String getPropertyString = System.getProperty("user.dir");
+        if (getPropertyString == null) {
+            System.err.println("I can't find this directory");
+            System.exit(1);
+        }
+        commands = new ShellCommands();
+        commands.currentDir = new File(getPropertyString);
+        shell = new Shell(commands);
         if (argc == 0) {
             shell.interactive();
         } else {
