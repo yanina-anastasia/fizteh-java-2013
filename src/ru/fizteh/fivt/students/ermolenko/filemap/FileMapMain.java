545daf566d87c1d94bbbc4dcc8256d316f87bc74
Pingvin786 <pingvin786@mail.ru>
2013-10-24 17:20:13 +0400

    tests_2

--- src/ru/fizteh/fivt/students/ermolenko/filemap/FileMapMain.java
+++ src/ru/fizteh/fivt/students/ermolenko/filemap/FileMapMain.java
@@ -22,11 +22,11 @@ public class FileMapMain {
             System.out.println("2");
             System.out.println(base.toString());
             FileMap filemap = new FileMap(base);
+            System.out.println("3");
             FileMapExecutor exec = new FileMapExecutor();
             if (args.length > 0) {
                 filemap.batchState(args, exec);
             } else {
-                System.out.println("3");
                 filemap.interactiveState(exec);
             }
         } catch (Exception e) {
