49dd8ef849f097e34bc39c895002ea86cd80027b
surakshina <yuliya.surakshina@phystech.edu>
2013-10-25 04:37:29 +0400

    multifilehashmap

--- src/ru/fizteh/fivt/students/surakshina/filemap/FileMap.java
+++ src/ru/fizteh/fivt/students/surakshina/filemap/FileMap.java
@@ -12,52 +12,30 @@ import java.util.Vector;
 public class FileMap {
     public static String workingDirectory = System.getProperty("fizteh.db.dir");
     public static RandomAccessFile dataBase;
-    static HashMap<String, String> fileMap = new HashMap<String, String>();
+    public static FileMap[][] table = new FileMap[16][16];
+    public HashMap<String, String> fileMap = new HashMap<String, String>();
+    static boolean hasOpenedTable = false;
+    public static String currentTable;
 
     public static void main(String[] args) throws FileNotFoundException {
         if (workingDirectory != null) {
             File directory = new File(workingDirectory);
-            if (directory.isDirectory()) {
-                dataBase = new RandomAccessFile(workingDirectory + File.separator + "db.dat", "rw");
-                try {
-                    if (dataBase.length() != 0) {
-                        readDatabase();
-                    }
-                } catch (FileNotFoundException e) {
-                    System.err.println("Can't read database");
-                    closeFile(dataBase);
-                    System.exit(1);
-                } catch (IOException e1) {
-                    System.err.println("Can't read from database");
-                    closeFile(dataBase);
-                    System.exit(1);
-                }
-                Commands input = new Commands();
-                input.workWithShell(args);
-                try {
-                    writeInDatabase();
-                } catch (FileNotFoundException e) {
-                    System.err.println("Can't read database");
-                    closeFile(dataBase);
-                    System.exit(1);
-                } catch (IOException e1) {
-                    System.err.println("Can't write in database");
-                    closeFile(dataBase);
-                    System.exit(1);
-                }
-                closeFile(dataBase);
+            if (directory.isDirectory() && directory.exists()) {
+                Commands cmd = new Commands();
+                cmd.workWithShell(args);
+                cmd.saveTable();
             } else {
                 System.err.println("Not a directory");
                 System.exit(1);
             }
         } else {
-            System.err.println("No file");
+            System.err.println("No directory");
             System.exit(1);
         }
         return;
     }
 
-    static void closeFile(RandomAccessFile file) {
+    static void closeFile() {
         try {
             dataBase.close();
         } catch (IOException e2) {
@@ -96,50 +74,54 @@ public class FileMap {
         return result;
     }
 
-    private static void readDatabase() throws IOException {
-        String keyFirst;
-        String keySecond = null;
-        String value;
-        int currentPosition = (int) dataBase.getFilePointer();
-        int offsetOfValueFirst = 0;
-        int firstOffset = 0;
-        int offsetOfValueSecond = 0;
-        dataBase.seek(currentPosition);
-        keyFirst = getKey();
-        offsetOfValueFirst = dataBase.readInt();
-        if (offsetOfValueFirst > 0) {
-            currentPosition = (int) dataBase.getFilePointer();
-            firstOffset = offsetOfValueFirst;
-            do {
-                dataBase.seek(currentPosition);
-                if (currentPosition < firstOffset) {
-                    keySecond = getKey();
-                    offsetOfValueSecond = dataBase.readInt();
-                    currentPosition = (int) dataBase.getFilePointer();
-                } else if (currentPosition == offsetOfValueFirst) {
-                    offsetOfValueSecond = (int) dataBase.length();
-                    ++currentPosition;
+    protected void readDatabase(File tmp) throws IOException {
+        if (tmp.length() != 0) {
+            fileMap = new HashMap<String, String>();
+            dataBase = new RandomAccessFile(tmp.getAbsolutePath(), "rw");
+            String keyFirst;
+            String keySecond = null;
+            String value;
+            int currentPosition = (int) dataBase.getFilePointer();
+            int offsetOfValueFirst = 0;
+            int firstOffset = 0;
+            int offsetOfValueSecond = 0;
+            dataBase.seek(currentPosition);
+            keyFirst = getKey();
+            offsetOfValueFirst = dataBase.readInt();
+            if (offsetOfValueFirst > 0) {
+                currentPosition = (int) dataBase.getFilePointer();
+                firstOffset = offsetOfValueFirst;
+                do {
+                    dataBase.seek(currentPosition);
+                    if (currentPosition < firstOffset) {
+                        keySecond = getKey();
+                        offsetOfValueSecond = dataBase.readInt();
+                        currentPosition = (int) dataBase.getFilePointer();
+                    } else if (currentPosition == offsetOfValueFirst) {
+                        offsetOfValueSecond = (int) dataBase.length();
+                        ++currentPosition;
+                    }
+                    dataBase.seek(firstOffset);
+                    value = getValue(offsetOfValueSecond);
+                    fileMap.put(keyFirst, value);
+                    keyFirst = keySecond;
+                    firstOffset = offsetOfValueSecond;
+                } while (currentPosition < offsetOfValueFirst);
+                if (keyFirst != null) {
+                    value = getValue(offsetOfValueSecond);
+                    fileMap.put(keyFirst, value);
                 }
-                dataBase.seek(firstOffset);
-                value = getValue(offsetOfValueSecond);
-                fileMap.put(keyFirst, value);
-                keyFirst = keySecond;
-                firstOffset = offsetOfValueSecond;
-            } while (currentPosition < offsetOfValueFirst);
-            if (keyFirst != null) {
-                value = getValue(offsetOfValueSecond);
-                fileMap.put(keyFirst, value);
-            }
 
-        } else {
-            System.err.println("Offset is negative");
-            closeFile(dataBase);
-            System.exit(1);
+            } else {
+                System.err.println("Offset is negative");
+                closeFile();
+                System.exit(1);
+            }
         }
     }
 
-    static void writeInDatabase() throws IOException {
-        dataBase = new RandomAccessFile(workingDirectory + File.separator + "db.dat", "rw");
+    protected void writeInDatabase(File tmp) throws IOException {
+        dataBase = new RandomAccessFile(tmp.getAbsolutePath(), "rw");
         int lengthOfkeys = 0;
         Set<Map.Entry<String, String>> set = fileMap.entrySet();
         for (Map.Entry<String, String> entry : set) {
@@ -156,6 +138,36 @@ public class FileMap {
         for (Map.Entry<String, String> myEntry : set) {
             dataBase.write(myEntry.getValue().getBytes("UTF-8"));
         }
-        closeFile(dataBase);
+        closeFile();
+    }
+
+    public void exec(String[] input) {
+        Commands cmd = new Commands();
+        switch (input[0]) {
+        case "put":
+            if (input.length == 3) {
+                cmd.put(input[1], input[2]);
+            } else {
+                cmd.printError("Incorrect number of arguments");
+            }
+            break;
+        case "get":
+            if (input.length == 2) {
+                cmd.get(input[1]);
+            } else {
+                cmd.printError("Incorrect number of arguments");
+            }
+            break;
+        case "remove":
+            if (input.length == 2) {
+                cmd.remove(input[1]);
+            } else {
+                cmd.printError("Incorrect number of arguments");
+            }
+            break;
+        default:
+            cmd.printError("Incorrect input");
+        }
+
     }
 }
