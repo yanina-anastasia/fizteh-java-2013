760579aad23d56528b9425ce536e73f3b2bed96a
Pingvin786 <pingvin786@mail.ru>
2013-10-26 21:07:18 +0400

    test #1

--- src/ru/fizteh/fivt/students/ermolenko/MultiFileHashMap/MultiFileHashMapMain.java
+++ src/ru/fizteh/fivt/students/ermolenko/MultiFileHashMap/MultiFileHashMapMain.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class MultiFileHashMapMain {
 
     public static void main(String[] args) throws IOException {
-
+        System.out.println("MAIN");
         //String currentProperty = "/Users/evgenij/Documents/JAVA_Ex/fizteh-java-2013/src/ru/fizteh/fivt/students/ermolenko/multifilehashmap/folder/";
         String currentProperty = System.getProperty("fizteh.db.dir");
         File base = new File(currentProperty);
