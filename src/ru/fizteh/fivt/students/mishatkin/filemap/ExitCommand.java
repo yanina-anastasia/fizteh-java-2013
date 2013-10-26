9437eacdf3dc6ede70c0851651e784f976a4cb26
Vladimir Mishatkin <sane.vova@gmail.com>
2013-10-26 13:50:29 +0400

    [FileMap] Black magic trick.

--- src/ru/fizteh/fivt/students/mishatkin/filemap/ExitCommand.java
+++ src/ru/fizteh/fivt/students/mishatkin/filemap/ExitCommand.java
@@ -2,6 +2,7 @@ package ru.fizteh.fivt.students.mishatkin.filemap;
 
 import ru.fizteh.fivt.students.mishatkin.shell.ShellCommand;
 import ru.fizteh.fivt.students.mishatkin.shell.ShellReceiver;
+import ru.fizteh.fivt.students.mishatkin.shell.TimeToExitException;
 
 /**
  * Created by Vladimir Mishatkin on 10/15/13
@@ -12,8 +13,8 @@ public class ExitCommand extends ShellCommand {
 		setInputArgumentsCount(0);
 	}
 
-//	@Override
-//	public void execute() throws TimeToExitException {
-//		receiver.exitCommand();
-//	}
+	@Override
+	public void execute() throws TimeToExitException {
+		receiver.exitCommand();
+	}
 }
