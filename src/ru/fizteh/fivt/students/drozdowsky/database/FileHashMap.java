b4ae2ffeaf9be108407b0e6c3ed0250fad0f7546
Taygrim <tagrimar@mail.ru>
2013-10-26 08:29:59 +0400

    commit

--- src/ru/fizteh/fivt/students/drozdowsky/database/FileHashMap.java
+++ src/ru/fizteh/fivt/students/drozdowsky/database/FileHashMap.java
@@ -103,7 +103,10 @@ public class FileHashMap {
     } */
 
     private int getDirNum(String key) {
-        byte b = key.getBytes()[0];
+        int b = key.getBytes()[0];
+        if (b < 0) {
+            b += 256;
+        }
         int nDir = b % 16;
         int nFile = (b / 16) % 16;
         return nDir;
@@ -111,6 +114,9 @@ public class FileHashMap {
 
     private int getFileNum(String key) {
         byte b = key.getBytes()[0];
+        if (b <= 0) {
+            b += 256;
+        }
         int nDir = b % 16;
         int nFile = (b / 16) % 16;
         return nFile;
