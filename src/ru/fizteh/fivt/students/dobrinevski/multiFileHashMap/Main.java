cb158e6691a51c56c3ad9d619ee06774b5c07f24
Kangar0o <88888777771@mail.ru>
2013-10-26 15:38:51 +0400

    multiFileHashMap 1

---
+++ src/ru/fizteh/fivt/students/dobrinevski/multiFileHashMap/Main.java
@@ -0,0 +1,50 @@
+package ru.fizteh.fivt.students.dobrinevski.multiFileHashMap;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    private static File dbsDir;
+    private static MyHashMap dtb;
+    public static void main(String[] args) throws IOException {
+        try {
+            dbsDir = new File(System.getProperty("fizteh.db.dir"));
+            if (!dbsDir.isDirectory()) {
+                throw new Exception(dbsDir + " doesn't exist or is not a directory");
+            }
+            File[] files = dbsDir.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if(file.isFile()) {
+                    throw new Exception("It's a file in a root directory.");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error while opening database: " + (e.getMessage()));
+            System.exit(1);
+        }
+
+        dtb = new MyHashMap();
+        Shell sl = new Shell(dtb);
+        if (args.length > 0) {
+            StringBuilder builder = new StringBuilder();
+            for (String arg : args) {
+                builder.append(arg).append(' ');
+            }
+
+            try {
+                sl.executeCommands(builder.toString());
+            } catch (SException e) {
+                System.err.println(e);
+                System.exit(1);
+            }
+            catch (Exception e) {
+                System.err.println(e);
+                System.exit(1);
+            }
+        } else {
+            sl.iMode();
+        }
+    }
+}
---
+++ src/ru/fizteh/fivt/students/dobrinevski/multiFileHashMap/MyHashMap.java
@@ -0,0 +1,169 @@
+package ru.fizteh.fivt.students.dobrinevski.multiFileHashMap;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.nio.ByteBuffer;
+import java.io.FileInputStream;
+
+
+
+public class MyHashMap {
+    private File curTable = null;
+    private HashMap<Integer, HashMap<String, String> > dataBase = null;
+
+    private void parseFile(File dbFile, int firstControlValue, int secondControlValue) throws Exception {
+        FileInputStream fstream = new FileInputStream(dbFile);
+        while (fstream.available() > 0) {
+            Map.Entry<String, String> newEntry = parseEntry(fstream);
+            Integer hashCode = newEntry.getKey().hashCode();
+            Integer nDirectory = hashCode % 16;
+            Integer nFile = hashCode / 16 % 16;
+            if(firstControlValue != nDirectory || secondControlValue != nFile) {
+                throw new Exception("Error: bad file");
+            }
+            dataBase.get(nFile * 16 + nDirectory).put(newEntry.getKey(), newEntry.getValue());
+        }
+        dbFile.delete();
+    }
+
+    private static Map.Entry<String, String> parseEntry(FileInputStream fstream) throws Exception {
+        byte[] Buf = new byte[4];
+        Read(fstream, Buf, 4);
+        int keySize = ByteBuffer.wrap(Buf).getInt();
+        Read(fstream, Buf, 4);
+        int valueSize = ByteBuffer.wrap(Buf).getInt();
+        if (keySize <= 0 || valueSize <= 0 || keySize > (1 << 24) || valueSize > (1 << 24)) {
+            throw new Exception("Error: bad file");
+        }
+        byte[] kBuf = new byte[keySize];
+        Read(fstream, kBuf, keySize);
+        byte[] vBuf = new byte[valueSize];
+        Read(fstream, vBuf, valueSize);
+        return new AbstractMap.SimpleEntry<String, String>(new String(kBuf, "UTF-8"), new String(vBuf, "UTF-8"));
+    }
+
+    private static void Read(FileInputStream fstream, byte[] buf, int readCount) throws Exception {
+        int readed = 0;
+        if (readCount < 0) {
+            throw new Exception("Error: bad file");
+        }
+        while (readed < readCount) {
+            int readNow = fstream.read(buf, readed, readCount - readed);
+            if (readNow < 0) {
+                throw new Exception("Error: bad file");
+            }
+            readed += readNow;
+        }
+    }
+
+    public void use(String args) throws Exception {
+        Path dbsDir = Paths.get(System.getProperty("fizteh.db.dir")).resolve(args).normalize();
+        if(Files.notExists(dbsDir) || !Files.isDirectory(dbsDir)) {
+            System.out.println("tablename not exists");
+            return;
+        }
+        writeOut();
+        curTable = new File(System.getProperty("fizteh.db.dir") + File.separator + args);
+        System.out.println("using " + args);
+    }
+
+    public void get(String args) throws Exception {
+        if(curTable == null) {
+        System.out.println("no table") ;
+        }
+        Integer hashCode = args.hashCode();
+        Integer nDirectory = hashCode % 16;
+        Integer nFile = hashCode / 16 % 16;
+
+        File dbFile = new File(curTable.getCanonicalPath() + File.separator + nDirectory.toString() + ".dir"
+                + File.separator + nFile.toString() + ".dat");
+        if(dbFile.exists() && dbFile.isFile()) {
+            parseFile(dbFile, nDirectory, nFile);
+        }
+        String Value = dataBase.get(nDirectory * 16 + nFile).get(args);
+        if(Value == null) {
+            System.out.println("not found");
+        } else {
+            System.out.println("found");
+            System.out.println(Value);
+        }
+
+    }
+
+    public void put(String[] args) throws  Exception {
+        if(curTable == null) {
+            System.out.println("no table") ;
+        }
+        Integer hashCode = args[1].hashCode();
+        Integer nDirectory = hashCode % 16;
+        Integer nFile = hashCode / 16 % 16;
+
+        File dbFile = new File(curTable.getCanonicalPath() + File.separator + nDirectory.toString() + ".dir"
+                + File.separator + nFile.toString() + ".dat");
+        if(dbFile.exists() && dbFile.isFile()) {
+            parseFile(dbFile, nDirectory, nFile);
+        }
+        if(!dataBase.get(nDirectory * 16 + nFile).containsKey(args[1])) {
+            System.out.println("overwrite");
+        }
+        String value = dataBase.get(nDirectory * 16 + nFile).put(args[1], args[2]);
+        System.out.println(value);
+
+    }
+
+    public void remove(String args) throws  Exception {
+        if(curTable == null) {
+            System.out.println("no table") ;
+        }
+        Integer hashCode = args.hashCode();
+        Integer nDirectory = hashCode % 16;
+        Integer nFile = hashCode / 16 % 16;
+
+        File dbFile = new File(curTable.getCanonicalPath() + File.separator + nDirectory.toString() + ".dir"
+                + File.separator + nFile.toString() + ".dat");
+        if(dbFile.exists() && dbFile.isFile()) {
+            parseFile(dbFile, nDirectory, nFile);
+        }
+        if(dataBase.get(nDirectory * 16 + nFile).remove(args) == null) {
+            System.out.println("not found");
+        } else {
+            System.out.println("removed");
+        }
+    }
+
+    private void writeOut() throws IOException {
+        for(Integer i = 0; i < 16; i++ ) {
+            for(Integer j = 0; j < 16; j++) {
+                if(!dataBase.get(i * 16 + j).isEmpty()) {
+                    File workFile = new File(curTable.getCanonicalPath()
+                            + File.separator + i.toString() + ".dir"
+                            + File.separator + j.toString() + ".dat");
+                    FileOutputStream fstream = new FileOutputStream(workFile);
+                    for (Map.Entry<String, String> entry : dataBase.get(16 * i + j).entrySet()) {
+                        writeEntry(entry, fstream);
+                    }
+                }
+            }
+        }
+    }
+
+    public void exit() throws IOException {
+             writeOut();
+    }
+
+    private static void writeEntry(Map.Entry<String, String> e, FileOutputStream fstream) throws IOException {
+        byte[] keyBuf = e.getKey().getBytes("UTF-8");
+        byte[] valueBuf = e.getValue().getBytes("UTF-8");
+        fstream.write(ByteBuffer.allocate(4).putInt(keyBuf.length).array());
+        fstream.write(ByteBuffer.allocate(4).putInt(valueBuf.length).array());
+        fstream.write(keyBuf);
+        fstream.write(valueBuf);
+    }
+}
