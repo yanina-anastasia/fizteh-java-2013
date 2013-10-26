2ee6d2d2baab9e0f72020fb422f64c2b900263f2
Vladimir Mishatkin <sane.vova@gmail.com>
2013-10-26 06:52:28 +0400

    [FileMap] Unified receiver class for filemap exit command as temporary solution.

--- src/ru/fizteh/fivt/students/mishatkin/filemap/ExitCommand.java
+++ src/ru/fizteh/fivt/students/mishatkin/filemap/ExitCommand.java
@@ -1,12 +1,13 @@
 package ru.fizteh.fivt.students.mishatkin.filemap;
 
 import ru.fizteh.fivt.students.mishatkin.shell.ShellCommand;
+import ru.fizteh.fivt.students.mishatkin.shell.ShellReceiver;
 
 /**
  * Created by Vladimir Mishatkin on 10/15/13
  */
 public class ExitCommand extends ShellCommand {
-	public ExitCommand(FileMapReceiver receiver) {
+	public ExitCommand(ShellReceiver receiver) {
 		super(receiver);
 		setInputArgumentsCount(0);
 	}
