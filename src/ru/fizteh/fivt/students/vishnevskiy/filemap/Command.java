3022390ca0f1e498de1645ba9d6f5091c49cdac4
idvish <ivan.vishnevskiy@phystech.edu>
2013-10-28 19:42:06 +0400

    Stuff removed

--- src/ru/fizteh/fivt/students/vishnevskiy/filemap/Command.java
+++
@@ -1,7 +0,0 @@
-package ru.fizteh.fivt.students.vishnevskiy.filemap;
-
-public interface Command {
-    String getName();
-    int getArgsNum();
-    void execute(SingleFileMap singleFileMap, String[] args) throws FileMapException;
-}
