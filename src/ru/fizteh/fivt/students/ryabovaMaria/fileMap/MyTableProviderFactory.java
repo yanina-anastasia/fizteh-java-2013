00f9b8e0cfeeb35601ddcaa2a61420f987c57de4
RyabovaMaria <liastramv@gmail.com>
2013-10-31 19:13:09 +0400

    JUnit, first commit

---
+++ src/ru/fizteh/fivt/students/ryabovaMaria/fileMap/MyTableProviderFactory.java
@@ -0,0 +1,27 @@
+package ru.fizteh.fivt.students.ryabovaMaria.fileMap;
+
+import java.io.File;
+import ru.fizteh.fivt.storage.strings.TableProvider;
+import ru.fizteh.fivt.storage.strings.TableProviderFactory;
+
+public class MyTableProviderFactory implements TableProviderFactory {
+    public TableProvider myTableProvider;
+    private File curDir;
+    
+    @Override
+    public TableProvider create(String dir) {
+        String getPropertyString = System.getProperty(dir);
+        if (getPropertyString == null) {
+            throw new IllegalArgumentException("Bad property");
+        }
+        curDir = new File(getPropertyString);
+        if (!curDir.exists()) {
+            throw new IllegalArgumentException("Directory doesn't exists");
+        }
+        if (!curDir.isDirectory()) {
+            throw new IllegalArgumentException("Argument is not a directory");
+        }
+        myTableProvider = new TableProviderCommands(curDir);
+        return myTableProvider;
+    }
+}
---
+++ src/ru/fizteh/fivt/students/ryabovaMaria/fileMap/TableProviderCommands.java
@@ -0,0 +1,71 @@
+package ru.fizteh.fivt.students.ryabovaMaria.fileMap;
+
+import java.io.File;
+import ru.fizteh.fivt.storage.strings.Table;
+import ru.fizteh.fivt.storage.strings.TableProvider;
+import ru.fizteh.fivt.students.ryabovaMaria.shell.ShellCommands;
+
+public class TableProviderCommands implements TableProvider {
+    private File curDir;
+    private File tableDir;
+    public Table myTable;
+    
+    TableProviderCommands(File tablesDir) {
+        curDir = tablesDir;
+    }
+    
+    private void isCorrectArgument(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("null argument");
+        }
+        if (name.contains("/")) {
+            throw new IllegalArgumentException("argument contains illegal symbols");
+        }
+        tableDir = curDir.toPath().resolve(name).normalize().toFile();
+    }
+    
+    @Override
+    public Table getTable(String name) {
+        isCorrectArgument(name);
+        if (!tableDir.exists()) {
+            throw new IllegalArgumentException(name + " doesn't exists");
+        }
+        if (!tableDir.isDirectory()) {
+            throw new IllegalArgumentException(name + " is not a directory");
+        }
+        myTable = new TableCommands(tableDir);
+        return myTable;
+    }
+
+    @Override
+    public Table createTable(String name) {
+        isCorrectArgument(name);
+        if (tableDir.exists()) {
+            return null;
+        }
+        if (!tableDir.mkdir()) {
+            throw new IllegalArgumentException(name + " cannot be created");
+        } else {
+            myTable = new TableCommands(tableDir);
+            return myTable;
+        }
+    }
+
+    @Override
+    public void removeTable(String name) {
+        isCorrectArgument(name);
+        if (!tableDir.isDirectory()) {
+            throw new IllegalStateException(name + " cannot be deleted");
+        }
+        ShellCommands shellCommands = new ShellCommands();
+        shellCommands.lexems = new String[2];
+        shellCommands.lexems[1] = name;
+        shellCommands.currentDir = curDir;
+        try {
+            shellCommands.rm();
+        } catch (Exception e) {
+            throw new IllegalStateException(name + " cannot be deleted");
+        }
+    }
+    
+}
