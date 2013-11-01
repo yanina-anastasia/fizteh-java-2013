616ff820064170b756c3d81e66da3c788195f3c0
paulin-mipt <paulin.mipt@gmail.com>
2013-11-02 01:50:59 +0400

    +1

--- src/ru/fizteh/fivt/students/paulinMatavina/filemap/MultiDbUse.java
+++ src/ru/fizteh/fivt/students/paulinMatavina/filemap/MultiDbUse.java
@@ -7,20 +7,19 @@ public class MultiDbUse implements Command {
     @Override
     public int execute(String[] args, State state) {
         String dbName = args[0];
-        MyTableProvider multiState = (MyTableProvider) state;
-        if (multiState.getCurrTable() != null) {
-            int chNum = ((MultiDbState) multiState.getCurrTable()).changesNum;
-            if (chNum > 0) {
-                System.out.println(chNum + " uncommited changes");
-                return 0;
-            }
-        }     
-        
+        MyTableProvider multiState = (MyTableProvider) state;    
         Table table = multiState.getTable(dbName);
         
         if (table == null) {
             System.out.println(dbName + " not exists");
         } else {
+            if (multiState.getCurrTable() != null) {
+                int chNum = ((MultiDbState) multiState.getCurrTable()).changesNum;
+                if (chNum > 0) {
+                    System.out.println(chNum + " uncommited changes");
+                    return 0;
+                }
+            } 
             multiState.currTableName = dbName;
             System.out.println("using " + dbName);
         }
