d50104417f74df191a9b7fe03a4b3542785e9a55
dubov94 <dubov94@gmail.com>
2013-11-02 06:11:30 +0400

    Typo

--- src/ru/fizteh/fivt/students/dubovpavel/strings/IsKeyAllowable.java
+++ src/ru/fizteh/fivt/students/dubovpavel/strings/IsKeyAllowable.java
@@ -3,7 +3,7 @@ package ru.fizteh.fivt.students.dubovpavel.strings;
 import java.util.regex.Pattern;
 
 public class IsKeyAllowable {
-    private static Pattern allowable = Pattern.compile("\\w");
+    private static Pattern allowable = Pattern.compile("\\w+");
     public static boolean check(String key) {
         return allowable.matcher(key).matches();
     }
