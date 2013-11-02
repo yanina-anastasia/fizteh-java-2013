7f6dde2c72207c26a476d4facbaa6cb7352a8cd5
Elena Vorobyeva <good-mark@mail.ru>
2013-11-03 03:20:53 +0400

    Some RuntimeExceptions

--- src/ru/fizteh/fivt/students/elenav/multifilemap/MultiFileMapProvider.java
+++ src/ru/fizteh/fivt/students/elenav/multifilemap/MultiFileMapProvider.java
@@ -11,6 +11,8 @@ import ru.fizteh.fivt.students.elenav.utils.Functions;
 
 public class MultiFileMapProvider implements TableProvider {
 
+	private final String NAME_FORMAT = "[a-zA-Zà-ßÀ-ß0-9]+";
+	
 	private File workingDirectory = null;
 	private PrintStream stream;
 	public HashMap<String, MultiFileMapState> tables = new HashMap<>();
@@ -29,16 +31,22 @@ public class MultiFileMapProvider implements TableProvider {
 
 	@Override
 	public Table getTable(String name) {
-		if (name == null) {
+		if (name == null || name.trim().isEmpty()) {
 			throw new IllegalArgumentException("can't get table with null name");
+		}
+		if (!name.matches(NAME_FORMAT)) {
+			throw new RuntimeException("can't get table with invalid name");
 		}
 		return tables.get(name);
 	}
 	
 	@Override
 	public MultiFileMapState createTable(String name) {
-		if (name == null) {
+		if (name == null || name.trim().isEmpty()) {
 			throw new IllegalArgumentException("can't create table with null name");
+		}
+		if (!name.matches(NAME_FORMAT)) {
+			throw new RuntimeException("can't create table with invalid name");
 		}
 		File f = new File(getWorkingDirectory(), name);
 		if (f.exists()) {
@@ -52,11 +60,14 @@ public class MultiFileMapProvider implements TableProvider {
 	
 	@Override
 	public void removeTable(String name) {
-		if (name == null) {
+		if (name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException("can't remove table: invalid name");
         }
 		if (tables.get(name) == null) {
-			throw new IllegalStateException("can't remove table: table not exist");
+			throw new IllegalArgumentException("can't remove table: table not exist");
+		}
+		if (!name.matches(NAME_FORMAT)) {
+			throw new RuntimeException("can't remove table with invalid name");
 		}
 		try {
 			Functions.deleteRecursively(tables.get(name).getWorkingDirectory());
