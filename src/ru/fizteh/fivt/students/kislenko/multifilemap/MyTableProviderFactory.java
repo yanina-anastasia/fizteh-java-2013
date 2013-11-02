8d66453b55cafeb9859e86e5d1b17dd18d5f3f1d
ignorer <ignorer@bk.ru>
2013-11-02 09:28:54 +0400

    Testing.

--- src/ru/fizteh/fivt/students/kislenko/multifilemap/MyTableProviderFactory.java
+++ src/ru/fizteh/fivt/students/kislenko/multifilemap/MyTableProviderFactory.java
@@ -2,13 +2,18 @@ package ru.fizteh.fivt.students.kislenko.multifilemap;
 
 import ru.fizteh.fivt.storage.strings.TableProviderFactory;
 
+import java.io.File;
+
 public class MyTableProviderFactory implements TableProviderFactory {
     @Override
     public MyTableProvider create(String dir) {
         if (dir == null || dir.equals("")) {
             throw new IllegalArgumentException("Incorrect database name.");
         }
-        MyTableProvider provider = new MyTableProvider();
-        return provider;
+        File file = new File(dir);
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException("Database must be a directory.");
+        }
+        return new MyTableProvider();
     }
 }
