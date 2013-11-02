f3e6065f56af94a60823e956e6b88c0d66232140
Kangar0o <88888777771@mail.ru>
2013-11-02 10:37:31 +0400

    fix k

--- src/ru/fizteh/fivt/students/dobrinevski/shell/Shell.java
+++ src/ru/fizteh/fivt/students/dobrinevski/shell/Shell.java
@@ -43,19 +43,20 @@ public class Shell {
         }
     }
 
-    private void removeFile(String[] args) throws Exception {
-
-        checkLen(args[0], args.length - 1, 1);
+    public void removeFile(String[] args) throws Exception {
         Path pathToRemove = currentDir.toPath().resolve(args[1]).normalize();
         if (!Files.exists(pathToRemove)) {
-            throw new SException(args[0], "Cannot be removed: File does not exist");
+            throw new Exception(args[0], "Cannot be removed: File does not exist");
         }
         if (currentDir.toPath().normalize().startsWith(pathToRemove)) {
-            throw new SException(args[0], "\'" + args[1] +
+            throw new Exception(args[0], "\'" + args[1] +
                     "\': Cannot be removed: First of all, leave this directory");
         }
 
-        File fileToRemove = new File(pathAppend(args[1]));
+        File fileToRemove = new File(args[1]);
+        if (!fileToRemove.isAbsolute()) {
+            fileToRemove = new File(parentShell.currentDir.getCanonicalPath() + File.separator + args[1]);
+        }
         File[] filesToRemove = fileToRemove.listFiles();
         if (filesToRemove != null) {
             for (File file : filesToRemove) {
@@ -65,14 +66,14 @@ public class Shell {
                     toRemove[1] = file.getPath();
                     removeFile(toRemove);
                 } catch (Exception e) {
-                    throw new SException(args[0], "\'" + file.getCanonicalPath()
+                    throw new Exception(args[0], "\'" + file.getCanonicalPath()
                             + "\' : File cannot be removed: " + e.getMessage() + " ");
                 }
             }
         }
 
         if (!Files.deleteIfExists(pathToRemove)) {
-            throw new SException(args[0], "\'" + fileToRemove.getCanonicalPath()
+            throw new Exception(args[0], "\'" + fileToRemove.getCanonicalPath()
                     + "\' : File cannot be removed ");
         }
     }
