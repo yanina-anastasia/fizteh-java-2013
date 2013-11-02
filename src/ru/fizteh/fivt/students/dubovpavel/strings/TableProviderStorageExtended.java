ca145fd7a321b5be321178d330606786772d5518
dubov94 <dubov94@gmail.com>
2013-11-02 06:51:42 +0400

    TableBadSymbolShouldFail

--- src/ru/fizteh/fivt/students/dubovpavel/strings/TableProviderStorageExtended.java
+++ src/ru/fizteh/fivt/students/dubovpavel/strings/TableProviderStorageExtended.java
@@ -4,8 +4,23 @@ import ru.fizteh.fivt.storage.strings.Table;
 import ru.fizteh.fivt.storage.strings.TableProvider;
 import ru.fizteh.fivt.students.dubovpavel.multifilehashmap.Storage;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TableProviderStorageExtended implements TableProvider{
     Storage<WrappedMindfulDataBaseMultiFileHashMap> storage;
+
+    private boolean isNameValid(String name) {
+        File litmus = new File(name);
+        try {
+            if(!litmus.getCanonicalFile().getName().equals(name)) {
+                return false;
+            }
+        } catch(IOException e) {
+            return false;
+        }
+        return true;
+    }
 
     public TableProviderStorageExtended(Storage storage) {
         this.storage = storage;
@@ -15,12 +30,18 @@ public class TableProviderStorageExtended implements TableProvider{
         if(name == null || name.equals("")) {
             throw new IllegalArgumentException();
         }
+        if(!isNameValid(name)) {
+            throw new RuntimeException();
+        }
         return storage.getDataBase(name);
     }
 
     public Table createTable(String name) {
         if(name == null || name.equals("")) {
             throw new IllegalArgumentException();
+        }
+        if(!isNameValid(name)) {
+            throw new RuntimeException();
         }
         return storage.create(name);
     }
