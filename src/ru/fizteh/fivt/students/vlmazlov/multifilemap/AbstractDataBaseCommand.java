5f4e23d69747c8a9908f04561dcc40dd1f3a5620
VlMazlov <vlmazlov@gmail.com>
2013-11-02 04:08:53 +0400

    Fixed

--- src/ru/fizteh/fivt/students/vlmazlov/multifilemap/AbstractDataBaseCommand.java
+++ src/ru/fizteh/fivt/students/vlmazlov/multifilemap/AbstractDataBaseCommand.java
@@ -1,7 +1,6 @@
 package ru.fizteh.fivt.students.vlmazlov.multifilemap;
 
 import ru.fizteh.fivt.students.vlmazlov.shell.AbstractCommand;
-import ru.fizteh.fivt.students.vlmazlov.multifilemap.DataBaseState;
 
 public abstract class AbstractDataBaseCommand extends AbstractCommand<DataBaseState> {
 	public AbstractDataBaseCommand(String name, int argNum) {
