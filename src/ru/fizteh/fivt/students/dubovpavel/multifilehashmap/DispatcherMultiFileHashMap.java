7bb54daf9daaef9a61baf0b7c18ec790f2803b14
dubov94 <dubov94@gmail.com>
2013-11-02 05:25:29 +0400

    Cradle

--- src/ru/fizteh/fivt/students/dubovpavel/multifilehashmap/DispatcherMultiFileHashMap.java
+++ src/ru/fizteh/fivt/students/dubovpavel/multifilehashmap/DispatcherMultiFileHashMap.java
@@ -12,7 +12,7 @@ public class DispatcherMultiFileHashMap extends Dispatcher implements DataBaseAc
             try {
                 storage = new Storage(getInitProperty(path), this, builder);
             } catch (DispatcherException e) {
-                storage = null;
+                System.exit(-1);
             }
         } else {
             storage = new Storage(path, this, builder);
