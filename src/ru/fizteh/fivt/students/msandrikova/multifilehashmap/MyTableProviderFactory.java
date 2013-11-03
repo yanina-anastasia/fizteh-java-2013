72a786258d7c6094e5ca4cc22091bfc7c99a14fd
Sandrikova Maria <mashasandrikova@gmail.com>
2013-11-03 21:26:46 +0400

    MyTableProviderFactory

--- src/ru/fizteh/fivt/students/msandrikova/multifilehashmap/MyTableProviderFactory.java
+++ src/ru/fizteh/fivt/students/msandrikova/multifilehashmap/MyTableProviderFactory.java
@@ -2,20 +2,13 @@ package ru.fizteh.fivt.students.msandrikova.multifilehashmap;
 
 import java.io.File;
 
-import ru.fizteh.fivt.storage.strings.TableProvider;
-import ru.fizteh.fivt.storage.strings.TableProviderFactory;
-
-public class MyTableProviderFactory implements TableProviderFactory {
-	
-
-	public MyTableProviderFactory() {}
-
+public class MyTableProviderFactory implements ChangesCountingTableProviderFactory {
 	@Override
-	public TableProvider create(String dir) throws IllegalArgumentException {
+	public ChangesCountingTableProvider create(String dir) throws IllegalArgumentException {
 		if(dir == null) {
 			throw new IllegalArgumentException("Directory can not be null.");
 		}
-		TableProvider newTableProvider = null;
+		ChangesCountingTableProvider newTableProvider = null;
 		try {
 			newTableProvider = new MyTableProvider(new File(dir));
 		} catch (IllegalArgumentException e) {
