ad96cb3617c46051c27f5d9260d35c9d929546b1
Sandrikova Maria <mashasandrikova@gmail.com>
2013-11-03 21:49:41 +0400

    Exit command

--- src/ru/fizteh/fivt/students/msandrikova/shell/ExitCommand.java
+++ src/ru/fizteh/fivt/students/msandrikova/shell/ExitCommand.java
@@ -1,9 +1,5 @@
 package ru.fizteh.fivt.students.msandrikova.shell;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import ru.fizteh.fivt.storage.strings.Table;
 
 public class ExitCommand extends Command {
 
@@ -12,28 +8,14 @@ public class ExitCommand extends Command {
 	}
 	
 	@Override
-	public void execute(String[] argumentsList, Shell myShell) {
-		if(!super.getArgsAcceptor(argumentsList.length - 1, myShell.getIsInteractive())) {
+	public void execute(String[] argumentsList, Shell shell) {
+		if(!super.getArgsAcceptor(argumentsList.length - 1, shell.getIsInteractive())) {
 			return;
 		}
-		if(myShell.getState().getIsFileMap()) {
-			if(myShell.getState().getDBMap() != null) {
-				DBMap curDBMap = myShell.getState().getDBMap();
-				try {
-					curDBMap.writeFile();
-				} catch (FileNotFoundException e) {
-					Utils.generateAnError("Fatal error during writing", "DBMap", false);
-				} catch (IOException e) {
-					Utils.generateAnError("Fatal error during writing", "DBMap", false);
-				}
-			}
+		if(shell.getState().currentTable != null) {
+			shell.getState().currentTable.commit();
 		}
-		if(myShell.getState().getIsMultiFileHashMap()) {
-			if(myShell.getState().getCurrentTable() != null) {
-				Table currentTable = myShell.getState().getCurrentTable();
-				currentTable.commit();
-			}
-		}
+		
 		Thread.currentThread().interrupt();
 	}
 }
