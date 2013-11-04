5a3a537155801c27694b5d7e8edb8b7b25202247
Sandrikova Maria <mashasandrikova@gmail.com>
2013-11-04 11:22:19 +0400

    Tests

--- src/ru/fizteh/fivt/students/msandrikova/multifilehashmap/MyTableTest.java
+++ src/ru/fizteh/fivt/students/msandrikova/multifilehashmap/MyTableTest.java
@@ -1,12 +1,12 @@
 package ru.fizteh.fivt.students.msandrikova.multifilehashmap;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.io.File;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import ru.fizteh.fivt.students.msandrikova.shell.Utils;
@@ -30,6 +30,7 @@ public class MyTableTest {
 	public void setUp() throws Exception {
 		path = new File(System.getProperty("user.home"), "sandbox");
 		clear();
+		new File(path, "tableName").mkdirs();
 		table = new MyTable(path, "tableName");
 	}
 
@@ -39,34 +40,38 @@ public class MyTableTest {
 	}
 
 	@Test
-	public void testPutGet() {
-		table.put("key", "value");
+	public void testPutGetOverwrite() {
+		assertNull(table.put("key", "value"));
 		assertEquals(table.get("key"), "value");
+		assertEquals(table.put("key", "newvalue"), "value");
+		assertEquals(table.get("key"), "newvalue");
 	}
 
 	@Test
 	public void testRemove() {
-		
+		assertNull(table.put("key", "value"));
+		assertEquals(table.remove("key"), "value");
+		assertNull(table.get("key"));
 	}
 
 	@Test
 	public void testSize() {
-		
+		assertNull(table.put("key", "value"));
+		assertEquals(table.size(), 1);
+		assertEquals(table.put("key", "newvalue"), "value");
+		assertEquals(table.remove("key"), "newvalue");
+		assertEquals(table.size(), 0);
 	}
 
 	@Test
-	public void testCommit() {
-		
+	public void testCommitRollback() {
+		assertNull(table.put("key", "value"));
+		assertEquals(table.commit(), 1);
+		assertEquals(table.remove("key"), "value");
+		assertEquals(table.put("key", "value"), null);
+		assertEquals(table.rollback(), 0);
+		assertNull(table.put("stuff", "stuff"));
+		assertEquals(table.rollback(), 1);
+		assertNull(table.get("stuff"));
 	}
-
-	@Test
-	public void testRollback() {
-		
-	}
-
-	@Test
-	public void testUnsavedChangesCount() {
-		
-	}
-
 }
