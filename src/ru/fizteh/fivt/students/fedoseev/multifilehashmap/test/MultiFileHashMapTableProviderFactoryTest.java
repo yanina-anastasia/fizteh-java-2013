48b27fe1d06844b83fe24badb663dee284c7c0a5
KFedoseev <kirill.fedoseev@phystech.edu>
2013-11-05 03:10:57 +0400

    .

--- src/ru/fizteh/fivt/students/fedoseev/multifilehashmap/test/MultiFileHashMapTableProviderFactoryTest.java
+++ src/ru/fizteh/fivt/students/fedoseev/multifilehashmap/test/MultiFileHashMapTableProviderFactoryTest.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class MultiFileHashMapTableProviderFactoryTest {
     private MultiFileHashMapTableProviderFactory tpf;
-    File db;
+    private File db;
 
     public MultiFileHashMapTableProviderFactoryTest() throws IOException {
         tpf = new MultiFileHashMapTableProviderFactory();
