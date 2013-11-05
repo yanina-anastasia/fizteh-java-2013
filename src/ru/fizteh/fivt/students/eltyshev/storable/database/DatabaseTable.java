b6acad96ae6a75560df0c357a896991bed840bdd
Evgeny Eltishev <evgeny.eltishev@phystech.edu>
2013-11-05 23:42:49 +0400

    fixes

--- src/ru/fizteh/fivt/students/eltyshev/storable/database/DatabaseTable.java
+++ src/ru/fizteh/fivt/students/eltyshev/storable/database/DatabaseTable.java
@@ -54,21 +54,7 @@ public class DatabaseTable extends AbstractStorage<String, Storeable> implements
 
 
         if (!checkAlienStoreable(value)) {
-            for(int i = 0; i < getColumnsCount(); ++i)
-            {
-                System.out.println(getColumnType(i).getName());
-            }
-            try {
-                for(int index = 0; index < 10; ++index)
-                {
-                    System.out.println(value.getColumnAt(index));
-                }
-            } catch (IndexOutOfBoundsException e) {
-                return value;
-            }
-
-            return value;
-            //throw new ColumnFormatException("alien storeable");
+            throw new ColumnFormatException("alien storeable");
         }
         checkCorrectStoreable(value);
 
