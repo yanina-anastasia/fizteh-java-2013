7d8529856f1d6e0278f0e5517eb96f480b47d7df
Dmitry Zvonarev <dmitry.zvonorev@phystech.edu>
2013-11-05 23:34:12 +0400

    tests fixed

--- src/ru/fizteh/fivt/students/dzvonarev/filemap/MyTableTests.java
+++ src/ru/fizteh/fivt/students/dzvonarev/filemap/MyTableTests.java
@@ -2,19 +2,25 @@ package ru.fizteh.fivt.students.dzvonarev.filemap;
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import ru.fizteh.fivt.storage.strings.Table;
+import ru.fizteh.fivt.storage.strings.TableProvider;
 
 public class MyTableTests {
 
     private Table table;
+    private TableProvider provider;
+
+    @Before
+    public void test() {
+        MyTableProviderFactory factory = new MyTableProviderFactory();
+        provider = factory.create(System.getProperty("fizteh.db.dir"));
+        table = provider.createTable("my");
+    }
 
     @Test
     public void testCommitRollback() {
-        MyTableProviderFactory factory = new MyTableProviderFactory();
-        MyTableProvider provider;
-        provider = factory.create(System.getProperty("fizteh.db.dir"));
-        table = provider.createTable("my");
         Assert.assertNull(table.put("commit", "rollback"));
         Assert.assertEquals(table.get("commit"), "rollback");
         Assert.assertEquals(table.rollback(), 1);
@@ -26,14 +32,11 @@ public class MyTableTests {
         Assert.assertNull(table.put("commit", "rollback1"));
         Assert.assertEquals(table.commit(), 1);
         Assert.assertEquals(table.get("commit"), "rollback1");
+        provider.removeTable("my");
     }
 
     @Test
     public void testCommitWithNoChanges() {
-        MyTableProviderFactory factory = new MyTableProviderFactory();
-        MyTableProvider provider;
-        provider = factory.create(System.getProperty("fizteh.db.dir"));
-        table = provider.createTable("my1");
         Assert.assertNull(table.put("no_changes", "will_be_deleted_soon"));
         Assert.assertEquals(table.remove("no_changes"), "will_be_deleted_soon");
         Assert.assertEquals(table.commit(), 0);
@@ -42,40 +45,33 @@ public class MyTableTests {
         Assert.assertEquals(table.put("key", "value_new"), "value");
         Assert.assertEquals(table.put("key", "value"), "value_new");
         Assert.assertEquals(table.commit(), 0);
+        provider.removeTable("my");
     }
 
     @Test
     public void checkSize() {
-        MyTableProviderFactory factory = new MyTableProviderFactory();
-        MyTableProvider provider;
-        provider = factory.create(System.getProperty("fizteh.db.dir"));
-        table = provider.createTable("my2");
         table.put("key", "value");
         table.remove("key");
         table.put("newKey", "value");
         Assert.assertEquals("Incorrect size", 1, table.size());
+        provider.removeTable("my");
     }
 
+    @Test
     public void testGet() {
-        MyTableProviderFactory factory = new MyTableProviderFactory();
-        MyTableProvider provider;
-        provider = factory.create(System.getProperty("fizteh.db.dir"));
-        table = provider.createTable("my3");
         table.put("Dmitry", "value");
         table.put("Kolya", "value");
         table.remove("Dmitry");
         Assert.assertNotNull("expected value", table.get("Kolya"));
+        provider.removeTable("my");
     }
 
     @Test
     public void testGetAfterRemove() {
-        MyTableProviderFactory factory = new MyTableProviderFactory();
-        MyTableProvider provider;
-        provider = factory.create(System.getProperty("fizteh.db.dir"));
-        table = provider.createTable("my4");
         table.put("Pasha", "value");
         table.remove("Pasha");
         Assert.assertNull("expected null when get removed value", table.get("Pasha"));
+        provider.removeTable("my");
     }
 
 }
