4368902843cfe5dec0b8431eec283e0045f38bc9
Pingvin786 <pingvin786@mail.ru>
2013-11-07 00:08:53 +0400

    i am sorry, but it is really last commit

--- src/ru/fizteh/fivt/students/ermolenko/MultiFileHashMap/test/MultiFileHashMapTableProviderFactoryTest.java
+++ src/ru/fizteh/fivt/students/ermolenko/MultiFileHashMap/test/MultiFileHashMapTableProviderFactoryTest.java
@@ -1,11 +1,31 @@
 package ru.fizteh.fivt.students.ermolenko.multifilehashmap.test;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import ru.fizteh.fivt.students.ermolenko.multifilehashmap.MultiFileHashMapTableProviderFactory;
 
+import java.io.File;
+
 public class MultiFileHashMapTableProviderFactoryTest {
 
+    private static File database;
     private MultiFileHashMapTableProviderFactory tableProviderFactory = new MultiFileHashMapTableProviderFactory();
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        database = new File("newNotExistedTable").getCanonicalFile();
+        if (database.isFile()) {
+            database.delete();
+        }
+        database.mkdir();
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        database.delete();
+    }
 
     @Test(expected = IllegalArgumentException.class)
     public void testCreateNull() throws Exception {
@@ -14,6 +34,11 @@ public class MultiFileHashMapTableProviderFactoryTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testCreateEmpty() throws Exception {
-        tableProviderFactory.create("");
+        tableProviderFactory.create(" ");
+    }
+
+    @Test
+    public void testCreateNotExisted() throws Exception {
+        Assert.assertNotNull(tableProviderFactory.create("newNotExistedTable"));
     }
 }
