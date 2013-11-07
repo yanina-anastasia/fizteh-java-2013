89d9a62495b99cf7d2e028accc42f15feb7cd7d0
DanAnastasyev <dan.anastasyev@gmail.com>
2013-11-08 03:41:52 +0400

    ..

--- src/ru/fizteh/fivt/students/anastasyev/filemap/FileMapTableProviderFactory.java
+++ src/ru/fizteh/fivt/students/anastasyev/filemap/FileMapTableProviderFactory.java
@@ -8,10 +8,10 @@ import java.io.IOException;
 public class FileMapTableProviderFactory implements TableProviderFactory {
     @Override
     public FileMapTableProvider create(String dir) throws IOException {
-        if (dir == null) {
+        if (dir == null || dir.trim().trim().isEmpty()) {
             throw new IllegalArgumentException("Directory path can't be null");
         }
-        File fileDir = new File(dir);
+        File fileDir = new File(dir.trim());
         if (fileDir.isFile()) {
             throw new IllegalArgumentException("Wrong directory path");
         }
