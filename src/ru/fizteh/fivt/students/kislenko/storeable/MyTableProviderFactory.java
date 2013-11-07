2af8c06f863a2712c67d2f281b2f27cedb5ba57e
ignorer <ignorer@bk.ru>
2013-11-07 20:22:09 +0400

    Testing.

--- src/ru/fizteh/fivt/students/kislenko/storeable/MyTableProviderFactory.java
+++ src/ru/fizteh/fivt/students/kislenko/storeable/MyTableProviderFactory.java
@@ -2,13 +2,18 @@ package ru.fizteh.fivt.students.kislenko.storeable;
 
 import ru.fizteh.fivt.storage.structured.TableProviderFactory;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MyTableProviderFactory implements TableProviderFactory {
     @Override
     public MyTableProvider create(String path) throws IOException {
-        if (path == null || path.trim().isEmpty()) {
+        if (path == null || path.trim().equals("")) {
             throw new IllegalArgumentException("Incorrect database name.");
+        }
+        File file = new File(path.trim());
+        if (!file.isDirectory()) {
+            throw new IOException("Database must be a directory.");
         }
         return new MyTableProvider();
     }
