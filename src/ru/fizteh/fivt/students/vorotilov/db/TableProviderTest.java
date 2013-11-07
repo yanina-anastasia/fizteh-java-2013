cf5f078056bfcb980d4524c29f2af1913a205527
vlad-vorotilov <vlad.vorotilov@gmail.com>
2013-11-07 11:50:31 +0400

    JUnit fix

--- src/ru/fizteh/fivt/students/vorotilov/db/TableProviderTest.java
+++ src/ru/fizteh/fivt/students/vorotilov/db/TableProviderTest.java
@@ -7,32 +7,24 @@ import java.io.IOException;
 
 public class TableProviderTest {
 
-    static VorotilovTableProviderFactory tableProviderFactory;
-    static VorotilovTableProvider tableProvider;
-
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
-    @BeforeClass
-    public static void setUp() {
-        tableProviderFactory = new VorotilovTableProviderFactory();
-    }
-
-    @Before
-    public void setTableProvider() throws IOException {
-        tableProvider = tableProviderFactory.create(folder.newFolder().toString());
-        Assert.assertNotNull(tableProvider);
-    }
-
     @Test
-    public void testCreateAndRemoveTable() {
+    public void testCreateAndRemoveTable() throws IOException {
+        VorotilovTableProviderFactory tableProviderFactory = new VorotilovTableProviderFactory();
+        VorotilovTableProvider tableProvider = tableProviderFactory.create(folder.newFolder().toString());
+        Assert.assertNotNull(tableProvider);
         Assert.assertNotNull(tableProvider.createTable("newTable"));
         Assert.assertNull(tableProvider.createTable("newTable"));
         tableProvider.removeTable("newTable");
     }
 
     @Test
-    public void testGetTable() {
+    public void testGetTable() throws IOException {
+        VorotilovTableProviderFactory tableProviderFactory = new VorotilovTableProviderFactory();
+        VorotilovTableProvider tableProvider = tableProviderFactory.create(folder.newFolder().toString());
+        Assert.assertNotNull(tableProvider);
         VorotilovTable table = tableProvider.createTable("newTable");
         Assert.assertNotNull(tableProvider.getTable("newTable"));
         Assert.assertEquals(table, tableProvider.getTable("newTable"));
@@ -40,47 +32,74 @@ public class TableProviderTest {
     }
 
     @Test
-    public void testGetNotExistedTable() {
+    public void testGetNotExistedTable() throws IOException {
+        VorotilovTableProviderFactory tableProviderFactory = new VorotilovTableProviderFactory();
+        VorotilovTableProvider tableProvider = tableProviderFactory.create(folder.newFolder().toString());
+        Assert.assertNotNull(tableProvider);
         Assert.assertNull(tableProvider.getTable("notExistedTable"));
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testGetTableWithNotValidName() {
+    public void testGetTableWithNotValidName() throws IOException {
+        VorotilovTableProviderFactory tableProviderFactory = new VorotilovTableProviderFactory();
+        VorotilovTableProvider tableProvider = tableProviderFactory.create(folder.newFolder().toString());
+        Assert.assertNotNull(tableProvider);
         tableProvider.getTable(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testCreateTableWithNotValidName() {
+    public void testCreateTableWithNotValidName() throws IOException {
+        VorotilovTableProviderFactory tableProviderFactory = new VorotilovTableProviderFactory();
+        VorotilovTableProvider tableProvider = tableProviderFactory.create(folder.newFolder().toString());
+        Assert.assertNotNull(tableProvider);
         tableProvider.createTable(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testCreateTableWithEmptyName() {
+    public void testCreateTableWithEmptyName() throws IOException {
+        VorotilovTableProviderFactory tableProviderFactory = new VorotilovTableProviderFactory();
+        VorotilovTableProvider tableProvider = tableProviderFactory.create(folder.newFolder().toString());
+        Assert.assertNotNull(tableProvider);
         tableProvider.createTable(" ");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testRemoveTableWithEmptyName() {
+    public void testRemoveTableWithEmptyName() throws IOException {
+        VorotilovTableProviderFactory tableProviderFactory = new VorotilovTableProviderFactory();
+        VorotilovTableProvider tableProvider = tableProviderFactory.create(folder.newFolder().toString());
+        Assert.assertNotNull(tableProvider);
         tableProvider.removeTable("  ");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testGetTableWithEmptyName() {
+    public void testGetTableWithEmptyName() throws IOException {
+        VorotilovTableProviderFactory tableProviderFactory = new VorotilovTableProviderFactory();
+        VorotilovTableProvider tableProvider = tableProviderFactory.create(folder.newFolder().toString());
+        Assert.assertNotNull(tableProvider);
         tableProvider.getTable("");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testRemoveTableWithNotValidName() {
+    public void testRemoveTableWithNotValidName() throws IOException {
+        VorotilovTableProviderFactory tableProviderFactory = new VorotilovTableProviderFactory();
+        VorotilovTableProvider tableProvider = tableProviderFactory.create(folder.newFolder().toString());
+        Assert.assertNotNull(tableProvider);
         tableProvider.removeTable(null);
     }
 
     @Test(expected = IllegalStateException.class)
-    public void testRemoveNotExistedTable() {
+    public void testRemoveNotExistedTable() throws IOException {
+        VorotilovTableProviderFactory tableProviderFactory = new VorotilovTableProviderFactory();
+        VorotilovTableProvider tableProvider = tableProviderFactory.create(folder.newFolder().toString());
+        Assert.assertNotNull(tableProvider);
         tableProvider.removeTable("notExistedTable");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testBadSymbolsInTableName() {
+    public void testBadSymbolsInTableName() throws IOException {
+        VorotilovTableProviderFactory tableProviderFactory = new VorotilovTableProviderFactory();
+        VorotilovTableProvider tableProvider = tableProviderFactory.create(folder.newFolder().toString());
+        Assert.assertNotNull(tableProvider);
         tableProvider.createTable("//a");
     }
 
