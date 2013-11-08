ce5f3c87dc0212d1254626a8c97ca3182646ca7a
Arlenka <alenarykunova@gmail.com>
2013-11-09 00:36:59 +0400

    ...

--- src/ru/fizteh/fivt/students/elenarykunova/filemap/MyTableProvider.java
+++ src/ru/fizteh/fivt/students/elenarykunova/filemap/MyTableProvider.java
@@ -216,6 +216,9 @@ public class MyTableProvider implements TableProvider {
 
     public Object checkClasses(Object first, Class<?> second) {
         Object result;
+        if (first == null) {
+            return first;
+        }
         if (first.getClass().equals(second)) {
             return first;
         }
@@ -297,7 +300,7 @@ public class MyTableProvider implements TableProvider {
         ArrayList<Object> values = new ArrayList<Object>();
         for (int i = 0; i < json.length(); i++) {
             Object resCast = checkClasses(json.get(i), table.getColumnType(i));
-            if (resCast != null && !resCast.getClass().equals(table.getColumnType(i))) {
+            if (json.get(i) != null && !resCast.getClass().equals(table.getColumnType(i))) {
                 throw new ParseException(
                         "deserialize: types mismatch " + json.get(i).getClass()
                                 + " " + table.getColumnType(i), i);
