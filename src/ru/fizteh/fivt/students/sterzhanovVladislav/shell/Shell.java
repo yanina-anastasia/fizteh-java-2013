7a5415665cc06eb946c099f3e7dcb8106ecd8673
Quadrocube <gliderok@gmail.com>
2013-11-09 02:15:42 +0400

    changed prompt

--- src/ru/fizteh/fivt/students/sterzhanovVladislav/shell/Shell.java
+++ src/ru/fizteh/fivt/students/sterzhanovVladislav/shell/Shell.java
@@ -89,7 +89,7 @@ public class Shell {
     
     private void maybePrintPrompt(boolean doPrintPrompt) {
         if (doPrintPrompt) {
-            out.print("$ ");
+            out.print(" $ ");
         }
     }
     
