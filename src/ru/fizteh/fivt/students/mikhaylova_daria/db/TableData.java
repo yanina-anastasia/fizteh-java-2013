6a9da0323d85a6c25a799875edfde7b7d5b5fee1
mikhaylova-daria <dasham94@yandex.ru>
2013-11-09 07:56:02 +0400

    --отладочный вывод

--- src/ru/fizteh/fivt/students/mikhaylova_daria/db/TableData.java
+++ src/ru/fizteh/fivt/students/mikhaylova_daria/db/TableData.java
@@ -175,7 +175,6 @@ public class TableData implements Table {
         if (value == null) {
             throw new IllegalArgumentException("value is null");
         }
-        System.out.println("int: " + value.getColumnAt(1));
         int i = 0;
         try {
             for (i = 0; i < getColumnsCount(); ++i) {
