dcfdcec0877451a3f92e9a2f4477e1a2cb8896e4
Meerstein <dermeerstein@gmail.com>
2013-11-09 10:29:15 +0400

    maybe...

--- src/ru/fizteh/fivt/students/ichalovaDiana/filemap/FileMap.java
+++ src/ru/fizteh/fivt/students/ichalovaDiana/filemap/FileMap.java
@@ -70,7 +70,7 @@ public class FileMap {
 
                 String[] parsedArguments = arguments[1].split("\\s+", 2);
                 if (parsedArguments.length != 2) {
-                    throw new IllegalArgumentException("wrong type (Illegal number of arguments)");
+                    throw new IllegalArgumentException("wrong type (Illegalnumberofarguments)");
                 }
                 
                 String tableName = parsedArguments[0];
