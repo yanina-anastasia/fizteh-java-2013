f5c3a92fd15b50ee65204bdc61b307fdefbe06f4
mikhaylova-daria <dasham94@yandex.ru>
2013-11-09 07:24:01 +0400

    снова отладочный вывод #2

--- src/ru/fizteh/fivt/students/mikhaylova_daria/db/Value.java
+++ src/ru/fizteh/fivt/students/mikhaylova_daria/db/Value.java
@@ -71,9 +71,10 @@ public class Value implements Storeable {
                     + table.getColumnType(columnIndex).getCanonicalName());
         }
         Integer integer = null;
-        System.out.println(value.get(columnIndex));
         if (value.get(columnIndex) != null) {
+            System.out.println(value.get(columnIndex));
             integer = (Integer) value.get(columnIndex);
+            System.out.println(integer + "\n");
         }
         return integer;
     }
