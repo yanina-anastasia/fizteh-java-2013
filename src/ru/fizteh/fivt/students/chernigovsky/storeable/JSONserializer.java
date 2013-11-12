5f77279848977464ef5436f420e39c7efc64c7c5
Evgeny Chernigovsky <evgeny.chernigovsky@gmail.com>
2013-11-13 00:11:37 +0400

    Abstract add

--- src/ru/fizteh/fivt/students/chernigovsky/storeable/JSONserializer.java
+++
@@ -1,9 +0,0 @@
-package ru.fizteh.fivt.students.chernigovsky.storeable;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class JSONSerializer {
-
-}
