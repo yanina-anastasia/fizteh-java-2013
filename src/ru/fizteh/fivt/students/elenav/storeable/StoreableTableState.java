df4efeb6889f77371a22a70fce7e559a749f30fb
Elena Vorobyeva <good-mark@mail.ru>
2013-11-12 21:54:07 +0400

    test commit

--- src/ru/fizteh/fivt/students/elenav/storeable/StoreableTableState.java
+++ src/ru/fizteh/fivt/students/elenav/storeable/StoreableTableState.java
@@ -280,7 +280,8 @@ public class StoreableTableState extends FilesystemState implements Table {
 				String key = new String(tempKey, StandardCharsets.UTF_8);
 				
 				if (!in.getName().equals(getFile(key)) || !in.getParentFile().getName().equals(getDir(key))) {
-					throw new IOException("wrong key placement");
+					throw new IOException("wrong key "+key+" placement,expected: "+getFile(key)+" "
+									+getDir(key)+" but: "+ in.getCanonicalPath());
 				}
 				
 				byte[] tempValue = new byte[valueLength];
