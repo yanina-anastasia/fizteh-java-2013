9aafaa0cc66567f65d9138c2d4abc4df23d37b8a
irina-goltsman <irina-goltsman@mail.ru>
2013-11-12 20:21:40 +0400

    + check empty dat file

--- src/ru/fizteh/fivt/students/irinaGoltsman/multifilehashmap/DBTable.java
+++ src/ru/fizteh/fivt/students/irinaGoltsman/multifilehashmap/DBTable.java
@@ -45,6 +45,10 @@ public class DBTable implements Table {
                         if (!datFiles.getName().matches("(0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15)\\.dat")) {
                             throw new IOException(String.format("illegal name of file %s inside dir %s inside table %s",
                                     datFiles.getName(), dirFile.getName(), tableDir.getName()));
+                        } else {
+                            if (datFiles.length() == 0) {
+                                throw new IOException("empty file " + datFiles.getName());
+                            }
                         }
                     }
                 }
