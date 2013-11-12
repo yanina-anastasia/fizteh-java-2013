a6cace9c6c89f239263855c6a39a8265570f8455
Elena Vorobyeva <good-mark@mail.ru>
2013-11-12 13:31:15 +0400

    little changes

--- src/ru/fizteh/fivt/students/elenav/storeable/StoreableTableState.java
+++ src/ru/fizteh/fivt/students/elenav/storeable/StoreableTableState.java
@@ -81,7 +81,7 @@ public class StoreableTableState extends FilesystemState implements Table {
 
 	@Override
 	public Storeable put(String key, Storeable value1) throws ColumnFormatException {
-		if (key == null || value1 == null || key.trim().isEmpty() || key.contains(" ")) {
+		if (key == null || value1 == null || key.trim().isEmpty() || key.contains("\\s+")) {
 			throw new IllegalArgumentException("can't put null key or(and) value");
 		}
 		checkStoreable(value1);
