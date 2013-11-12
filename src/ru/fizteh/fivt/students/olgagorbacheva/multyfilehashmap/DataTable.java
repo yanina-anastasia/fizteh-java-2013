eaeee45bd3d7fb2f88f7b23ea526c9912792e1f2
Olga <olga.gorbacheva@phystech.edu>
2013-11-13 00:09:04 +0400

    дескриптор

--- src/ru/fizteh/fivt/students/olgagorbacheva/multyfilehashmap/DataTable.java
+++ src/ru/fizteh/fivt/students/olgagorbacheva/multyfilehashmap/DataTable.java
@@ -186,52 +186,57 @@ public class DataTable implements Table {
       @SuppressWarnings("resource")
       public void read(File dataBaseFile) throws IOException {
             RandomAccessFile reader = new RandomAccessFile(dataBaseFile, "r");
-            if (reader.length() == 0) {
-                  return;
-            }
-
-            List<Integer> offsets = new ArrayList<Integer>();
-            List<String> keys = new ArrayList<String>();
-            List<String> values = new ArrayList<String>();
-
             try {
-                  do {
-                        List<Byte> keySymbols = new ArrayList<Byte>();
-                        byte b = reader.readByte();
-                        while (b != 0) {
-                              keySymbols.add(b);
-                              b = reader.readByte();
-                        }
-                        byte[] bytes = new byte[keySymbols.size()];
-                        for (int i = 0; i < bytes.length; ++i) {
-                              bytes[i] = keySymbols.get(i);
-                        }
-                        keys.add(new String(bytes, "UTF-8"));
-
-                        int offset = reader.readInt();
-                        if ((offset <= 0) || (!offsets.isEmpty() && offset <= offsets.get(offsets.size() - 1))
-                                    || (offset >= reader.length())) {
-                              throw new IOException("Неверное значение сдвигов");
-                        }
-                        offsets.add(offset);
-
-                  } while (reader.getFilePointer() != offsets.get(0));
-            } catch (EOFException e) {
-                  throw new IOException("Файл законнчен раньше времени");
-            }
-
-            offsets.add((int) reader.length());
-
-            for (int i = 0; i < keys.size(); ++i) {
-                  byte[] bytes = new byte[offsets.get(i + 1) - offsets.get(i)];
-                  reader.readFully(bytes);
-                  values.add(new String(bytes, "UTF-8"));
-            }
-
-            for (int i = 0; i < keys.size(); ++i) {
-                  if (!dataStorage.put(keys.get(i), values.get(i))) {
-                        throw new IOException("Значение ключа не уникально");
+                  if (reader.length() == 0) {
+                        return;
                   }
+
+                  List<Integer> offsets = new ArrayList<Integer>();
+                  List<String> keys = new ArrayList<String>();
+                  List<String> values = new ArrayList<String>();
+
+                  try {
+                        do {
+                              List<Byte> keySymbols = new ArrayList<Byte>();
+                              byte b = reader.readByte();
+                              while (b != 0) {
+                                    keySymbols.add(b);
+                                    b = reader.readByte();
+                              }
+                              byte[] bytes = new byte[keySymbols.size()];
+                              for (int i = 0; i < bytes.length; ++i) {
+                                    bytes[i] = keySymbols.get(i);
+                              }
+                              keys.add(new String(bytes, "UTF-8"));
+
+                              int offset = reader.readInt();
+                              if ((offset <= 0) || (!offsets.isEmpty() && offset <= offsets.get(offsets.size() - 1))
+                                          || (offset >= reader.length())) {
+                                    throw new IOException("Неверное значение сдвигов");
+                              }
+                              offsets.add(offset);
+
+                        } while (reader.getFilePointer() != offsets.get(0));
+                  } catch (EOFException e) {
+                        throw new IOException("Файл законнчен раньше времени");
+                  }
+
+                  offsets.add((int) reader.length());
+
+                  for (int i = 0; i < keys.size(); ++i) {
+                        byte[] bytes = new byte[offsets.get(i + 1) - offsets.get(i)];
+                        reader.readFully(bytes);
+                        values.add(new String(bytes, "UTF-8"));
+                  }
+
+                  for (int i = 0; i < keys.size(); ++i) {
+                        if (!dataStorage.put(keys.get(i), values.get(i))) {
+                              throw new IOException("Значение ключа не уникально");
+                        }
+                  }
+            } catch (IOException e) {
+                  reader.close();
+                  throw new IOException("Ошибка чтения файла");
             }
             reader.close();
       }
@@ -285,25 +290,31 @@ public class DataTable implements Table {
             dataStorage.clear();
       }
 
-      public void write(File dataBaseFile, Map<String, String> storage) throws FileNotFoundException, IOException {
+      public void write(File dataBaseFile, Map<String, String> storage) throws IOException {
+
             RandomAccessFile writer = new RandomAccessFile(dataBaseFile, "rw");
-            writer.setLength(0);
+            try {
+                  writer.setLength(0);
 
-            Integer offset = countOffset(storage);
-            List<String> values = new ArrayList<String>();
+                  Integer offset = countOffset(storage);
+                  List<String> values = new ArrayList<String>();
 
-            Iterator<Map.Entry<String, String>> it = storage.entrySet().iterator();
+                  Iterator<Map.Entry<String, String>> it = storage.entrySet().iterator();
 
-            while (it.hasNext()) {
-                  Map.Entry<String, String> elem = it.next();
-                  writer.write(elem.getKey().getBytes("UTF-8"));
-                  writer.write("\0".getBytes("UTF-8"));
-                  writer.writeInt(offset);
-                  offset += elem.getValue().getBytes("UTF-8").length;
-                  values.add(elem.getValue());
-            }
-            for (int i = 0; i < values.size(); i++) {
-                  writer.write(values.get(i).getBytes("UTF-8"));
+                  while (it.hasNext()) {
+                        Map.Entry<String, String> elem = it.next();
+                        writer.write(elem.getKey().getBytes("UTF-8"));
+                        writer.write("\0".getBytes("UTF-8"));
+                        writer.writeInt(offset);
+                        offset += elem.getValue().getBytes("UTF-8").length;
+                        values.add(elem.getValue());
+                  }
+                  for (int i = 0; i < values.size(); i++) {
+                        writer.write(values.get(i).getBytes("UTF-8"));
+                  }
+            } catch (IOException e) {
+                  writer.close();
+                  throw new IOException("Ошибка записи файла");
             }
 
             writer.close();
