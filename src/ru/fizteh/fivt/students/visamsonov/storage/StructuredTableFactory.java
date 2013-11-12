dfa5dde23e1170c1b73618b81285d511b36b5e54
Влад <vvladxx@gmail.com>
2013-11-13 01:55:19 +0400

    Fixes

--- src/ru/fizteh/fivt/students/visamsonov/storage/StructuredTableFactory.java
+++ src/ru/fizteh/fivt/students/visamsonov/storage/StructuredTableFactory.java
@@ -1,12 +1,15 @@
 package ru.fizteh.fivt.students.visamsonov.storage;
 
-import java.io.File;
+import java.io.*;
 
 public class StructuredTableFactory implements StructuredTableProviderFactoryInterface {
 
-	public StructuredTableProviderInterface create (String dir) {
-		if (dir == null || dir.trim().isEmpty() || !(new File(dir).isDirectory())) {
+	public StructuredTableProviderInterface create (String dir) throws IOException {
+		if (dir == null || dir.trim().isEmpty()) {
 			throw new IllegalArgumentException();
+		}
+		if (!(new File(dir).isDirectory())) {
+			 throw new IOException();
 		}
 		return new StructuredTableDirectory(dir);
 	}
