b206187dccd5f4f9d73d2060f17deff3f84e686f
Evgeny Chernigovsky <evgeny.chernigovsky@gmail.com>
2013-11-13 00:28:52 +0400

    fix

--- src/ru/fizteh/fivt/students/chernigovsky/junit/MultiFileHashMapTable.java
+++ src/ru/fizteh/fivt/students/chernigovsky/junit/MultiFileHashMapTable.java
@@ -4,4 +4,11 @@ public class MultiFileHashMapTable extends AbstractTable<String> implements Exte
     public MultiFileHashMapTable(String name, boolean flag) {
         super(name, flag);
     }
+
+    public String put(String key, String value) {
+        if (value.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return super.put(key, value);
+    }
 }
