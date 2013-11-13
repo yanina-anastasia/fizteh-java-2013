eefe760df9b662ca0536f6f485e352d90b6d93d7
Александр Даниляк <adanilyak@gmail.com>
2013-11-13 04:40:02 +0400

    Parallel 1.0.0.2

--- src/ru/fizteh/fivt/students/adanilyak/commands/CmdGet.java
+++ src/ru/fizteh/fivt/students/adanilyak/commands/CmdGet.java
@@ -37,7 +37,7 @@ public class CmdGet implements Cmd {
             if (result == null) {
                 System.out.println("not found");
             } else {
-                /** TRYING TO PASS TEST ON CRADLE*/ System.out.println("found");
+                //System.out.println("found");
                 System.out.println(result);
             }
         } else {
