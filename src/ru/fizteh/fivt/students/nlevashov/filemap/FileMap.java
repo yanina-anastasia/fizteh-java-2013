f8f4935d2b081478b96c3bd4ad3a7a63abbed308
nlevashov <me@yandex.ru>
2013-11-14 17:58:12 +0400

    storable 2.9

--- src/ru/fizteh/fivt/students/nlevashov/filemap/FileMap.java
+++ src/ru/fizteh/fivt/students/nlevashov/filemap/FileMap.java
@@ -179,8 +179,7 @@ public class FileMap {
                                     throw new IOException("wrong type (create: wrong arguments)");
                                 }
                                 int bracketIndex = arguments.indexOf(' ');
-                                create(arguments.substring(0, bracketIndex),
-                                       arguments.substring(bracketIndex + 2, arguments.length() - 1).split(" "));
+                                create(arguments.substring(0, bracketIndex), arguments.substring(bracketIndex + 2, arguments.length() - 1).split("[\\s\\t\\n]"));
                                 break;
                             }
                             case "drop": {
