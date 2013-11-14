55a4168a1f8d14ebcc66785e081bd872796907c2
Demidov <demidov-study@yandex.ru>
2013-11-15 01:55:41 +0400

    fix

--- src/ru/fizteh/fivt/students/demidov/filemap/FileMapState.java
+++
@@ -1,7 +0,0 @@
-package ru.fizteh.fivt.students.demidov.filemap;
-
-import java.io.IOException;
-
-public interface FileMapState {
-	public FileMap getCurrentFileMap(String key) throws IOException;
-}
