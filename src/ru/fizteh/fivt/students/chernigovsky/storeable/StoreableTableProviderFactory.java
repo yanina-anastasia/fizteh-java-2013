aba9223f7d9271781813d86588433a7ced9b3d10
Evgeny Chernigovsky <evgeny.chernigovsky@gmail.com>
2013-11-14 13:50:30 +0400

    test

--- src/ru/fizteh/fivt/students/chernigovsky/storeable/StoreableTableProviderFactory.java
+++ src/ru/fizteh/fivt/students/chernigovsky/storeable/StoreableTableProviderFactory.java
@@ -4,17 +4,24 @@ import ru.fizteh.fivt.storage.structured.TableProviderFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StoreableTableProviderFactory implements TableProviderFactory {
     @Override
     public StoreableTableProvider create(String dir) throws IOException {
-        if (dir == null || dir.isEmpty()) {
+        if (dir == null) {
             throw new IllegalArgumentException("dir is null");
+        }
+        Pattern pattern = Pattern.compile("\\s");
+        Matcher matcher = pattern.matcher(dir);
+        if (dir.isEmpty() || matcher.find()) {
+            throw new IllegalArgumentException("dir is wrong");
         }
 
         File dbDirectory = new File(dir);
         if (!dbDirectory.exists() || !dbDirectory.isDirectory()) {
-            throw new IllegalArgumentException("no such directory");
+            throw new IOException("no such directory");
         }
 
         return new StoreableTableProvider(dbDirectory, false);
