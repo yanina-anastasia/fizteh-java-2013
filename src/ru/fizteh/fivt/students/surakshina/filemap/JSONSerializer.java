38fa695810018d0e78a6df5f7b164a34db2360ba
surakshina <yuliya.surakshina@phystech.edu>
2013-11-14 18:29:42 +0400

    ..

--- src/ru/fizteh/fivt/students/surakshina/filemap/JSONSerializer.java
+++ src/ru/fizteh/fivt/students/surakshina/filemap/JSONSerializer.java
@@ -8,17 +8,16 @@ import ru.fizteh.fivt.storage.structured.Table;
 
 public class JSONSerializer {
     public static String serialize(Table table, Storeable value) {
+        if (value == null) {
+            return null;
+        }
         int numberColumns = table.getColumnsCount();
         Object[] values = new Object[numberColumns];
         for (int i = 0; i < table.getColumnsCount(); ++i) {
-            if (value != null && value.getColumnAt(i) != null) {
-                if (value.getColumnAt(i).getClass() != table.getColumnType(i)) {
-                    throw new ColumnFormatException("Incorrect column name");
-                }
-            } else {
+            values[i] = value.getColumnAt(i);
+            if (values[i] != null && !value.getColumnAt(i).getClass().equals(table.getColumnType(i))) {
                 throw new ColumnFormatException("Incorrect column name");
             }
-            values[i] = value.getColumnAt(i);
         }
         JSONArray array = new JSONArray(values);
         return array.toString();
