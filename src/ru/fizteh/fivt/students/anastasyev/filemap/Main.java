d676719937ee8765495fb97d65acf00b75f0d174
DanAnastasyev <dan.anastasyev@gmail.com>
2013-11-15 19:27:39 +0400

    db.dir creating

--- src/ru/fizteh/fivt/students/anastasyev/filemap/Main.java
+++ src/ru/fizteh/fivt/students/anastasyev/filemap/Main.java
@@ -1,12 +1,12 @@
 package ru.fizteh.fivt.students.anastasyev.filemap;
 
 import ru.fizteh.fivt.students.anastasyev.shell.Launcher;
+
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        //System.getProperties().setProperty("fizteh.db.dir", "C:\\Users\\qBic
-        // \\Documents\\GitHub\\fizteh-java-2013\\src\\ru\\fizteh\\fivt\\students\\anastasyev\\filemap\\db.dir");
         if (System.getProperty("fizteh.db.dir") == null) {
             System.err.println("Set home data base's directory");
             System.err.println("Use: -Dfizteh.db.dir=<directory>");
@@ -14,6 +14,20 @@ public class Main {
         }
         FileMapTableProvider fileMapTableProvider = null;
         try {
+            File dbProviderDir = new File(System.getProperty("fizteh.db.dir")).getCanonicalFile();
+            if (!dbProviderDir.exists()) {
+                if (!dbProviderDir.mkdirs()) {
+                    System.err.println("Can't create " + dbProviderDir.getName());
+                }
+            }
+            if (dbProviderDir.isFile()) {
+                if (!dbProviderDir.delete()) {
+                    System.err.println("Can't delete file " + dbProviderDir.getName());
+                }
+                if (!dbProviderDir.mkdirs()) {
+                    System.err.println("Can't create " + dbProviderDir.getName());
+                }
+            }
             fileMapTableProvider = new FileMapTableProvider(System.getProperty("fizteh.db.dir"));
             Launcher launcher = new Launcher(fileMapTableProvider, args);
         } catch (IllegalArgumentException e) {
