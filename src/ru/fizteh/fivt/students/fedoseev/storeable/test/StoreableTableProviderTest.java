48c0d5b2aefbcbaf7817f6c9ab635eadeb5814a9
KFedoseev <kirill.fedoseev@phystech.edu>
2013-11-16 02:32:54 +0400

    Added other unit tests.

--- src/ru/fizteh/fivt/students/fedoseev/storeable/test/StoreableTableProviderTest.java
+++ src/ru/fizteh/fivt/students/fedoseev/storeable/test/StoreableTableProviderTest.java
@@ -1,52 +1,80 @@
 package ru.fizteh.fivt.students.fedoseev.storeable.test;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import ru.fizteh.fivt.students.fedoseev.storeable.ColumnTypes;
+import ru.fizteh.fivt.students.fedoseev.storeable.StoreableTableProvider;
+import ru.fizteh.fivt.students.fedoseev.storeable.StoreableTableProviderFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class StoreableTableProviderTest {
-    @Before
-    public void setUp() throws Exception {
+    private static StoreableTableProvider tp;
+    private static File dbDir;
+    private static List<Class<?>> types;
 
+    public StoreableTableProviderTest() throws IOException {
+        dbDir = new File("database");
+        types = new ArrayList<>();
+
+        dbDir.mkdirs();
+
+        tp = new StoreableTableProviderFactory().create("database");
+        types = ColumnTypes.getTypesList();
     }
 
     @After
-    public void tearDown() throws Exception {
-
-    }
-
-    @Test
-    public void testGetTable() throws Exception {
-
+    public void tearDown() {
+        dbDir.delete();
     }
 
     @Test
     public void testCreateTable() throws Exception {
-
+        tp.createTable("chakachaka", types);
     }
 
     @Test
-    public void testRemoveTable() throws Exception {
-
+    public void testCreateExistingTable() throws Exception {
+        tp.createTable("bombom", types);
+        tp.createTable("bombom", types);
     }
 
-    @Test
-    public void testDeserialize() throws Exception {
-
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateNullNameTable() throws Exception {
+        tp.createTable(null, types);
     }
 
-    @Test
-    public void testSerialize() throws Exception {
-
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateNullTypesTable() throws Exception {
+        tp.createTable("chuchu", null);
     }
 
-    @Test
-    public void testCreateFor() throws Exception {
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateIllegalTypesTable() throws Exception {
+        List<Class<?>> types = new ArrayList<>();
 
+        types.add(Arrays.class);
+        tp.createTable("obama", types);
     }
 
-    @Test
-    public void testCreateForWithValues() throws Exception {
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateEmptyTypeListTable() throws Exception {
+        List<Class<?>> types = new ArrayList<>();
 
+        tp.createTable("barokko", types);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateEmptyNameTable() throws Exception {
+        tp.createTable("", types);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCreateIllegalSymbolTable() throws Exception {
+        tp.createTable(".oops.", types);
     }
 }
