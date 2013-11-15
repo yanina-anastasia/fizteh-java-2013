3527736c35b45e945832d89a8cc1a354e62977fe
nlevashov <me@yandex.ru>
2013-11-16 03:00:23 +0400

    storable 2.15

--- src/ru/fizteh/fivt/students/nlevashov/factory/MyTable.java
+++ src/ru/fizteh/fivt/students/nlevashov/factory/MyTable.java
@@ -227,27 +227,27 @@ public class MyTable implements Table {
                 for (int i = 0; i < getColumnsCount(); ++i) {
                     Class<?> c = getColumnType(i);
                     if (c == Integer.class) {
-                        Integer tempValue = value.getIntAt(i);
+                        //Integer tempValue = value.getIntAt(i);
                         temp.setColumnAt(i, Integer.valueOf(1));
                         //temp.setColumnAt(i, tempValue);
                     } else if (c == Long.class)  {
-                        Long tempValue = value.getLongAt(i);
+                        //Long tempValue = value.getLongAt(i);
                         temp.setColumnAt(i, Long.valueOf((long) 1));
                         //temp.setColumnAt(i, tempValue);
                     } else if (c == Byte.class) {
-                        Byte tempValue = value.getByteAt(i);
+                        //Byte tempValue = value.getByteAt(i);
                         temp.setColumnAt(i, Byte.valueOf((byte) 1));
                         //temp.setColumnAt(i, tempValue);
                     } else if (c == Float.class) {
-                        Float tempValue = value.getFloatAt(i);
+                        //Float tempValue = value.getFloatAt(i);
                         temp.setColumnAt(i, Float.valueOf((float) 1.5));
                         //temp.setColumnAt(i, tempValue);
                     } else if (c == Double.class) {
-                        Double tempValue = value.getDoubleAt(i);
+                        //Double tempValue = value.getDoubleAt(i);
                         temp.setColumnAt(i, Double.valueOf(1.5));
                         //temp.setColumnAt(i, tempValue);
                     } else if (c == Boolean.class) {
-                        Boolean tempValue = value.getBooleanAt(i);
+                        //Boolean tempValue = value.getBooleanAt(i);
                         temp.setColumnAt(i, Boolean.valueOf(true));
                         //temp.setColumnAt(i, tempValue);
                     } else {
--- src/ru/fizteh/fivt/students/nlevashov/factory/MyTableProvider.java
+++ src/ru/fizteh/fivt/students/nlevashov/factory/MyTableProvider.java
@@ -320,27 +320,27 @@ public class MyTableProvider implements TableProvider {
                 for (int i = 0; i < table.getColumnsCount(); ++i) {
                     Class<?> c = table.getColumnType(i);
                     if (c == Integer.class) {
-                        Integer tempValue = value.getIntAt(i);
+                        //Integer tempValue = value.getIntAt(i);
                         temp.setColumnAt(i, Integer.valueOf(1));
                         //temp.setColumnAt(i, tempValue);
                     } else if (c == Long.class)  {
-                        Long tempValue = value.getLongAt(i);
+                        //Long tempValue = value.getLongAt(i);
                         temp.setColumnAt(i, Long.valueOf((long) 1));
                         //temp.setColumnAt(i, tempValue);
                     } else if (c == Byte.class) {
-                        Byte tempValue = value.getByteAt(i);
+                        //Byte tempValue = value.getByteAt(i);
                         temp.setColumnAt(i, Byte.valueOf((byte) 1));
                         //temp.setColumnAt(i, tempValue);
                     } else if (c == Float.class) {
-                        Float tempValue = value.getFloatAt(i);
+                        //Float tempValue = value.getFloatAt(i);
                         temp.setColumnAt(i, Float.valueOf((float) 1.5));
                         //temp.setColumnAt(i, tempValue);
                     } else if (c == Double.class) {
-                        Double tempValue = value.getDoubleAt(i);
+                        //Double tempValue = value.getDoubleAt(i);
                         temp.setColumnAt(i, Double.valueOf(1.5));
                         //temp.setColumnAt(i, tempValue);
                     } else if (c == Boolean.class) {
-                        Boolean tempValue = value.getBooleanAt(i);
+                        //Boolean tempValue = value.getBooleanAt(i);
                         temp.setColumnAt(i, Boolean.valueOf(true));
                         //temp.setColumnAt(i, tempValue);
                     } else {
