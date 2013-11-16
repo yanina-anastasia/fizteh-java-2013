466724c293195f944bca4ed72e543851d64f6fb5
mikhaylova-daria <dasham94@yandex.ru>
2013-11-16 12:29:24 +0300

    Update TableData.java

--- src/ru/fizteh/fivt/students/mikhaylova_daria/db/TableData.java
+++ src/ru/fizteh/fivt/students/mikhaylova_daria/db/TableData.java
@@ -101,7 +101,7 @@ public class TableData implements Table {
         }
         try (BufferedWriter signatureWriter =
                      new BufferedWriter(new FileWriter(sign))) {
-            signatureWriter.write(str.toString();
+            signatureWriter.write(str.toString().trim());
         } catch (IOException e) {
             throw new IOException("Writing error: signature.tsv", e);
         }
