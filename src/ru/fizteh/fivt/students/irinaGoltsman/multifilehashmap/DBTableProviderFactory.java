57bc0d2f77eb7e6f5b4f6f09dcafeec776c638f9
irina-goltsman <irina-goltsman@mail.ru>
2013-11-16 11:01:06 +0400

    fixes

--- src/ru/fizteh/fivt/students/irinaGoltsman/multifilehashmap/DBTableProviderFactory.java
+++ src/ru/fizteh/fivt/students/irinaGoltsman/multifilehashmap/DBTableProviderFactory.java
@@ -6,9 +6,8 @@ import java.io.File;
 import java.io.IOException;
 
 public class DBTableProviderFactory implements TableProviderFactory {
-    private static final String TABLE_NAME_FORMAT = "[A-Za-zА-Яа-я0-9@.]+";
     public TableProvider create(String rootDir) throws IOException {
-        if (rootDir == null || rootDir.trim().isEmpty() || !rootDir.matches(TABLE_NAME_FORMAT)) {
+        if (rootDir == null || rootDir.trim().isEmpty() || rootDir.contains("\0")) {
             throw new IllegalArgumentException("wrong directory name");
         }
         TableProvider newTableProvider = null;
