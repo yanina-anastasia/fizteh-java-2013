124157b2c8baa3c9726d46b6d7f651ed924dcb27
KFedoseev <kirill.fedoseev@phystech.edu>
2013-11-17 21:17:35 +0400

    .

--- src/ru/fizteh/fivt/students/fedoseev/filemap/FileMapPutCommand.java
+++ src/ru/fizteh/fivt/students/fedoseev/filemap/FileMapPutCommand.java
@@ -1,17 +1,18 @@
 package ru.fizteh.fivt.students.fedoseev.filemap;
 
 import ru.fizteh.fivt.students.fedoseev.common.AbstractCommand;
+import ru.fizteh.fivt.students.fedoseev.common.State;
 
 import java.io.IOException;
 import java.text.ParseException;
 
-public class FileMapPutCommand extends AbstractCommand<FileMapState> {
+public class FileMapPutCommand extends AbstractCommand<State> {
     public FileMapPutCommand() {
         super("put", 2);
     }
 
     @Override
-    public void execute(String[] input, FileMapState state) throws IOException, ParseException {
+    public void execute(String[] input, State state) throws IOException, ParseException {
         if (state.usingTables()) {
             if (state.getCurTable() == null) {
                 throw new IOException("no table");
--- src/ru/fizteh/fivt/students/fedoseev/filemap/FileMapRemoveCommand.java
+++ src/ru/fizteh/fivt/students/fedoseev/filemap/FileMapRemoveCommand.java
@@ -1,16 +1,17 @@
 package ru.fizteh.fivt.students.fedoseev.filemap;
 
 import ru.fizteh.fivt.students.fedoseev.common.AbstractCommand;
+import ru.fizteh.fivt.students.fedoseev.common.State;
 
 import java.io.IOException;
 
-public class FileMapRemoveCommand extends AbstractCommand<FileMapState> {
+public class FileMapRemoveCommand extends AbstractCommand<State> {
     public FileMapRemoveCommand() {
         super("remove", 1);
     }
 
     @Override
-    public void execute(String[] input, FileMapState state) throws IOException {
+    public void execute(String[] input, State state) throws IOException {
         if (state.usingTables()) {
             if (state.getCurTable() == null) {
                 throw new IOException("no table");
