b2b8e3128b38117aca1b99b25fc1bb7f1ac4b5b7
irina-goltsman <irina-goltsman@mail.ru>
2013-11-18 10:48:57 +0400

    параллели без тестов

--- src/ru/fizteh/fivt/students/irinaGoltsman/multifilehashmap/DBTableProvider.java
+++ src/ru/fizteh/fivt/students/irinaGoltsman/multifilehashmap/DBTableProvider.java
@@ -15,11 +15,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class DBTableProvider implements TableProvider {
     private Map<String, Table> allTables = new HashMap<String, Table>();
     private File rootDirectoryOfTables;
     private static final String TABLE_NAME_FORMAT = "[A-Za-zА-Яа-я0-9@.]+";
+    private final ReadWriteLock lock = new ReentrantReadWriteLock(true);
+    private final Lock readLock = lock.readLock();
+    private final Lock writeLock = lock.writeLock();
 
     public DBTableProvider(File rootDirectory) throws IOException {
         if (!rootDirectory.exists()) {
@@ -31,9 +37,14 @@ public class DBTableProvider implements TableProvider {
             throw new IllegalArgumentException(rootDirectory.getName() + ": not a directory");
         }
         rootDirectoryOfTables = rootDirectory;
-        for (File tableFile : rootDirectoryOfTables.listFiles()) {
-            Table table = new DBTable(tableFile, this);
-            allTables.put(tableFile.getName(), table);
+        try {
+            writeLock.lock();
+            for (File tableFile : rootDirectoryOfTables.listFiles()) {
+                Table table = new DBTable(tableFile, this);
+                allTables.put(tableFile.getName(), table);
+            }
+        } finally {
+            writeLock.unlock();
         }
     }
 
@@ -48,7 +59,12 @@ public class DBTableProvider implements TableProvider {
         if (!tableName.matches(TABLE_NAME_FORMAT)) {
             throw new IllegalArgumentException("get table: error table name");
         }
-        return allTables.get(tableName);
+        try {
+            readLock.lock();
+            return allTables.get(tableName);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     @Override
@@ -65,17 +81,22 @@ public class DBTableProvider implements TableProvider {
         ColumnTypes ct = new ColumnTypes();
         ct.checkTypes(columnTypes);
         Table newTable = null;
-        File tableFile = new File(rootDirectoryOfTables, tableName);
-        if (tableFile.exists()) {
-            return null;
+        try {
+            writeLock.lock();
+            File tableFile = new File(rootDirectoryOfTables, tableName);
+            if (tableFile.exists()) {
+                return null;
+            }
+            if (!tableFile.mkdir()) {
+                throw new IOException("table" + tableName + "can't be create");
+            }
+            List<String> types = ct.convertListOfClassesToListOfStrings(columnTypes);
+            FileManager.writeSignature(tableFile, types);
+            newTable = new DBTable(tableFile, this);
+            allTables.put(tableName, newTable);
+        } finally {
+            writeLock.unlock();
         }
-        if (!tableFile.mkdir()) {
-            throw new IOException("table" + tableName + "can't be create");
-        }
-        List<String> types = ct.convertListOfClassesToListOfStrings(columnTypes);
-        FileManager.writeSignature(tableFile, types);
-        newTable = new DBTable(tableFile, this);
-        allTables.put(tableName, newTable);
         return newTable;
     }
 
@@ -90,16 +111,21 @@ public class DBTableProvider implements TableProvider {
         MapOfCommands cm = new MapOfCommands();
         cm.addCommand(new ShellCommands.Remove());
         cm.addCommand(new ShellCommands.ChangeDirectory());
-        if (!allTables.containsKey(tableName)) {
-            throw new IllegalStateException(String.format("%s not exists", tableName));
+        try {
+            writeLock.lock();
+            if (!allTables.containsKey(tableName)) {
+                throw new IllegalStateException(String.format("%s not exists", tableName));
+            }
+            //File table = new File(rootDirectoryOfTables, tableName);
+            cm.commandProcessing("cd " + rootDirectoryOfTables.toString());
+            Code returnCode = cm.commandProcessing("rm " + tableName);
+            if (returnCode != Code.OK) {
+                throw new IOException("");
+            }
+            allTables.remove(tableName);
+        } finally {
+            writeLock.unlock();
         }
-        //File table = new File(rootDirectoryOfTables, tableName);
-        cm.commandProcessing("cd " + rootDirectoryOfTables.toString());
-        Code returnCode = cm.commandProcessing("rm " + tableName);
-        if (returnCode != Code.OK) {
-            throw new IOException("");
-        }
-        allTables.remove(tableName);
     }
 
     @Override
