4e8042cefd19fe3acbd51de0179a617b9662a223
RyabovaMaria <liastramv@gmail.com>
2013-11-18 22:29:16 +0400

    diff

--- src/ru/fizteh/fivt/students/ryabovaMaria/fileMap/TableCommands.java
+++ src/ru/fizteh/fivt/students/ryabovaMaria/fileMap/TableCommands.java
@@ -38,7 +38,6 @@ public class TableCommands implements Table {
         tableDir = directory;
         update = new HashMap<Integer, String>();
         isCorrectTable();
-        assigment(lastList, list);
     }
     
     private void isCorrectTable() throws IOException {
@@ -188,6 +187,11 @@ public class TableCommands implements Table {
         }
         getUsingDatFile(key);
         String value = list[numberOfDir][numberOfFile].get(key);
+        if (value == null) {
+            if (!list[numberOfDir][numberOfFile].containsKey(key)) {
+                value = lastList[numberOfDir][numberOfFile].get(key);
+            }
+        }
         try {
             return tableProvider.deserialize(this, value);
         } catch (ParseException e) {
@@ -205,6 +209,11 @@ public class TableCommands implements Table {
             update.put(numberOfDir * 16 + numberOfFile, " ");
             String stringValue = tableProvider.serialize(this, value);
             String lastValue = list[numberOfDir][numberOfFile].put(key, stringValue);
+            if (lastValue == null) {
+                if (!list[numberOfDir][numberOfFile].containsKey(key)) {
+                    lastValue = lastList[numberOfDir][numberOfFile].get(key);
+                }
+            }
             Storeable answer = tableProvider.deserialize(this, lastValue);
             return answer;
         } catch (ParseException | NullPointerException e) {
@@ -220,6 +229,10 @@ public class TableCommands implements Table {
         getUsingDatFile(key);
         update.put(numberOfDir * 16 + numberOfFile, null);
         String value = list[numberOfDir][numberOfFile].remove(key);
+        if (value == null) {
+            value = lastList[numberOfDir][numberOfFile].get(key);
+            list[numberOfDir][numberOfFile].put(key, null);
+        }
         try {
             return tableProvider.deserialize(this, value);
         } catch (ParseException e) {
@@ -227,12 +240,30 @@ public class TableCommands implements Table {
         }
     }
 
+    private int getCountSize(int first, int second) {
+        int result = lastList[first][second].size();
+        for (Map.Entry entry : list[first][second].entrySet()) {
+            String key = (String) entry.getKey();
+            String value = (String) entry.getValue();
+            if (value == null){
+                if (lastList[first][second].containsKey(key)) {
+                    --result;
+                }
+            } else {
+                if (!lastList[first][second].containsKey(key)) {
+                    ++result;
+                }
+            }
+        }
+        return result;
+    }
+    
     @Override
     public int size() {
         int countSize = 0;
         for (int i = 0; i < 16; ++i) {
             for (int j = 0; j < 16; ++j) {
-                countSize += list[i][j].size();
+                countSize += getCountSize(i, j);
             }
         }
         return countSize; 
@@ -261,7 +292,62 @@ public class TableCommands implements Table {
         try {
             db = new RandomAccessFile(dbFile, "rw");
             db.setLength(0);
-            Iterator<Map.Entry<String, String>> it;
+            long[] pointers = new long[getCountSize(numOfDir, numOfFile)];
+            int counter = 0;
+            for (Map.Entry entry : lastList[numOfDir][numOfFile].entrySet()) {
+                String key = (String) entry.getKey();
+                String newValue = list[numOfDir][numOfFile].get(key);
+                if (newValue == null && list[numOfDir][numOfFile].containsKey(key)) {
+                    continue;
+                }
+                db.write(key.getBytes("UTF-8"));
+                db.write("\0".getBytes("UTF-8"));
+                pointers[counter] = db.getFilePointer();
+                db.seek(pointers[counter] + 4);
+                ++counter;
+            }
+            for (Map.Entry entry : list[numOfDir][numOfFile].entrySet()) {
+                String key = (String) entry.getKey();
+                String value = (String) entry.getValue();
+                if (value != null) {
+                    db.write(key.getBytes("UTF-8"));
+                    db.write("\0".getBytes("UTF-8"));
+                    pointers[counter] = db.getFilePointer();
+                    db.seek(pointers[counter] + 4);
+                    ++counter;
+                }
+            }
+            counter = 0;
+            for (Map.Entry entry : lastList[numOfDir][numOfFile].entrySet()) {
+                String key = (String) entry.getKey();
+                String value = (String) entry.getValue();
+                String newValue = list[numOfDir][numOfFile].get(key);
+                if (newValue == null && list[numOfDir][numOfFile].containsKey(key)) {
+                    continue;
+                }
+                if (newValue != null) {
+                    value = newValue; 
+                }
+                int curPointer = (int) db.getFilePointer();
+                db.seek(pointers[counter]);
+                db.writeInt(curPointer);
+                db.seek(curPointer);
+                db.write(value.getBytes("UTF-8"));
+                ++counter;
+            }
+            for (Map.Entry entry : list[numOfDir][numOfFile].entrySet()) {
+                String key = (String) entry.getKey();
+                String value = (String) entry.getValue();
+                if (value != null) {
+                    int curPointer = (int) db.getFilePointer();
+                    db.seek(pointers[counter]);
+                    db.writeInt(curPointer);
+                    db.seek(curPointer);
+                    db.write(value.getBytes("UTF-8"));
+                    ++counter;
+                }
+            }
+            /*Iterator<Map.Entry<String, String>> it;
             it = list[numOfDir][numOfFile].entrySet().iterator();
             long[] pointers = new long[list[numOfDir][numOfFile].size()];
             int counter = 0;
@@ -285,7 +371,7 @@ public class TableCommands implements Table {
                 db.seek(curPointer);
                 db.write(value.getBytes("UTF-8"));
                 ++counter;
-            }
+            }*/
         } catch (Exception e) {
             if (db != null) {
                 try {
@@ -303,45 +389,30 @@ public class TableCommands implements Table {
         for (Integer file : update.keySet()) {
             numberOfFile = file % 16;
             numberOfDir = (file - numberOfFile) / 16;
-            if (isWrite) {
-                writeIntoFile(numberOfDir, numberOfFile);
-            }
             for (Map.Entry entry : list[numberOfDir][numberOfFile].entrySet()) {
                 String key = (String) entry.getKey();
                 String value = (String) entry.getValue();
-                if (lastList[numberOfDir][numberOfFile].containsKey(key)) {
-                    if (!lastList[numberOfDir][numberOfFile].get(key).equals(value)) {
+                if (value == null) {
+                    if (lastList[numberOfDir][numberOfFile].containsKey(key)) {
                         ++result;
-                    } 
+                    }
                 } else {
-                    ++result;
+                    if (!value.equals(lastList[numberOfDir][numberOfFile].get(key))) {
+                        ++result;
+                    }
                 }
             }
-            for (Map.Entry entry : lastList[numberOfDir][numberOfFile].entrySet()) {
-                String key = (String) entry.getKey();
-                if (!list[numberOfDir][numberOfFile].containsKey(key)) {
-                    ++result;
-                }
+            if (isWrite) {
+                writeIntoFile(numberOfDir, numberOfFile);
+                list[numberOfDir][numberOfFile].clear();
             }
         }
         return result;
     }
     
-    private void assigment(HashMap<String, String>[][] first, HashMap<String, String>[][] second) {
-        for (int i = 0; i < 16; ++i) {
-            for (int j = 0; j < 16; ++j) {
-                first[i][j].clear();
-                for (Map.Entry entry : second[i][j].entrySet()) {
-                    first[i][j].put((String) entry.getKey(), (String) entry.getValue());
-                }
-            }
-        }
-    }
-    
     @Override
     public int commit() throws IOException {
         int result = countChanges(true);
-        assigment(lastList, list);
         return result;
     }
 
@@ -352,7 +423,11 @@ public class TableCommands implements Table {
             result = countChanges(false);
         } catch (IOException e) {
         }
-        assigment(list, lastList);
+        for (int i = 0; i < 16; ++i) {
+            for(int j = 0; j < 16; ++j) {
+                list[i][j].clear();
+            }
+        }
         return result;
     }
 
