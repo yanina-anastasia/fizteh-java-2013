ade61bcbb69ec05a8c5c4a08eab0c0b9b2af7c16
Sandrikova Maria <mashasandrikova@gmail.com>
2013-11-18 19:54:58 +0400

    Null column types

--- src/ru/fizteh/fivt/students/msandrikova/storeable/StoreableTableProvider.java
+++ src/ru/fizteh/fivt/students/msandrikova/storeable/StoreableTableProvider.java
@@ -165,6 +165,9 @@ public class StoreableTableProvider implements ChangesCountingTableProvider {
 		if(Utils.isEmpty(name) || !Utils.testBadSymbols(name)) {
 			throw new IllegalArgumentException("Table name can not be null or empty or contain bad symbols");
 		}
+		if(columnTypes == null) {
+			throw new IllegalArgumentException("Null column types");
+		}
 		if(!Utils.testColumnTypes(columnTypes)) {
 			throw new IllegalArgumentException("Bad column types");
 		}
