41712d92338952c1f3a105edfc61c92f358c22b5
Dmitry Zvonarev <dmitry.zvonorev@phystech.edu>
2013-11-20 02:39:43 +0400

    parallel 1 commit. without parallel-tests(later, adter storeable merge i will change folder)

---
+++ src/ru/fizteh/fivt/students/dzvonarev/parallel/MyStoreable.java
@@ -0,0 +1,137 @@
+package ru.fizteh.fivt.students.dzvonarev.parallel;
+
+import ru.fizteh.fivt.storage.structured.ColumnFormatException;
+import ru.fizteh.fivt.storage.structured.Storeable;
+import ru.fizteh.fivt.storage.structured.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyStoreable implements Storeable {
+
+    public MyStoreable(Table table, List<?> args) throws IndexOutOfBoundsException, ColumnFormatException {
+        columnTypes = new ArrayList<>();
+        for (int i = 0; i < table.getColumnsCount(); ++i) {
+            columnTypes.add(table.getColumnType(i));
+        }
+        if (args.size() != columnTypes.size()) {            // wrong count of columns
+            throw new IndexOutOfBoundsException("wrong type (wrong count of columns in value - " + args.size() + ")");
+        }
+        for (int i = 0; i < args.size(); ++i) {
+            Parser myParser = new Parser();
+            if (!myParser.canBeCastedTo(columnTypes.get(i), args.get(i))) {
+                throw new ColumnFormatException("wrong type (" + i + " column got wrong type - " + args.get(i) + ")");
+            }
+        }
+        ArrayList<Object> temp = new ArrayList<>();      // if all OK we init column array
+        for (Object arg : args) {
+            temp.add(arg);
+        }
+        column = temp;
+    }
+
+    public MyStoreable(Table table) {
+        column = new ArrayList<>();
+        columnTypes = new ArrayList<>();
+        for (int i = 0; i < table.getColumnsCount(); ++i) {
+            columnTypes.add(table.getColumnType(i));
+            column.add(null);
+        }
+    }
+
+    private ArrayList<Object> column;                        // values in columns
+    private ArrayList<Class<?>> columnTypes;                 // types of these values
+
+    public void setColumnAt(int columnIndex, Object value) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (columnIndex < 0 || columnIndex >= column.size()) {
+            throw new IndexOutOfBoundsException("wrong type (wrong column index - " + columnIndex + ")");
+        }
+        if (value == null || value.equals(null)) {
+            column.set(columnIndex, null);
+            return;
+        }
+        if (!value.getClass().equals(columnTypes.get(columnIndex))) {
+            throw new ColumnFormatException("wrong type (value " + value
+                    + " got invalid type in " + columnIndex + " column)");
+        }
+        column.set(columnIndex, value);
+    }
+
+    public Object getColumnAt(int columnIndex) throws IndexOutOfBoundsException {
+        if (columnIndex < 0 || columnIndex >= column.size()) {
+            throw new IndexOutOfBoundsException("wrong type (wrong column index - " + columnIndex + ")");
+        }
+        return column.get(columnIndex);
+    }
+
+    public Integer getIntAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (columnIndex < 0 || columnIndex >= column.size()) {
+            throw new IndexOutOfBoundsException("wrong type (wrong column index - " + columnIndex + ")");
+        }
+        if (!column.get(columnIndex).getClass().equals(Integer.class)) {
+            throw new ColumnFormatException("wrong type (wrong type of value in " + columnIndex + " column)");
+        }
+        return (Integer) column.get(columnIndex);
+    }
+
+    public Long getLongAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (columnIndex < 0 || columnIndex >= column.size()) {
+            throw new IndexOutOfBoundsException("wrong type (wrong column index - " + columnIndex + ")");
+        }
+        if (!column.get(columnIndex).getClass().equals(Long.class)) {
+            throw new ColumnFormatException("wrong type (wrong type of value in " + columnIndex + " column)");
+        }
+        return (Long) column.get(columnIndex);
+    }
+
+    public Byte getByteAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (columnIndex < 0 || columnIndex >= column.size()) {
+            throw new IndexOutOfBoundsException("wrong type (wrong column index - " + columnIndex + ")");
+        }
+        if (!column.get(columnIndex).getClass().equals(Byte.class)) {
+            throw new ColumnFormatException("wrong type (wrong type of value in " + columnIndex + " column)");
+        }
+        return (Byte) column.get(columnIndex);
+    }
+
+    public Float getFloatAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (columnIndex < 0 || columnIndex >= column.size()) {
+            throw new IndexOutOfBoundsException("wrong type (wrong column index - " + columnIndex + ")");
+        }
+        if (!column.get(columnIndex).getClass().equals(Float.class)) {
+            throw new ColumnFormatException("wrong type (wrong type of value in " + columnIndex + " column)");
+        }
+        return (Float) column.get(columnIndex);
+    }
+
+    public Double getDoubleAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (columnIndex < 0 || columnIndex >= column.size()) {
+            throw new IndexOutOfBoundsException("wrong type (wrong column index - " + columnIndex + ")");
+        }
+        if (!column.get(columnIndex).getClass().equals(Double.class)) {
+            throw new ColumnFormatException("wrong type (wrong type of value in " + columnIndex + " column)");
+        }
+        return (Double) column.get(columnIndex);
+    }
+
+    public Boolean getBooleanAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (columnIndex < 0 || columnIndex >= column.size()) {
+            throw new IndexOutOfBoundsException("wrong type (wrong column index - " + columnIndex + ")");
+        }
+        if (!column.get(columnIndex).getClass().equals(Boolean.class)) {
+            throw new ColumnFormatException("wrong type (wrong type of value in " + columnIndex + " column)");
+        }
+        return (Boolean) column.get(columnIndex);
+    }
+
+    public String getStringAt(int columnIndex) throws ColumnFormatException, IndexOutOfBoundsException {
+        if (columnIndex < 0 || columnIndex >= column.size()) {
+            throw new IndexOutOfBoundsException("wrong type (wrong column index - " + columnIndex + ")");
+        }
+        if (!column.get(columnIndex).getClass().equals(String.class)) {
+            throw new ColumnFormatException("wrong type (wrong type of value in " + columnIndex + " column)");
+        }
+        return (String) column.get(columnIndex);
+    }
+
+}
---
+++ src/ru/fizteh/fivt/students/dzvonarev/parallel/MyTableTests.java
@@ -0,0 +1,135 @@
+package ru.fizteh.fivt.students.dzvonarev.parallel;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import ru.fizteh.fivt.storage.structured.ColumnFormatException;
+import ru.fizteh.fivt.storage.structured.Storeable;
+import ru.fizteh.fivt.storage.structured.Table;
+import ru.fizteh.fivt.storage.structured.TableProvider;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class MyTableTests {
+
+    private Table table;
+    private TableProvider provider;
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Before
+    public void test() throws IOException {
+        MyTableProviderFactory factory = new MyTableProviderFactory();
+        provider = factory.create(folder.newFolder().getCanonicalPath());
+        List<Class<?>> cl = new ArrayList<>();
+        cl.add(Integer.class);
+        cl.add(String.class);
+        cl.add(Double.class);
+        table = provider.createTable("testTable", cl);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullValue() {
+        table.put("key", null);
+    }
+
+    @Test
+    public void testIsCorrect() throws IOException {
+        List<Class<?>> cl = new ArrayList<>();
+        cl.add(Integer.class);
+        cl.add(String.class);
+        cl.add(Double.class);
+        Table t = provider.createTable("correct", cl);
+        List<Object> data = new ArrayList<>();
+        data.add(2);
+        data.add("qwerty");
+        data.add(3.3);
+        Storeable st = provider.createFor(t, data);
+        table.put("blabla", st);
+    }
+
+    @Test(expected = ColumnFormatException.class)
+    public void testLessArguments() throws IOException {
+        List<Class<?>> cl = new ArrayList<>();
+        cl.add(Integer.class);
+        cl.add(String.class);
+        Table newTable = provider.createTable("less", cl);
+        List<Object> args = new ArrayList<>();
+        args.add(24);
+        args.add("less");
+        Storeable st = provider.createFor(newTable, args);
+        table.put("less", st);
+    }
+
+    @Test(expected = ColumnFormatException.class)
+    public void testMoreArguments() throws IOException {
+        List<Class<?>> cl = new ArrayList<>();
+        cl.add(Integer.class);
+        cl.add(String.class);
+        cl.add(Double.class);
+        cl.add(Integer.class);
+        Table newTable = provider.createTable("more", cl);
+        List<Object> args = new ArrayList<>();
+        args.add(1);
+        args.add("2");
+        args.add(3.4);
+        args.add(1);
+        Storeable st = provider.createFor(newTable, args);
+        table.put("more", st);
+    }
+
+    @Test(expected = ColumnFormatException.class)
+    public void wrongTypes() {
+        List<Object> args = new ArrayList<>();
+        args.add(1);
+        args.add(2);
+        args.add(3);
+        Storeable st = provider.createFor(table, args);
+        table.put("wrongTypes", st);
+    }
+
+    @Test
+    public void ifHaveNulls() {
+        List<Object> args = new ArrayList<>();
+        args.add(null);
+        args.add("null");
+        args.add(null);
+        Storeable st = provider.createFor(table, args);
+        table.put("hasnull", st);
+    }
+
+    @Test
+    public void testCommit() throws IOException {
+        Storeable st = new MyStoreable(table);
+        st.setColumnAt(0, 1);
+        st.setColumnAt(1, "string");
+        st.setColumnAt(2, 2.5);
+        table.put("key1", st);
+        st.setColumnAt(0, 2);
+        st.setColumnAt(1, "string2");
+        st.setColumnAt(2, 4.5);
+        table.put("key2", st);
+        assertEquals(table.commit(), 2);
+    }
+
+    @Test
+    public void testRollback() throws IOException {
+        Storeable st = new MyStoreable(table);
+        st.setColumnAt(0, 3);
+        st.setColumnAt(1, "stringHere");
+        st.setColumnAt(2, 2.666);
+        table.put("keyNew", st);
+        assertEquals(table.commit(), 1);
+        assertEquals(table.commit(), 0);
+        assertNotNull(table.remove("keyNew"));
+        assertEquals(table.rollback(), 1);
+    }
+
+}
