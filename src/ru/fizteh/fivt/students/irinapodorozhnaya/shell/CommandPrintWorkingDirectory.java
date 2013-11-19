b479c694a7faef39794bd80cfb77e561819dd88c
1Risha1 <Irapodorozhnaya@yandex.ru>
2013-11-19 14:50:45 +0400

    tabs -> spaces

--- src/ru/fizteh/fivt/students/irinapodorozhnaya/shell/CommandPrintWorkingDirectory.java
+++ src/ru/fizteh/fivt/students/irinapodorozhnaya/shell/CommandPrintWorkingDirectory.java
@@ -4,19 +4,19 @@ import java.io.IOException;
 
 
 public class CommandPrintWorkingDirectory extends AbstractCommand {
-	
-	private final StateShell state;
-	
-	public CommandPrintWorkingDirectory(StateShell st) {
-		super(0);
-		state = st;
-	}
-	
-	public String getName() {
-		return "pwd";
-	}
-	
-	public void execute(String[] args) throws IOException {
-		state.getOutputStream().println(state.getCurrentDir().getCanonicalPath());
-	}
+    
+    private final StateShell state;
+    
+    public CommandPrintWorkingDirectory(StateShell st) {
+        super(0);
+        state = st;
+    }
+    
+    public String getName() {
+        return "pwd";
+    }
+    
+    public void execute(String[] args) throws IOException {
+        state.getOutputStream().println(state.getCurrentDir().getCanonicalPath());
+    }
 }
