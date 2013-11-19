d5a8081d639224c0face7522f7f02d86896b94ba
Anna Vyatkina <ava.katushka@gmail.com>
2013-11-19 11:41:04 +0400

    fixed compilation

--- src/ru/fizteh/fivt/students/vyatkina/database/multitable/MultiTable.java
+++ src/ru/fizteh/fivt/students/vyatkina/database/multitable/MultiTable.java
@@ -1,16 +1,9 @@
 package ru.fizteh.fivt.students.vyatkina.database.multitable;
 
-import ru.fizteh.fivt.storage.strings.Table;
-import ru.fizteh.fivt.students.vyatkina.WrappedIOException;
 import ru.fizteh.fivt.students.vyatkina.database.StringTable;
 import ru.fizteh.fivt.students.vyatkina.database.superior.SuperTable;
-import ru.fizteh.fivt.students.vyatkina.database.superior.TableChecker;
 
-import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
-
-public class MultiTable extends SuperTable <String> implements StringTable {
+public class MultiTable extends SuperTable<String> implements StringTable {
 
     private MultiTableProvider tableProvider;
 
@@ -22,13 +15,6 @@ public class MultiTable extends SuperTable <String> implements StringTable {
     @Override
     public int commit () {
         tableProvider.commitTable (this);
-        int savedChanges = 0;
-        try {
-            savedChanges = super.commit ();
-        }
-        catch (IOException e) {
-            throw new WrappedIOException (e);
-        }
-        return savedChanges;
+        return super.commit ();
     }
 }
