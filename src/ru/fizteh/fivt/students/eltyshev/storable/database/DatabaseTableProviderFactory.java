5bd9a8fea7d9308e6211832283f4250de3d11038
Evgeny Eltishev <evgeny.eltishev@phystech.edu>
2013-11-21 00:52:39 +0400

    fixes

--- src/ru/fizteh/fivt/students/eltyshev/storable/database/DatabaseTableProviderFactory.java
+++ src/ru/fizteh/fivt/students/eltyshev/storable/database/DatabaseTableProviderFactory.java
@@ -29,7 +29,7 @@ public class DatabaseTableProviderFactory implements TableProviderFactory {
             }
         }
 
-        File file = new File("/home/judge/fizteh-java-2013/src/ru/fizteh/fivt");
+        File file = new File("/home/judge/fizteh-java-2013/src/ru/fizteh/fivt/storage");
         for (File subfile : file.listFiles()) {
             System.out.println(subfile.getAbsolutePath());
         }
