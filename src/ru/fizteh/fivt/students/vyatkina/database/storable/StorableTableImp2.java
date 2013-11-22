3fd7ce76c81a376bc48c60a0d71be53bd089588e
Anna Vyatkina <ava.katushka@gmail.com>
2013-11-23 00:19:11 +0400

    debag output

--- src/ru/fizteh/fivt/students/vyatkina/database/storable/StorableTableImp2.java
+++ src/ru/fizteh/fivt/students/vyatkina/database/storable/StorableTableImp2.java
@@ -78,8 +78,12 @@ public class StorableTableImp2 implements StorableTable {
         try {
             tableKeeper.readLock().lock();
             if (localMap.get().containsKey(key)) {
+                System.out.println("key = " + key);
+                System.out.println("localMap.get().get(key) = " + localMap.get().get(key));
                 oldValue = localMap.get().get(key);
             } else if (mainMap.containsKey(key)) {
+                System.out.println("key = " + key);
+                System.out.println("mainMap.get().get(key) = " + mainMap.get(key));
                 oldValue = mainMap.get(key);
             }
             localMap.get().put(key, value);
