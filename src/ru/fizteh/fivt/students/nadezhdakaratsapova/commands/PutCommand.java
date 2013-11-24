99613c5d38ed9ffe27bd88f413d04f64235faf84
Nadezhda94 <amdnady@yandex.ru>
2013-11-24 18:35:34 +0400

    Put

--- src/ru/fizteh/fivt/students/nadezhdakaratsapova/commands/PutCommand.java
+++ src/ru/fizteh/fivt/students/nadezhdakaratsapova/commands/PutCommand.java
@@ -1,6 +1,5 @@
 package ru.fizteh.fivt.students.nadezhdakaratsapova.commands;
 
-import ru.fizteh.fivt.storage.structured.Storeable;
 import ru.fizteh.fivt.students.nadezhdakaratsapova.shell.Command;
 import ru.fizteh.fivt.students.nadezhdakaratsapova.tableutils.UniversalTableProvider;
 
@@ -23,7 +22,7 @@ public class PutCommand implements Command {
             Object value = curState.getCurTable().get(args[1]);
             try {
                 curState.getCurTable().put(args[1],
-                        (Storeable) curState.getCurTable().valueConverter.convertStringToValueType(args[2]));
+                        curState.getCurTable().valueConverter.convertStringToValueType(args[2]));
 
                 if (value == null) {
                     System.out.println("new");
