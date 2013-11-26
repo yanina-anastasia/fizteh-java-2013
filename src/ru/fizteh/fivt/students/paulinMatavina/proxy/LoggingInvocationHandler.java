fe20d9d717c68584e16cdc41c4d25fcf36d0c3d2
paulin-mipt <paulin.mipt@gmail.com>
2013-11-26 22:38:00 +0400

    it will be strange, if it works...

--- src/ru/fizteh/fivt/students/paulinMatavina/proxy/LoggingInvocationHandler.java
+++ src/ru/fizteh/fivt/students/paulinMatavina/proxy/LoggingInvocationHandler.java
@@ -41,18 +41,17 @@ public class LoggingInvocationHandler implements InvocationHandler {
     private void writeNull(XMLStreamWriter writer) throws XMLStreamException {
         writer.writeStartElement("null");
         writer.writeEndElement();
-        //writer.writeCharacters("<null\\>");
     }
 
     private void writeList(XMLStreamWriter writer, Iterable<?> list, IdentityHashMap<Object, Boolean> map) 
                                                                            throws XMLStreamException {  
+        if (map.containsKey(list)) {
+            writer.writeCharacters("cyclic");
+            writer.writeEndElement();
+            return;
+        }
         for (Object listElement : list) {
-            writer.writeStartElement("value");
-            if (map.containsKey(list)) {
-                writer.writeCharacters("cyclic");
-                writer.writeEndElement();
-                return;
-            } 
+            writer.writeStartElement("value"); 
             if (listElement == null) {
                 writeNull(writer);
             } else {
