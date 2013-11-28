e7c7dec6d1874e68e688a24d63c39bb1fa8ffef0
ignorer <ignorer@bk.ru>
2013-11-28 20:34:14 +0400

    Testing.

--- src/ru/fizteh/fivt/students/kislenko/proxy/MyInvocationHandler.java
+++ src/ru/fizteh/fivt/students/kislenko/proxy/MyInvocationHandler.java
@@ -33,7 +33,7 @@ public class MyInvocationHandler implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws NoSuchMethodException, XMLStreamException {
-        if (method.getDeclaringClass() == Object.class && implementation.getClass().getMethod(method.getName(), method.getParameterTypes()) == null) {
+        if (implementation.get().getClass().getMethod(method.getName(), method.getParameterTypes()).equals(Object.class.getMethod(method.getName(), method.getParameterTypes()))) {
             return null;
         }
         Object result = null;
