f5bdf93b8e2143e824ecdb6554ea8c8e4b116222
dmitrybaldin <dmitry.baldin@yandex.ru>
2013-11-28 21:10:56 +0400

    I was wrong

--- src/ru/fizteh/fivt/students/baldindima/junit/MyTableProviderFactory.java
+++ src/ru/fizteh/fivt/students/baldindima/junit/MyTableProviderFactory.java
@@ -7,18 +7,15 @@ import ru.fizteh.fivt.storage.structured.TableProviderFactory;
 import ru.fizteh.fivt.storage.structured.TableProvider;
 public class MyTableProviderFactory implements TableProviderFactory{
 	
-	public TableProvider create(String directory){
+	public TableProvider create(String directory) throws IOException{
 		if ((directory == null) || directory.trim().equals("")) {
 			throw new IllegalArgumentException(" Directory cannot be null");
 		}
 		File directoryFile = new File(directory);
 		if (!directoryFile.exists()){
 			if (!directoryFile.mkdir()){
-				try {
-					throw new IllegalArgumentException("Cannot create such directory " + directoryFile.getCanonicalPath());
-				} catch (IOException e) {
-					throw new RuntimeException("mkdir failed");
-				}
+				throw new IOException("Cannot create such directory " + directoryFile.getCanonicalPath());
+				
 					
 			}
 		}
