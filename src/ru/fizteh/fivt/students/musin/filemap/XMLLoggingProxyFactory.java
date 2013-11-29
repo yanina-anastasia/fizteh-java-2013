680f21fc001695a9d77e19ad3af66bed12d6cbe8
Kamilot <musakami@gmail.com>
2013-11-30 02:13:51 +0400

    Exceptions fix

--- src/ru/fizteh/fivt/students/musin/filemap/XMLLoggingProxyFactory.java
+++ src/ru/fizteh/fivt/students/musin/filemap/XMLLoggingProxyFactory.java
@@ -2,6 +2,7 @@ package ru.fizteh.fivt.students.musin.filemap;
 
 import ru.fizteh.fivt.proxy.LoggingProxyFactory;
 
+import javax.xml.stream.XMLStreamException;
 import java.io.Writer;
 import java.lang.reflect.Proxy;
 
@@ -10,8 +11,8 @@ public class XMLLoggingProxyFactory implements LoggingProxyFactory{
     public Object wrap(Writer writer, Object implementation, Class<?> interfaceClass) {
         try {
             XMLLoggingProxyInvocationHandler handler = new XMLLoggingProxyInvocationHandler(implementation, writer);
-            return Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), new Class[] {interfaceClass}, handler);
-        } catch (Exception e) {
+            return Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), new Class[]{interfaceClass}, handler);
+        } catch (XMLStreamException e) {
             throw new RuntimeException(e);
         }
     }
