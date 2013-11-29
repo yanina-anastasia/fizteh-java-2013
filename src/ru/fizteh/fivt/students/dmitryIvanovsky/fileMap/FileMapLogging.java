018e47a4ba4b65b5f79206589f5fdbdc751788a2
dmitry_ivanovsky <dima-iv@mail.ru>
2013-11-30 02:50:21 +0400

    some changes

--- src/ru/fizteh/fivt/students/dmitryIvanovsky/fileMap/FileMapLogging.java
+++ src/ru/fizteh/fivt/students/dmitryIvanovsky/fileMap/FileMapLogging.java
@@ -15,10 +15,12 @@ public class FileMapLogging implements InvocationHandler {
     private Object proxied;
     private Writer writer;
     private ReentrantLock writeLock = new ReentrantLock(true);
+    String s5 = "";
 
     class ProviderArrayJSON {
         private Object argument;
         private IdentityHashMap<Object, Object> identifyAttended = new IdentityHashMap<>();
+
 
         ProviderArrayJSON(Object argument) {
             this.argument = argument;
@@ -26,48 +28,70 @@ public class FileMapLogging implements InvocationHandler {
 
         private JSONArray recursiveLog(Object arg, JSONArray creatingArray) {
             JSONArray newCreatingArray = new JSONArray();
+            s5+="1";
             if (arg != null) {
+                s5+="2";
                 if (Iterable.class.isAssignableFrom(arg.getClass())) {
+                    s5+="3";
                     if (identifyAttended.containsKey(arg)) {
+                        s5+="4";
                         creatingArray.put("cyclic");
                     } else {
+                        s5+="5";
                         identifyAttended.put(arg, arg);
                         for (Object obj: (Iterable) arg) {
+                            s5+="6";
                             try {
+                                s5+="7";
                                 newCreatingArray = recursiveLog(obj, newCreatingArray);
                             } catch (java.lang.ClassCastException e) {
+                                s5+="8";
                                 newCreatingArray.put(arg.toString());
                             }
                         }
                         identifyAttended.remove(arg);
                         creatingArray.put(newCreatingArray);
+                        s5+="9";
                     }
                 } else if (arg.getClass().isArray()) {
+                    s5+="a";
                     if (identifyAttended.containsKey(arg)) {
+                        s5+="b";
                         creatingArray.put("cyclic");
                     } else {
+                        s5+="c";
                         identifyAttended.put(arg, arg);
                         identifyAttended.put(arg, arg);
                         for (Object obj: (Object[]) arg) {
+                            s5+="d";
                             try {
+                                s5+="e";
                                 newCreatingArray = recursiveLog(obj, newCreatingArray);
                             } catch (java.lang.ClassCastException e) {
+                                s5+="f";
                                 newCreatingArray.put(obj.toString());
                             }
                         }
                         identifyAttended.remove(arg);
                         creatingArray.put(newCreatingArray);
+                        s5+="g";
                     }
                 } else {
+                    s5+="q";
                     try {
+                        s5+="w";
                         creatingArray.put(arg);
                     } catch (java.lang.ClassCastException e) {
+                        s5+="e";
                         creatingArray.put(arg.toString());
                     }
                 }
+                s5+="t";
             } else {
+                s5+="y";
                 creatingArray.put(JSONObject.NULL);
             }
+            s5+="u";
             return creatingArray;
         }
 
@@ -174,7 +198,7 @@ public class FileMapLogging implements InvocationHandler {
             }
         }
         if (method.getName().equals("put")) {
-            //throw new Exception("\n-- "+s1+" -- "+record.toString()+" --\n");
+            throw new Exception("\n-- "+s5+" -- "+record.toString()+" --\n");
         }
         return returnedValue;
     }
