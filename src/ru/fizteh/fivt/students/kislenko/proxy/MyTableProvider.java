1f96bbf9c64dd494962aef702df10c7c5e5cc3fc
ignorer <ignorer@bk.ru>
2013-11-29 04:16:22 +0400

    Testing.

--- src/ru/fizteh/fivt/students/kislenko/proxy/MyTableProvider.java
+++ src/ru/fizteh/fivt/students/kislenko/proxy/MyTableProvider.java
@@ -185,9 +185,6 @@ public class MyTableProvider implements TableProvider, AutoCloseable {
     public Storeable createFor(Table table) {
         assertClosed();
         ArrayList<Class<?>> types = new ArrayList<Class<?>>();
-        if (table == null || !tables.containsValue(table)) {
-            return null;
-        }
         for (int i = 0; i < table.getColumnsCount(); ++i) {
             types.add(table.getColumnType(i));
         }
