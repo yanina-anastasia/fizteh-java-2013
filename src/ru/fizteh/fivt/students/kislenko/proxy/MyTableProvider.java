58b220cc6e190a56e23a702424ea32da42365674
ignorer <ignorer@bk.ru>
2013-11-29 04:08:47 +0400

    Testing.

--- src/ru/fizteh/fivt/students/kislenko/proxy/MyTableProvider.java
+++ src/ru/fizteh/fivt/students/kislenko/proxy/MyTableProvider.java
@@ -47,7 +47,7 @@ public class MyTableProvider implements TableProvider, AutoCloseable {
             try {
                 return createTable(name, tables.get(name).getTypes());
             } catch (IOException e) {
-                return null;
+                throw new IllegalArgumentException("Incorrect table name");
             }
         }
         lock.writeLock().lock();
@@ -201,7 +201,7 @@ public class MyTableProvider implements TableProvider, AutoCloseable {
 
     @Override
     public Storeable createFor(Table table, List<?> values) throws ColumnFormatException, IndexOutOfBoundsException {
-//        assertClosed();
+        assertClosed();
         if (values.size() < table.getColumnsCount()) {
             throw new ColumnFormatException("Invalid list of values.");
         }
@@ -216,7 +216,7 @@ public class MyTableProvider implements TableProvider, AutoCloseable {
         for (int i = 0; i < table.getColumnsCount(); ++i) {
             v.setColumnAt(i, values.get(i));
         }
-//        assertClosed();
+        assertClosed();
         return v;
     }
 
