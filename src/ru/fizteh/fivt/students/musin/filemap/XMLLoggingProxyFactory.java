eaa11275f614e3d986c1ab8373be594ce91a1b78
Kamilot <musakami@gmail.com>
2013-11-30 02:25:40 +0400

    additional argument checks

--- src/ru/fizteh/fivt/students/musin/filemap/XMLLoggingProxyFactory.java
+++ src/ru/fizteh/fivt/students/musin/filemap/XMLLoggingProxyFactory.java
@@ -9,6 +9,21 @@ import java.lang.reflect.Proxy;
 public class XMLLoggingProxyFactory implements LoggingProxyFactory{
 
     public Object wrap(Writer writer, Object implementation, Class<?> interfaceClass) {
+        if (writer == null) {
+            throw new IllegalArgumentException("Null writer not allowed");
+        }
+        if (implementation == null) {
+            throw new IllegalArgumentException("Null implementation not allowed");
+        }
+        if (interfaceClass == null) {
+            throw new IllegalArgumentException("Null doesn't name a class");
+        }
+        if (!interfaceClass.isInterface()) {
+            throw new IllegalArgumentException("interfaceClass is not an interface");
+        }
+        if (!interfaceClass.isInstance(implementation)){
+            throw new IllegalArgumentException("Object doesn't implement specified interface");
+        }
         try {
             XMLLoggingProxyInvocationHandler handler = new XMLLoggingProxyInvocationHandler(implementation, writer);
             return Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), new Class[]{interfaceClass}, handler);
