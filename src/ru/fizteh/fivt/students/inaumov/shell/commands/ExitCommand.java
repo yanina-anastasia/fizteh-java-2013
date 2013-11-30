12e3a4fcca8a129327b39b98661320cab2b09644
Igor Naumov <igor.naumov.17@gmail.com>
2013-11-30 18:32:17 +0400

    checkstyle fix8

--- src/ru/fizteh/fivt/students/inaumov/shell/commands/ExitCommand.java
+++ src/ru/fizteh/fivt/students/inaumov/shell/commands/ExitCommand.java
@@ -6,10 +6,10 @@ import ru.fizteh.fivt.students.inaumov.shell.base.Shell;
 import ru.fizteh.fivt.students.inaumov.shell.exceptions.UserInterruptionException;
 
 public class ExitCommand<State> extends AbstractCommand<State> {
-	public ExitCommand() {
-		super("exit", 0);
-	}
-	
+    public ExitCommand() {
+        super("exit", 0);
+    }
+
 	public void execute(String argumentsLine, State shellState) throws UserInterruptionException {
         String[] arguments = Shell.parseCommandParameters(argumentsLine);
         ShellUtils.checkArgumentsNumber(this, arguments.length);
--- src/ru/fizteh/fivt/students/inaumov/shell/commands/MkdirCommand.java
+++ src/ru/fizteh/fivt/students/inaumov/shell/commands/MkdirCommand.java
@@ -6,10 +6,10 @@ import ru.fizteh.fivt.students.inaumov.shell.ShellState;
 import ru.fizteh.fivt.students.inaumov.shell.base.Shell;
 
 public class MkdirCommand extends AbstractCommand<ShellState> {
-	public MkdirCommand() {
-		super("mkdir", 1);
-	}
-	
+    public MkdirCommand() {
+        super("mkdir", 1);
+    }
+
 	public void execute(String argumentsLine, ShellState shellState) {
         String[] arguments = Shell.parseCommandParameters(argumentsLine);
         ShellUtils.checkArgumentsNumber(this, arguments.length);
