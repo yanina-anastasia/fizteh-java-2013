a9ed069cd5e8b44d9e2daa864073415493b9c70e
Igor Naumov <igor.naumov.17@gmail.com>
2013-11-30 17:26:52 +0400

    checkstyle fix1

--- src/ru/fizteh/fivt/students/inaumov/shell/commands/CdCommand.java
+++ src/ru/fizteh/fivt/students/inaumov/shell/commands/CdCommand.java
@@ -6,11 +6,11 @@ import ru.fizteh.fivt.students.inaumov.shell.ShellState;
 import ru.fizteh.fivt.students.inaumov.shell.base.Shell;
 
 public class CdCommand extends AbstractCommand<ShellState> {
-	public CdCommand() {
-		super("cd", 1);
-	}
-	
-	public void execute(String argumentsLine, ShellState shellState) {
+    public CdCommand() {
+        super("cd", 1);
+    }
+
+    public void execute(String argumentsLine, ShellState shellState) {
         String[] arguments = Shell.parseCommandParameters(argumentsLine);
         ShellUtils.checkArgumentsNumber(this, arguments.length);
 
