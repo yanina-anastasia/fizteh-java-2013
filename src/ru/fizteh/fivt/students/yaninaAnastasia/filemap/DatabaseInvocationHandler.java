5d6167292fc6048c6a234918f5b0740572c759b7
Yanina Anastasia <ayanina@mail.ru>
2013-11-30 16:42:24 +0400

    fixes

--- src/ru/fizteh/fivt/students/yaninaAnastasia/filemap/DatabaseInvocationHandler.java
+++ src/ru/fizteh/fivt/students/yaninaAnastasia/filemap/DatabaseInvocationHandler.java
@@ -17,24 +17,30 @@ public class DatabaseInvocationHandler implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         Object result = null;
-
-        JSONWriter logWriter = new JSONWriter();
-        logWriter.logTimestamp();
-        logWriter.logClass(innerObject.getClass());
-        logWriter.logMethod(method);
-        logWriter.logArguments(args);
-
+        JSONWriter logWriter = null;
+        Throwable targetException = null;
+        try {
+            logWriter = new JSONWriter();
+            logWriter.logTimestamp();
+            logWriter.logClass(innerObject.getClass());
+            logWriter.logMethod(method);
+            logWriter.logArguments(args);
+        } catch (Exception e) {
+            //
+        }
         try {
             result = method.invoke(innerObject, args);
             if (!method.getReturnType().getName().equals("void")) {
                 logWriter.logReturnValue(result);
             }
         } catch (InvocationTargetException e) {
-            Throwable targetException = e.getTargetException();
-            logWriter.logThrown(targetException);
+            targetException = e.getTargetException();
             throw targetException;
         } finally {
             try {
+                if (targetException != null) {
+                    logWriter.logThrown(targetException);
+                }
                 if (!method.getDeclaringClass().equals(Object.class)) {
                     writer.write(logWriter.getStringRepresentation() + "\n");
                 }
