a9cce56a918eac36b9aec7714b1d1662077bc087
Sandrikova Maria <mashasandrikova@gmail.com>
2013-12-01 23:55:57 +0400

    Stupid bug

--- src/ru/fizteh/fivt/students/msandrikova/proxy/XMLBuilder.java
+++ src/ru/fizteh/fivt/students/msandrikova/proxy/XMLBuilder.java
@@ -43,7 +43,7 @@ public class XMLBuilder {
 	
 	public void writeThrown(Throwable thrown) throws XMLStreamException {
 		this.writer.writeStartElement("thrown");
-		this.writer.writeStartElement(thrown.toString());
+		this.writer.writeCharacters(thrown.toString());
 		this.writer.writeEndElement();
 	}
 	
