865c9c6f16a63ee531e8154e76a1acdb9786e314
Sarkin <kinan.sarmini@gmail.com>
2013-12-12 02:24:17 +0400

    Okay...

---
+++ src/ru/fizteh/fivt/students/kinanAlsarmini/grep/Main.java
@@ -0,0 +1,98 @@
+package ru.fizteh.fivt.students.kinanAlsarmini.grep;
+
+import java.io.File;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean invert = false;
+        boolean countOnly = false;
+        boolean flagsDone = false;
+
+        String pattern = null;
+        String outputFile = null;
+
+        List<File> inputFiles = new ArrayList<File>();
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("-")) {
+                if (flagsDone) {
+                    System.err.println("Invalid arguments format, usage: Grep [flags] [Files]");
+                    System.exit(1);
+                }
+                if (args[i].equals("-i")) {
+                    invert = true;
+                } else if (args[i].equals("-c")) {
+                    countOnly = true;
+                } else if (args[i].equals("-p")) {
+                    if (i == args.length - 1) {
+                        System.err.println("Invalid flag format.");
+                        System.exit(1);
+                    } else if (pattern != null) {
+                        System.err.println("Invalid flag format.");
+                        System.exit(1);
+                    } else {
+                        pattern = args[i + 1];
+                    }
+                    i++;
+                } else if (args[i].equals("-o")) {
+                    if (i == args.length - 1) {
+                        System.err.println("Invalid flag format.");
+                        System.exit(1);
+                    } else if (outputFile != null) {
+                        System.err.println("Invalid flag format.");
+                        System.exit(1);
+                    } else {
+                        outputFile = args[i + 1];
+                    }
+                    i++;
+                }
+            } else {
+                flagsDone = true;
+                inputFiles.add(new File(args[i]));
+            }
+        }
+
+        OutputStream output = null;
+        try {
+            if (outputFile != null) {
+                output = new FileOutputStream(outputFile);
+            }
+
+            MyGrepFactory factory = new MyGrepFactory();
+            MyGrep grep = factory.create(pattern);
+
+            if (countOnly) {
+                if (outputFile != null) {
+                    try (PrintWriter printer = new PrintWriter(output)) {
+                        printer.println(grep.count(inputFiles, invert));
+                    }
+                } else {
+                    System.out.println(grep.count(inputFiles, invert));
+                }
+            } else {
+                grep.find(inputFiles, output, invert);
+            }
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+    }
+}
---
+++ src/ru/fizteh/fivt/students/kinanAlsarmini/grep/MyGrep.java
@@ -0,0 +1,82 @@
+package ru.fizteh.fivt.students.kinanAlsarmini.grep;
+
+import ru.fizteh.fivt.file.Grep;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+import java.util.ArrayList;
+
+class MyGrep implements Grep {
+    private Pattern pattern;
+    public MyGrep(Pattern pattern) {
+        if (pattern == null) {
+            throw new IllegalArgumentException("Invalid pattern.");
+        }
+
+        this.pattern = pattern;
+    }
+
+    private List<String> find(File inputFile, boolean inverse) throws IOException {
+        List<String> matches = new ArrayList<String>();
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                boolean found = matcher.find();
+
+                if ((!inverse && found) || (inverse && !found)) {
+                    matches.add(line);
+                }
+            }
+        }
+
+        return matches;
+    }
+
+    @Override
+    public void find(List<File> inputFiles, OutputStream output, boolean inverse) throws IOException {
+        if (inputFiles == null || inputFiles.isEmpty()) {
+            throw new IllegalArgumentException("Input files list is empty or not valid.");
+        }
+
+        if (output == null) {
+            throw new IllegalArgumentException("find: Invalid output stream.");
+        }
+
+        try (PrintWriter printer = new PrintWriter(output)) {
+            for (File inputFile: inputFiles) {
+                printer.println(inputFile.getName() + ":");
+
+                List<String> matches = find(inputFile, inverse);
+
+                if (matches.isEmpty()) {
+                    printer.println("no matches");
+                } else {
+                    for (String match: matches) {
+                        printer.println(match);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public int count(List<File> inputFiles, boolean inverse) throws IOException {
+        if (inputFiles == null || inputFiles.isEmpty()) {
+            throw new IllegalArgumentException("Input files list is empty or not valid.");
+        }
+
+        int countMatches = 0;
+        for (File inputFile: inputFiles) {
+            countMatches += find(inputFile, inverse).size();
+        }
+
+        return countMatches;
+    }
+}
